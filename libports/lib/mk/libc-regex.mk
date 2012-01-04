938ffd2bd569ee365a767e2d509393a2c73e6ef0
Norman Feske <norman.feske@genode-labs.com>
2012-01-04 22:10:46 +0100

    Add libc/regex to libc, fix #64

---
+++ libports/lib/mk/libc-regex.mk
@@ -0,0 +1,13 @@
+LIBC_REGEX_DIR = $(LIBC_DIR)/libc/regex
+
+#
+# 'engine.c' is meant to be included by other compilation units. It cannot
+# be compiled individually.
+#
+FILTER_OUT = engine.c
+
+SRC_C = $(filter-out $(FILTER_OUT),$(notdir $(wildcard $(LIBC_REGEX_DIR)/*.c)))
+
+include $(REP_DIR)/lib/mk/libc-common.inc
+
+vpath %.c $(LIBC_REGEX_DIR)
