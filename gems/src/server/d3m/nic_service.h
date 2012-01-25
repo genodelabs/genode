759e789ddd34bb799288d25ffd123163fdcd86c9
Norman Feske <norman.feske@genode-labs.com>
2012-01-25 20:19:03 +0100

    Demo device driver manager (d3m)
    The new d3m component is the designated device-driver manager for the
    upcoming live CD. It addresses the auto probing of USB storage and ATAPI
    boot devices (issue #94) and the aggregation of user input coming from
    USB HID and PS/2.

---
+++ gems/src/server/d3m/nic_service.h
@@ -0,0 +1,68 @@
+/*
+ * \brief  D3m NIC service
+ * \author Norman Feske
+ * \author Christian Helmuth
+ * \date   2012-01-25
+ */
+
+/*
+ * Copyright (C) 2012 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU General Public License version 2.
+ */
+
+#ifndef _NIC_SERVICE_H_
+#define _NIC_SERVICE_H_
+
+/* Genode includes */
+#include <nic_session/nic_session.h>
+#include <root/component.h>
+#include <util/list.h>
+
+namespace Nic {
+
+	struct Provider
+	{
+		bool ready_to_use() { return root().valid(); }
+
+		virtual Genode::Root_capability root() = 0;
+	};
+
+	/**
+	 * Root interface of NIC service
+	 */
+	class Root : public Genode::Rpc_object<Genode::Typed_root<Nic::Session> >
+	{
+		private:
+
+			Provider &_nic_provider;
+
+		public:
+
+			Genode::Session_capability session(Session_args const &args)
+			{
+				if (!args.is_valid_string()) throw Invalid_args();
+
+				if (!_nic_provider.ready_to_use())
+					throw Unavailable();
+
+				try {
+					return Genode::Root_client(_nic_provider.root()).session(args.string());
+				} catch (...) {
+					throw Unavailable();
+				}
+			}
+
+			void upgrade(Genode::Session_capability, Upgrade_args const &) { }
+
+			void close(Genode::Session_capability session)
+			{
+				Genode::Root_client(_nic_provider.root()).close(session);
+			}
+
+			Root(Provider &nic_provider) : _nic_provider(nic_provider) { }
+	};
+}
+
+#endif /* _INPUT_SERVICE_H_ */
---
+++ gems/src/server/d3m/target.mk
@@ -0,0 +1,4 @@
+TARGET   = d3m
+SRC_CC   = main.cc
+LIBS     = cxx env signal server process
+INC_DIR += $(PRG_DIR)
