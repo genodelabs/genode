b6b05a032bd6602da905445d0c896924a32d5a7c
Norman Feske <norman.feske@genode-labs.com>
2012-02-14 16:18:16 +0100

    Simple fork test and run script
    Currently the test is failing because fork is not yet implemented.

---
+++ ports/src/test/noux_fork/test.cc
@@ -0,0 +1,38 @@
+/*
+ * \brief  Simple fork test
+ * \author Norman Feske
+ * \date   2012-02-14
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int, char **)
+{
+	int i = 0;
+
+	pid_t fork_ret = fork();
+	if (fork_ret < 0) {
+		printf("Error: fork returned %d, errno=%d\n", fork_ret, errno);
+		return -1;
+	}
+
+	printf("pid %d: fork returned %d\n", getpid(), fork_ret);
+
+	if (fork_ret == 0) {
+		printf("pid %d: child says hello\n", getpid());
+		for (int j = 0; j < 50; j++) {
+			printf("pid %d: child  i = %d\n", getpid(), i);
+		}
+	}
+
+	printf("pid %d: parent received child pid %d, starts counting...\n",
+	       getpid(), fork_ret);
+
+	for (; i < 50; ) {
+		printf("pid %d: parent i = %d\n", getpid(), i++);
+	}
+
+	return 0;
+}
