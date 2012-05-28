bf91ea89265438b26e19062f5124ba522acd66b2
Christian Helmuth <christian.helmuth@genode-labs.com>
2012-05-28 12:17:43 +0200

    Enable building libav without optimization
    Some source files do not build with -O0 due to conflicting register
    allocation and inline assembler constrains. We enforce optimization (O2)
    for these files.
    
    Also, I reduced the noise from warning messages induced by lazy handling
    of "assignment discards ‘const’ qualifier" and "passing argument from
    incompatible pointer type" in third-party sources.

--- libports/lib/mk/avfilter.mk
+++ libports/lib/mk/avfilter.mk
@@ -2,8 +2,6 @@ include $(REP_DIR)/lib/mk/av.inc
 
 include $(REP_DIR)/lib/import/import-avfilter.mk
 
-CC_WARN += -Wno-pointer-sign -Wno-uninitialized -Wno-switch
-
 LIBAVFILTER_DIR = $(REP_DIR)/contrib/$(LIBAV)/libavfilter
 
 include $(LIBAVFILTER_DIR)/Makefile
--- libports/lib/mk/avformat.mk
+++ libports/lib/mk/avformat.mk
@@ -2,8 +2,6 @@ include $(REP_DIR)/lib/mk/av.inc
 
 include $(REP_DIR)/lib/import/import-avformat.mk
 
-CC_WARN += -Wno-pointer-sign -Wno-deprecated-declarations
-
 LIBAVFORMAT_DIR = $(REP_DIR)/contrib/$(LIBAV)/libavformat
 
 include $(LIBAVFORMAT_DIR)/Makefile
