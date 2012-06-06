4769c7e6481f7dc3abfeee796ca03997b32149f6
Josef Söntgen <jsoentgen@gmail.com>
2012-06-06 14:44:30 +0200

    Fix building noux-pkg/lynx on pistachio
    Story short:  --disable-rpath-hack.
    
    Long story: When building on pistachio_x86 $(LDFLAGS) contains
    '-L[...]/l4/lib' which will be transformed by autoconf to '-rpath
    [...]/l4/lib'. Now the problem at hand is that we pretend to be FreeBSD
    when we are actually not. So autoconf assumes it is valid to use '-path'
    when it is actually not.

--- ports/src/noux-pkg/lynx/target.mk
+++ ports/src/noux-pkg/lynx/target.mk
@@ -1,6 +1,7 @@
 NOUX_CONFIGURE_ARGS = --without-ssl \
                       --disable-nls \
                       --disable-ipv6 \
+                      --disable-rpath-hack \
                       --with-cfg-file=/etc/lynx.cfg \
                       --with-lss-file=/etc/lynx.lss
 
