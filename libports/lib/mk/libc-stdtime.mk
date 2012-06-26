ed5804079418f3f8875d65a0e41442e2a1a13f5c
Christian Prochaska <christian.prochaska@genode-labs.com>
2012-06-26 14:27:24 +0200

    Define 'TZ_MAX_TIMES' as 1
    This patch defines 'TZ_MAX_TIMES' as 1 when building 'localtime.c' to avoid
    a big stack allocation in the 'tzload()' function.
    
    Fixes #250.

--- libports/lib/mk/libc-stdtime.mk
+++ libports/lib/mk/libc-stdtime.mk
@@ -2,6 +2,8 @@ LIBC_STDTIME_DIR = $(LIBC_DIR)/libc/stdtime
 
 SRC_C = $(filter-out $(FILTER_OUT),$(notdir $(wildcard $(LIBC_STDTIME_DIR)/*.c)))
 
+CC_OPT_localtime = -DTZ_MAX_TIMES=1
+
 include $(REP_DIR)/lib/mk/libc-common.inc
 
 vpath %.c $(LIBC_STDTIME_DIR)
