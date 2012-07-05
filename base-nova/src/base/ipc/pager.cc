77729fab2a4b63c8d3a3c23712d02cfd5ceb0bca
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2012-07-05 11:14:02 +0200

    NOVA: warning fixes - check result of append_item

--- base-nova/src/base/ipc/pager.cc
+++ base-nova/src/base/ipc/pager.cc
@@ -57,7 +57,9 @@ void Ipc_pager::set_reply_mapping(Mapping m)
 {
 	Nova::Utcb *utcb = (Nova::Utcb *)Thread_base::myself()->utcb();
 	utcb->set_msg_word(0);
-	utcb->append_item(m.mem_crd(), m.dst_addr());
+	bool res = utcb->append_item(m.mem_crd(), m.dst_addr());
+	/* one item ever fits on the UTCB */
+	(void)res;
 }
 
 
