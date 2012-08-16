66198660aaf759de2c74b98bddfebe28c47640f9
Josef Söntgen <jsoentgen@gmail.com>
2012-08-16 08:41:24 +0200

    Noux: add noux_net_lighttpd.run
    This patch adds lighttpd as noux package. However, we do not use the
    original build system but rather compile lighttpd directly from the
    Genode build system. This is needed because we want to statically link
    lighttpd modules into the binary. This mode is (somehow) supported by
    the SConscript that comes with lighttpd. However, the GNU build scripts
    do not expose this feature.

--- ports/src/noux-pkg/lighttpd/target.mk
+++ ports/src/noux-pkg/lighttpd/target.mk
@@ -1,26 +1,8 @@
-TARGET = lighttpd
+TARGET = lighttpd_noux
 
-NOUX_CONFIGURE_ARGS += --disable-ipv6 \
-                       --disable-mmap \
-                       --without-bzip2 \
-                       --without-pcre
+include $(REP_DIR)/src/app/lighttpd/target.inc
 
-LIBS += libcrypto libssl zlib #libc_resolv
+# search path for 'plugin-static.h'
+INC_DIR += $(REP_DIR)/src/app/lighttpd
 
-#
-# Make the zlib linking test succeed
-#
-Makefile: dummy_libs
-
-NOUX_LDFLAGS += -L$(PWD)
-
-dummy_libs: libz.a libcrypto.a libssl.a
-
-libcrypto.a:
-	$(VERBOSE)$(AR) -rc $@
-libssl.a:
-	$(VERBOSE)$(AR) -rc $@
-libz.a:
-	$(VERBOSE)$(AR) -rc $@
-
-include $(REP_DIR)/mk/noux.mk
+LIBS += libc_noux
