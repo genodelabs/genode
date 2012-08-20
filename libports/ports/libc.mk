a8609213a011454ef7a00fcccd4aa94511264153
Norman Feske <norman.feske@genode-labs.com>
2012-08-20 17:46:04 +0200

    libc: Resolve subtle ambiguity in symlink creation
    Apparently, GNU make 3.81 picks the generic '%.h' rule instead of the
    more specific 'rpc/%.h' rule during the preparation of the libc.
    Changing the order of the rules solves the problem.

--- libports/ports/libc.mk
+++ libports/ports/libc.mk
@@ -497,9 +497,6 @@ libc_gen_symlink_subsubsubsub = $(VERBOSE)mkdir -p $(dir $@); ln -sf ../../../..
 include/libc/arpa/%.h: $(CONTRIB_DIR)/$(LIBC)/include/arpa/%.h
 	$(libc_gen_symlink_subsubsub)
 
-include/libc/%.h: $(CONTRIB_DIR)/$(LIBC)/include/%.h
-	$(libc_gen_symlink_subsub)
-
 include/libc/gssapi/%.h: $(CONTRIB_DIR)/$(LIBC)/include/gssapi/%.h
 	$(libc_gen_symlink_subsubsub)
 
@@ -517,6 +514,9 @@ include/libc/rpc/%.h: $(CONTRIB_DIR)/$(LIBC)/include/rpc/%.h
 
 include/libc/rpcsvc/%.h: $(CONTRIB_DIR)/$(LIBC)/include/rpcsvc/%.h
 	$(libc_gen_symlink_subsubsub)
+
+include/libc/%.h: $(CONTRIB_DIR)/$(LIBC)/include/%.h
+	$(libc_gen_symlink_subsub)
 
 include/libc/%.h: $(CONTRIB_DIR)/$(LIBC)/sys_sys/%.h
 	$(libc_gen_symlink_subsub)
@@ -583,7 +583,7 @@ apply_patches-libc: checkout-libc
 # as side effect of the 'LIBC_DIRS_TO_CHECKOUT' out rule).
 #
 create_include_symlinks-libc: checkout-libc
-	$(VERBOSE)make -s $(LIBC_IMPORT_INCLUDES)
+	$(VERBOSE)make $(LIBC_IMPORT_INCLUDES) VERBOSE=$(VERBOSE)
 
 prepare-libc: apply_patches-libc libc_net_generate libc_rpc_generate create_include_symlinks-libc
 
