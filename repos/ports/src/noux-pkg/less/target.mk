b80939cc0a0ed80ba236c8fa831601908525cfc6
Josef Söntgen <josef.soentgen@genode-labs.com>
2012-10-04 11:33:57 +0200

    Noux: add noux-pkg/less
    Issue #2014

---
+++ repos/ports/src/noux-pkg/less/target.mk
@@ -0,0 +1,17 @@
+TARGET = less
+
+LIBS   = ncurses
+
+#
+# Make the ncurses linking test succeed and copy the actual Makefile
+#
+Makefile: dummy_libs
+
+NOUX_LDFLAGS += -L$(PWD)
+
+dummy_libs: libncursesw.a
+
+libncursesw.a:
+	$(VERBOSE)$(AR) -rc $@
+
+include $(REP_DIR)/mk/noux.mk
