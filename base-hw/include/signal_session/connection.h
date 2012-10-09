965ffc1df26c0c20ed827c2941c99ca9c50f9499
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2012-10-09 14:05:45 +0200

    base-hw: increase signal session initial quota

---
+++ base-hw/include/signal_session/connection.h
@@ -0,0 +1,33 @@
+/*
+ * \brief  base-hw specific (higher ram-quota) connection to signal service
+ * \author Norman Feske
+ * \author Stefan Kalkowski
+ * \date   2009-08-05
+ */
+
+/*
+ * Copyright (C) 2009-2012 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU General Public License version 2.
+ */
+
+#ifndef _INCLUDE__SIGNAL_SESSION__CONNECTION_H_
+#define _INCLUDE__SIGNAL_SESSION__CONNECTION_H_
+
+#include <signal_session/client.h>
+#include <base/connection.h>
+
+namespace Genode {
+
+	struct Signal_connection : Connection<Signal_session>, Signal_session_client
+	{
+		Signal_connection()
+		:
+			Connection<Signal_session>(session("ram_quota=16K")),
+			Signal_session_client(cap())
+		{ }
+	};
+}
+
+#endif /* _INCLUDE__CAP_SESSION__CONNECTION_H_ */
