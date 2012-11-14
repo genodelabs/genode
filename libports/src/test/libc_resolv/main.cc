42df6e3d0184489b5e323c3cf657e2fd6da49bf7
Josef Söntgen <josef.soentgen@genode-labs.com>
2012-11-14 16:30:44 +0100

    libports: add test/libc_resolv target
    libc_resolv is used by noux-pkgs and is only a runtime dependency but
    therefore needs to be build.

---
+++ libports/src/test/libc_resolv/main.cc
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2012 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU General Public License version 2.
+ */
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
---
+++ libports/src/test/libc_resolv/target.mk
@@ -0,0 +1,3 @@
+TARGET   = test-libc_resolv
+SRC_CC   = main.cc
+LIBS     = cxx env libc libc_resolv
