17966b707bdf5c01dd44b637a3de6f7247257dbe
Robert Campbell <waprave@gmail.com>
2013-01-11 22:17:35 -0500

    Fix download of jbig2dec
    Fixes #613

--- libports/ports/jbig2dec.mk
+++ libports/ports/jbig2dec.mk
@@ -1,6 +1,11 @@
-JBIG2DEC     = jbig2dec-0.11
-JBIG2DEC_TGZ = $(JBIG2DEC).tar.gz
-JBIG2DEC_URL = http://ghostscript.com/~giles/jbig2/jbig2dec/$(JBIG2DEC_TGZ)
+JBIG2DEC = jbig2dec-0.11
+GIT_URL  = git://git.ghostscript.com/jbig2dec.git
+GIT_REV  = 58b513e3ec60feac13ea429c4aff12ea8a8de91d
+
+#
+# Check for tools
+#
+$(call check_tool,git)
 
 #
 # Interface to top-level prepare Makefile
@@ -14,11 +19,17 @@ $(CONTRIB_DIR)/$(JBIG2DEC): clean-jbig2dec
 #
 # Port-specific local rules
 #
-$(DOWNLOAD_DIR)/$(JBIG2DEC_TGZ):
-	$(VERBOSE)wget -c -P $(DOWNLOAD_DIR) $(JBIG2DEC_URL) && touch $@
 
-$(CONTRIB_DIR)/$(JBIG2DEC): $(DOWNLOAD_DIR)/$(JBIG2DEC_TGZ)
-	$(VERBOSE)tar xfz $< -C $(CONTRIB_DIR) && touch $@
+$(DOWNLOAD_DIR)/$(JBIG2DEC)/.git:
+	$(VERBOSE)git clone $(GIT_URL) $(DOWNLOAD_DIR)/$(JBIG2DEC) && \
+	cd $(DOWNLOAD_DIR)/$(JBIG2DEC) && \
+	git reset --hard $(GIT_REV) && \
+	cd ../.. && touch $@
+	
+$(CONTRIB_DIR)/$(JBIG2DEC)/.git: $(DOWNLOAD_DIR)/$(JBIG2DEC)/.git
+	$(VERBOSE)git clone $(DOWNLOAD_DIR)/$(JBIG2DEC) $(CONTRIB_DIR)/$(JBIG2DEC)
+	
+$(CONTRIB_DIR)/$(JBIG2DEC): $(CONTRIB_DIR)/$(JBIG2DEC)/.git
 
 include/jbig2dec/jbig2.h:
 	$(VERBOSE)mkdir -p $(dir $@)
