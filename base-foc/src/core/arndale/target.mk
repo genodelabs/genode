7389e364faabbfaa7081e1270d5c669507387d18
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2013-01-23 12:05:12 +0100

    Arndale: Build uImage, kernel, bootstrap

---
+++ base-foc/src/core/arndale/target.mk
@@ -0,0 +1,9 @@
+include $(PRG_DIR)/../target.inc
+
+LD_TEXT_ADDR = 0x80140000
+
+REQUIRES += arm foc_arndale
+SRC_CC   += arm/platform_arm.cc
+INC_DIR  += $(REP_DIR)/src/core/include/arm
+
+vpath platform_services.cc $(GEN_CORE_DIR)
