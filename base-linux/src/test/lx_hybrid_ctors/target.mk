cecfbf2eb4d1d90ba2060101367e30da09c250f2
Christian Prochaska <christian.prochaska@genode-labs.com>
2013-01-26 19:46:04 +0100

    lx_hybrid_ctors: remove 'soname' link option
    This patch removes the 'soname' link option for building the host
    library for the 'lx_hybrid_ctors' test. Without this option, the
    library's absolute path at build time gets hardcoded into the
    application, which should be okay for this simple test case.
    
    Fixes #638.

--- base-linux/src/test/lx_hybrid_ctors/target.mk
+++ base-linux/src/test/lx_hybrid_ctors/target.mk
@@ -2,17 +2,17 @@ TARGET = test-lx_hybrid_ctors
 SRC_CC = main.cc
 LIBS   = env lx_hybrid
 
-EXT_OBJECTS += $(BUILD_BASE_DIR)/test/lx_hybrid_ctors/libtestlib.so
-
 TESTLIB_SO     = libtestlib.so
 TESTLIB_SRC_CC = testlib.cc
 
-$(TARGET): libtestlib.so
+EXT_OBJECTS += $(BUILD_BASE_DIR)/test/lx_hybrid_ctors/$(TESTLIB_SO)
+
+$(TARGET): $(TESTLIB_SO)
 
 $(TESTLIB_SO): $(TESTLIB_SRC_CC)
 	$(MSG_BUILD)$(TESTLIB_SO)
 	$(VERBOSE)g++ -fPIC -c $^
-	$(VERBOSE)g++ -shared -Wlsoname,$(TESTLIB_SO) -o $@ $(notdir $(^:.cc=.o))
+	$(VERBOSE)g++ -shared -o $@ $(notdir $(^:.cc=.o))
 
 clean_libtestlib:
 	$(VERBOSE)rm -f $(TESTLIB_SO) $(TESTLIB_SRC_CC:.cc=.o)
