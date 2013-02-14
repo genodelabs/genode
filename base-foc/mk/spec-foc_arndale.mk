79949bb33863b5df02e9a83e34da48f58db84d3e
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2013-02-14 16:57:09 +0100

    foc: Add 'uboot' spec to Arndale

--- base-foc/mk/spec-foc_arndale.mk
+++ base-foc/mk/spec-foc_arndale.mk
@@ -1,4 +1,4 @@
-SPECS += foc_arm platform_arndale
+SPECS += foc_arm platform_arndale uboot
 
 include $(call select_from_repositories,mk/spec-platform_arndale.mk)
 include $(call select_from_repositories,mk/spec-foc_arm.mk)
