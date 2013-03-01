9d0a377efe685c5635bdcfc4f07bb9c01e5d5544
Norman Feske <norman.feske@genode-labs.com>
2013-03-01 12:19:32 +0100

    Add 'seoul_libc_support' library
    This library contains the subset of the libc that is needed for the
    Seoul VMM.
    
    Related to issue #666.

---
+++ ports/lib/mk/seoul_libc_support.mk
@@ -0,0 +1,7 @@
+include $(call select_from_repositories,lib/mk/libc-common.inc)
+
+SRC_C  = stdlib/strtoul.c
+SRC_C += $(addprefix string/,strchr.c strncpy.c strspn.c strcspn.c strstr.c strlen.c strnlen.c strcpy.c memcmp.c strcmp.c)
+SRC_C += sys/__error.c gen/errno.c locale/none.c locale/table.c
+
+vpath %.c $(LIBC_DIR)/libc
