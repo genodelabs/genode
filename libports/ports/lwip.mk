69b16c24830e6109e3e46d5453222a8fb80ea572
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2013-04-29 15:35:39 +0200

    lwip: add "window scaling" support, RFC 1323
    Apply patch from lwip webpage to recent lwip git master branch.

--- libports/ports/lwip.mk
+++ libports/ports/lwip.mk
@@ -2,6 +2,7 @@ include ports/lwip.inc
 
 LWIP_URL = git://git.savannah.nongnu.org/lwip.git
 LWIP_REV = fe63f36656bd66b4051bdfab93e351a584337d7c
+PATCH_URL_WINDOW_SCALING = https://savannah.nongnu.org/patch/download.php?file_id=28026
 
 #
 # Interface to top-level prepare Makefile
@@ -23,14 +24,17 @@ $(CONTRIB_DIR)/$(LWIP): clean-lwip
 #
 $(DOWNLOAD_DIR)/$(LWIP)/.git:
 	$(VERBOSE)git clone $(LWIP_URL) $(DOWNLOAD_DIR)/$(LWIP) && \
-	cd download/$(LWIP) && \
+	cd $(DOWNLOAD_DIR)/$(LWIP) && \
 	git reset --hard $(LWIP_REV) && \
+	wget $(PATCH_URL_WINDOW_SCALING) -O window_scaling.patch && \
 	cd ../.. && touch $@
+
 
 $(CONTRIB_DIR)/$(LWIP)/.git: $(DOWNLOAD_DIR)/$(LWIP)/.git
 	$(VERBOSE)git clone $(DOWNLOAD_DIR)/$(LWIP) $(CONTRIB_DIR)/$(LWIP)
 	$(VERBOSE)find ./src/lib/lwip/ -name "*.patch" |\
 		xargs -ixxx sh -c "patch -p1 -r - -N -d $(CONTRIB_DIR)/$(LWIP) < xxx" || true
+	$(VERBOSE)patch -p1 -d $(CONTRIB_DIR)/$(LWIP) < $(DOWNLOAD_DIR)/$(LWIP)/window_scaling.patch
 
 $(CONTRIB_DIR)/$(LWIP): $(CONTRIB_DIR)/$(LWIP)/.git
 
