6b42fff30ec1dcdc80dd398b715b66f310b41055
Martin Stein <martin.stein@genode-labs.com>
2013-06-06 14:36:42 +0200

    hw: use generic crt0.s for progs other than core
    Use '__initial_sp' instead of '_main_utcb' to receive a main threads
    UTCB pointer outside core.
    
    Ref #766

--- base-hw/src/base/thread_support.cc
+++ base-hw/src/base/thread_support.cc
@@ -19,7 +19,7 @@
 
 using namespace Genode;
 
-extern Native_utcb * _main_utcb;
+extern Native_utcb * __initial_sp;
 
 namespace Genode { Rm_session  *env_context_area_rm_session(); }
 
@@ -31,7 +31,7 @@ namespace Genode { Rm_session  *env_context_area_rm_session(); }
 Native_utcb * Thread_base::utcb()
 {
 	/* this is a main thread, so CRT0 provides UTCB through '_main_utcb' */
-	if (!this) return _main_utcb;
+	if (!this) return __initial_sp;
 
 	/* otherwise we have a valid thread base */
 	return &_context->utcb;
