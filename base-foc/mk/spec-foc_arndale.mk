98ee7612f156b1dc0297d01ddaa732588309fa23
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2013-06-19 11:31:07 +0200

    base: ARM-FPU 'memcpy_cpu' implementation
    Added spec file for ARM-VFPv3 floating-point unit. This shadows
    'base/include/arm/' with 'base/include/arm/vfp' and enables a 'memcpy_cpu'
    version that mainly uses the FPU. Enabled VFP support for 'foc_arndale'.
    
    Ref #773

--- base-foc/mk/spec-foc_arndale.mk
+++ base-foc/mk/spec-foc_arndale.mk
@@ -1,4 +1,5 @@
 SPECS += foc_arm platform_arndale uboot
 
+include $(call select_from_repositories,mk/spec-fpu_vfpv3.mk)
 include $(call select_from_repositories,mk/spec-platform_arndale.mk)
 include $(call select_from_repositories,mk/spec-foc_arm.mk)
