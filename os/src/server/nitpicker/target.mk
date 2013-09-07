518a71ccf590e6d7e0819ee3dfc4a7dd1935a9ae
Norman Feske <norman.feske@genode-labs.com>
2013-09-07 23:58:10 +0200

    nitpicker: Simplify internal file structure

--- os/src/server/nitpicker/target.mk
+++ os/src/server/nitpicker/target.mk
@@ -5,14 +5,8 @@ SRC_CC   = main.cc \
            view.cc \
            user_state.cc \
            global_keys.cc
+
 SRC_BIN  = default.tff
 
 # enable C++11 support
 CC_CXX_OPT += -std=gnu++11
-
-INC_DIR  = $(PRG_DIR)/../include \
-           $(PRG_DIR)/../data
-
-vpath %.cc $(PRG_DIR)/../common
-vpath %    $(PRG_DIR)/../data
-
