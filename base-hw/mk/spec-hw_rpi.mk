62019be6d3a46e031e44e06c1663a8ef95031519
Norman Feske <norman.feske@genode-labs.com>
2013-09-15 17:45:35 +0200

    rpi: Change link address to support larger images

--- base-hw/mk/spec-hw_rpi.mk
+++ base-hw/mk/spec-hw_rpi.mk
@@ -8,7 +8,7 @@
 SPECS += hw platform_rpi
 
 # set address where to link the text segment at
-LD_TEXT_ADDR ?= 0x400000
+LD_TEXT_ADDR ?= 0x800000
 
 # include implied specs
 include $(call select_from_repositories,mk/spec-hw.mk)
