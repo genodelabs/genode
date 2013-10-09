1113f36e2f98688ecb8ee15ae1547154979b4e67
Martin Stein <martin.stein@genode-labs.com>
2013-10-09 10:34:08 +0200

    arndale: specs to enable demo scenario
    ref #796

--- base/mk/spec-platform_arndale.mk
+++ base/mk/spec-platform_arndale.mk
@@ -5,7 +5,7 @@
 #
 
 # denote specs that are fullfilled by this spec
-SPECS += exynos5 cortex_a15
+SPECS += exynos5 cortex_a15 framebuffer usb
 
 # add repository relative paths
 REP_INC_DIR += include/platform/arndale \
