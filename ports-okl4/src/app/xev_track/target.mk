2322ab202cf7ff51c9facb938a1b54bff135086c
Norman Feske <norman.feske@genode-labs.com>
2013-10-17 16:42:25 +0200

    Remove OKLinux
    Fixes #29
    Fixes #30
    Fixes #889

--- ports-okl4/src/app/xev_track/target.mk
+++
@@ -1,6 +0,0 @@
-TARGET       = xevent-tracker
-REQUIRES     = host x11 xtest xdamage
-SRC_CC       = main.cc
-LIBS         = xev_track
-EXT_OBJECTS += -lX11 -lXdamage /usr/lib/libXtst.so.6
-INC_DIR     += $(REP_DIR)/include/oklx_kernel
