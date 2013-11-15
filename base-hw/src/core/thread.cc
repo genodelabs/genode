3b2590b65a93645cdb9b9bbfdcb49c010dc6803b
Martin Stein <martin.stein@genode-labs.com>
2013-11-15 16:56:34 +0100

    hw: identify core threads through stack pointer
    ref #953

--- base-hw/src/core/thread.cc
+++ base-hw/src/core/thread.cc
@@ -37,20 +37,24 @@ Native_utcb * Thread_base::utcb()
 }
 
 
-/**
- * Returns 0 if this is the main thread or the thread base pointer otherwise
- */
 Thread_base * Thread_base::myself()
 {
-	Platform_thread * const t = Kernel::get_thread(0);
-	if (t) { return t->thread_base(); }
+	/* get thread ident from the aligned base of the stack */
+	int dummy = 0;
+	addr_t sp = (addr_t)(&dummy);
+	enum { SP_MASK = ~((1 << CORE_STACK_ALIGNM_LOG2) - 1) };
+	Core_thread_id id = *(Core_thread_id *)((addr_t)sp & SP_MASK);
+
+	/* if the ident is zero this is the main thread */
+	Platform_thread * const pt = (Platform_thread *)id;
+	if (pt) { return pt->thread_base(); }
 	return 0;
 }
 
 
 void Thread_base::_thread_start()
 {
-	/* this is never called by a main thread */
+	/* this is never called by the main thread */
 	Thread_base::myself()->_thread_bootstrap();
 	Thread_base::myself()->entry();
 }
@@ -73,12 +77,30 @@ Thread_base::~Thread_base()
 
 void Thread_base::start()
 {
-	size_t const stack_size = _tid.pt->stack_size()/sizeof(umword_t) + 1;
-	void * const stack_base = new (platform()->core_mem_alloc())
-	                              umword_t [stack_size];
-	void * sp = (void *)((addr_t)stack_base + _tid.pt->stack_size());
+	/* allocate stack memory that fullfills the constraints for core stacks */
+	size_t const size = _tid.pt->stack_size();
+	if (size > (1 << CORE_STACK_ALIGNM_LOG2) - sizeof(Core_thread_id)) {
+		PERR("stack size does not fit stack alignment of core");
+		return;
+	}
+	void * base;
+	Platform * const p = static_cast<Platform *>(platform());
+	Range_allocator * const alloc = p->core_mem_alloc();
+	if (alloc->alloc_aligned(size, &base, CORE_STACK_ALIGNM_LOG2).is_error()) {
+		PERR("failed to allocate stack memory");
+		return;
+	}
+	/* provide thread ident at the aligned base of the stack */
+	*(Core_thread_id *)base = (Core_thread_id)_tid.pt;
+
+	/* start thread with stack pointer at the top of stack */
+	void * sp = (void *)((addr_t)base + size);
 	void * ip = (void *)&_thread_start;
-	if (_tid.pt->start(ip, sp)) PERR("Couldn't start thread");
+	if (_tid.pt->start(ip, sp)) {
+		PERR("failed to start thread");
+		alloc->free(base, size);
+		return;
+	}
 }
 
 
