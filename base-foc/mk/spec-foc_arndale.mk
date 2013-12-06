9759000538c3452bc86c5474bd0b7191ff7979b1
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2013-12-06 11:49:30 +0100

    foc: evaluate run_opt target for uboot link
    Unify handling as base-hw does and enables us to use uBoot image for foc_panda.

--- base-foc/mk/spec-foc_arndale.mk
+++ base-foc/mk/spec-foc_arndale.mk
@@ -1,4 +1,4 @@
-SPECS += foc_arm platform_arndale uboot
+SPECS += foc_arm platform_arndale
 
 include $(call select_from_repositories,mk/spec-fpu_vfpv3.mk)
 include $(call select_from_repositories,mk/spec-platform_arndale.mk)
