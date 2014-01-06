12f560dc0e02894f340055adef3808d757436be8
Martin Stein <martin.stein@genode-labs.com>
2014-01-06 20:07:46 +0100

    fiasco: remove remains of once supported ARM port
    ref #989

--- base-fiasco/lib/mk/arm/platform.mk
+++
@@ -1,6 +0,0 @@
-#
-# Configuration for L4 build system (for kernel-bindings, sigma0, bootstrap).
-#
-L4_CONFIG = $(REP_DIR)/config/l4env-config.x86
-
-include $(REP_DIR)/lib/mk/platform.inc
--- base-fiasco/src/core/arm/target.mk
+++
@@ -1,8 +0,0 @@
-include $(PRG_DIR)/../target.inc
-
-REQUIRES += arm
-SRC_CC   += platform_arm.cc
-
-vpath io_port_session_component.cc $(GEN_CORE_DIR)/arm
-vpath platform_services.cc         $(GEN_CORE_DIR)
-
