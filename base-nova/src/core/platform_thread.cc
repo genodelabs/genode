93f06dd11af1849084487518890080e563246c1f
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2014-03-17 16:57:59 +0100

    nova: set invalid affinity space in thread constructor
    Otherwise affinity space is set to 1x1 and in cpu_session_component.cc the cpu
    session local affinity space defined by parent is not taken because
    "Location::valid()" returns true.
    
    Issue #1107

--- base-nova/src/core/platform_thread.cc
+++ base-nova/src/core/platform_thread.cc
@@ -286,7 +286,7 @@ Weak_ptr<Address_space> Platform_thread::address_space()
 Platform_thread::Platform_thread(const char *name, unsigned, int thread_id)
 :
 	_pd(0), _pager(0), _id_base(cap_map()->insert(1)),
-	_sel_exc_base(Native_thread::INVALID_INDEX), _location(boot_cpu(), 0),
+	_sel_exc_base(Native_thread::INVALID_INDEX), _location(boot_cpu(), 0, 0, 0),
 	_features(0)
 {
 	strncpy(_name, name, sizeof(_name));
