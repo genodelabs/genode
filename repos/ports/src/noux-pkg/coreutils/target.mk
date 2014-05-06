99245aceb25e0b0237e692202b542ed48e19901e
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2014-05-06 16:52:58 +0200

    noux: Disable makeinfo for coreutils
    Issue #1051

--- repos/ports/src/noux-pkg/coreutils/target.mk
+++ repos/ports/src/noux-pkg/coreutils/target.mk
@@ -6,5 +6,6 @@ NOUX_CONFIGURE_ARGS = --disable-acl --disable-largefile --disable-xattr \
 # libary, which is not supported by Noux, yet.
 #
 NOUX_CPPFLAGS += -U__ELF__
+NOUX_MAKE_ENV += "MAKEINFO=true"
 
 include $(REP_DIR)/mk/noux.mk
