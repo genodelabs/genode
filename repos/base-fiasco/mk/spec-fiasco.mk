de4335b4433e604ccbd266982ad1b429cc6fea4b
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2014-05-23 12:06:08 +0200

    base-fiasco: migrate to new ports mechanism
    Issue #1082

--- repos/base-fiasco/mk/spec-fiasco.mk
+++ repos/base-fiasco/mk/spec-fiasco.mk
@@ -10,8 +10,8 @@
 -include $(call select_from_repositories,etc/fiasco.conf)
 -include $(BUILD_BASE_DIR)/etc/fiasco.conf
 
-L4_BUILD_DIR ?= $(BUILD_BASE_DIR)/l4
-L4_SRC_DIR   ?= $(REP_DIR)/contrib/fiasco/snapshot
+L4_BUILD_DIR := $(BUILD_BASE_DIR)/l4
+L4_SRC_DIR   := $(call select_from_ports,fiasco)/src/kernel/fiasco/fiasco/snapshot
 
 #
 # L4/sys headers
