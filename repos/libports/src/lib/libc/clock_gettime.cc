c745aa445478a26217e2419eec30c56e607a1438
Josef Söntgen <josef.soentgen@genode-labs.com>
2014-08-20 16:49:13 +0200

    libc: utilize Rtc_file_system in time functions
    Fixes #1241.

--- repos/libports/src/lib/libc/clock_gettime.cc
+++ repos/libports/src/lib/libc/clock_gettime.cc
@@ -16,13 +16,25 @@
 
 #include <sys/time.h>
 
+namespace Libc {
+extern time_t read_rtc();
+}
+
 extern "C" __attribute__((weak))
 int clock_gettime(clockid_t clk_id, struct timespec *tp)
 {
+	static bool read_rtc = false;
+	static time_t rtc = 0;
+
+	if (!read_rtc) {
+		rtc = Libc::read_rtc();
+		read_rtc = true;
+	}
+
 	Genode::Alarm::Time time = Genode::Timeout_thread::alarm_timer()->time();
 
 	if (tp) {
-		tp->tv_sec = time / 1000;
+		tp->tv_sec = rtc + time / 1000;
 		tp->tv_nsec = (time % 1000) * (1000 * 1000);
 	}
 
--- repos/libports/src/lib/libc/gettimeofday.cc
+++ repos/libports/src/lib/libc/gettimeofday.cc
@@ -15,13 +15,25 @@
 
 #include <sys/time.h>
 
+namespace Libc {
+extern time_t read_rtc();
+}
+
 extern "C" __attribute__((weak))
 int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
+	static bool read_rtc = false;
+	static time_t rtc = 0;
+
+	if (!read_rtc) {
+		rtc = Libc::read_rtc();
+		read_rtc = true;
+	}
+
 	Genode::Alarm::Time time = Genode::Timeout_thread::alarm_timer()->time();
 
 	if (tv) {
-		tv->tv_sec = time / 1000;
+		tv->tv_sec = rtc + time / 1000;
 		tv->tv_usec = (time % 1000) * 1000;
 	}
 
