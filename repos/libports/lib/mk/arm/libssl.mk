53227141836ad790058d91e1390da71a579da698
Josef Söntgen <josef.soentgen@genode-labs.com>
2014-08-21 12:40:42 +0200

    libssl: use platform specific mk files
    Instead of checking the target platform in the mk file use the common
    pattern of providing a mk file for each platform.
    
    Fixes #1236.

---
+++ repos/libports/lib/mk/arm/libssl.mk
@@ -0,0 +1,4 @@
+include $(REP_DIR)/lib/mk/libssl.inc
+
+INC_DIR += $(REP_DIR)/src/lib/openssl/arm
+INC_DIR += $(LIBSSL_PORT_DIR)/src/lib/openssl/arm
---
+++ repos/libports/lib/mk/x86_64/libssl.mk
@@ -0,0 +1,4 @@
+include $(REP_DIR)/lib/mk/libssl.inc
+
+INC_DIR += $(REP_DIR)/src/lib/openssl/x86_64
+INC_DIR += $(LIBSSL_PORT_DIR)/src/lib/openssl/x86_64
