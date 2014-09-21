23dcbff9980c17a542abab042b1697b965996bea
Josef Söntgen <josef.soentgen@genode-labs.com>
2014-09-21 10:55:27 +0200

    libports: fix sdl_mixer include paths
    Fixes #1255.

--- repos/libports/lib/import/import-sdl_mixer.mk
+++ repos/libports/lib/import/import-sdl_mixer.mk
@@ -1,1 +1,2 @@
-INC_DIR += $(call select_from_ports,sdl_mixer)/include
+SDL_MIXER_PORT_DIR := $(call select_from_ports,sdl_mixer)
+INC_DIR += $(SDL_MIXER_PORT_DIR)/include $(SDL_MIXER_PORT_DIR)/include/SDL
