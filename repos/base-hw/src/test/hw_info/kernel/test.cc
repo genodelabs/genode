b3655902ed2841a115b93682a9c7d9b87393956a
Martin Stein <martin.stein@genode-labs.com>
2014-10-21 19:01:40 +0200

    hw & arm_v7: provide run/hw_info
    The run test 'hw_info' prints the content of the basic ARMv7 identification and
    feature registers in a pretty readable format. It is a kernel-internal test
    because many of these registers are restricted to privilege level 1 or higher.
    
    fix #1278

---
+++ repos/base-hw/src/test/hw_info/kernel/test.cc
@@ -0,0 +1,32 @@
+/*
+ * \brief  Provide detailed hardware Information
+ * \author Martin Stein
+ * \date   2014-10-20
+ */
+
+/*
+ * Copyright (C) 2014 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU General Public License version 2.
+ */
+
+/* base includes */
+#include <base/printf.h>
+
+using namespace Genode;
+
+namespace Kernel { void test(); }
+
+void info();
+
+/**
+ * Main routine
+ */
+void Kernel::test()
+{
+	printf("\n");
+	info();
+	printf("------ End ------\n");
+	while (1) ;
+}
