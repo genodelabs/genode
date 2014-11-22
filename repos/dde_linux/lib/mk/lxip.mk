2eaa55e6b1f819a199dba34f2826b3391f02a2af
Josef Söntgen <josef.soentgen@genode-labs.com>
2014-11-22 15:11:00 +0100

    dde_linux: remove base cxx from lxip.mk

--- repos/dde_linux/lib/mk/lxip.mk
+++ repos/dde_linux/lib/mk/lxip.mk
@@ -3,7 +3,8 @@ SHARED_LIB = yes
 LIB_DIR     = $(REP_DIR)/src/lib/lxip
 LIB_INC_DIR = $(LIB_DIR)/include
 
-LIBS += base cxx dde_kit
+# FIXME should we *really* effecticely add dde_kit to this shared library?
+LIBS += dde_kit
 
 LX_CONTRIB_DIR := $(call select_from_ports,dde_linux)/src/lib/lxip
 NET_DIR        := $(LX_CONTRIB_DIR)/net
