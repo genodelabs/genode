136d87bd9aba5a5cf27e5fe73da9bfb6f1557afe
Christian Prochaska <christian.prochaska@genode-labs.com>
2014-12-04 16:30:52 +0100

    qt5: fix include dir of 'qt5_qpa_nitpicker' library
    Fixes #1315

--- repos/libports/lib/import/import-qt5_qpa_nitpicker.mk
+++ repos/libports/lib/import/import-qt5_qpa_nitpicker.mk
@@ -5,7 +5,7 @@ include $(IMPORT_QT5_INC)
 # include dirs needed for 'qnitpickerplatformwindow.h', which is used by
 # the qt5_qnitpickerviewwidget library
 
-INC_DIR += $(REP_DIR)/src/lib/qt5/qtbase/src/plugins/platforms/nitpicker \
+INC_DIR += $(QT5_REP_DIR)/src/lib/qt5/qtbase/src/plugins/platforms/nitpicker \
            $(QT5_CONTRIB_DIR)/qtbase/src/platformsupport/input/evdevkeyboard \
            $(QT5_CONTRIB_DIR)/qtbase/include/QtGui/$(QT_VERSION)/QtGui \
 
