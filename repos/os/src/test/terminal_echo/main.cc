3fd561aab6bd16f1d67758e0c7a269c8f327f60e
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2014-12-17 16:12:01 +0100

    terminal_echo: Add line feed end carriage return
    Issue #1329

--- repos/os/src/test/terminal_echo/main.cc
+++ repos/os/src/test/terminal_echo/main.cc
@@ -32,7 +32,7 @@ int main(int, char **)
 	terminal.read_avail_sigh(sig_rec.manage(&sig_ctx));
 
 	static const char *intro_text =
-		"--- Terminal echo test started - now you can type characters to be echoed. ---\n";
+		"--- Terminal echo test started - now you can type characters to be echoed. ---\r\n";
 	terminal.write(intro_text, strlen(intro_text) + 1);
 
 	for (;;) {
@@ -44,8 +44,13 @@ int main(int, char **)
 		if (verbose && (num_bytes > 0))
 			PDBG("got %d bytes", num_bytes);
 
-		for (int i = 0; i < num_bytes; i++)
+		for (int i = 0; i < num_bytes; i++) {
+			if (read_buffer[i] == '\r') {
+				terminal.write("\n", 1);
+			}
+
 			terminal.write(&read_buffer[i], 1);
+		}
 	}
 
 	return 0;
