ba54a74947a11df72d757721e98503efa3b9e33c
Emery Hemingway <emery@vfemail.net>
2015-01-07 17:23:42 -0500

    libports: libbz2 (Bzip2)

---
+++ repos/libports/lib/mk/libbz2.mk
@@ -0,0 +1,12 @@
+LIBBZ2_DIR := $(call select_from_ports,bzip2)/src/lib/libbz2
+LIBS       += libc
+
+BIGFILES = -D_FILE_OFFSET_BITS=64
+CC_DEF 	+= -Winline $(BIGFILES)
+
+SRC_C = blocksort.c huffman.c crctable.c randtable.c \
+        compress.c decompress.c bzlib.c
+
+vpath %.c $(LIBBZ2_DIR)
+
+# Bzip2 upstream recommends using a static library.
