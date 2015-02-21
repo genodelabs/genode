2af5aaa54d9c279e5456a6f673699a8ecbecbb0f
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-02-21 01:00:35 +0100

    hw_x86_64: Add Tss class
    The class Genode::Tss represents a 64-bit Task State Segment (TSS) as
    specified by Intel SDM Vol. 3A, section 7.7.
    
    The setup function sets the stack pointers for privilege levels 0-2 to
    the kernel stack address. The load function loads the TSS segment
    selector into the task register.

---
+++ repos/base-hw/src/core/include/spec/x86_64/tss.h
@@ -0,0 +1,53 @@
+#ifndef _TSS_H_
+#define _TSS_H_
+
+#include <base/stdint.h>
+
+namespace Genode
+{
+	/**
+	 * Task State Segment (TSS)
+	 *
+	 * See Intel SDM Vol. 3A, section 7.7
+	 */
+	class Tss;
+}
+
+class Genode::Tss
+{
+	private:
+
+		enum {
+			TSS_SELECTOR = 0x28,
+		};
+
+		uint32_t : 32;
+		addr_t rsp0;
+		addr_t rsp1;
+		addr_t rsp2;
+		uint64_t : 64;
+		addr_t ist[7];
+		uint64_t : 64;
+		uint16_t : 16;
+		uint16_t iomap_base;
+
+		/**
+		 * TSS
+		 */
+		static Tss _tss asm ("_tss");
+
+	public:
+
+		/**
+		 * Setup TSS.
+		 */
+		static void setup();
+
+		/**
+		 * Load TSS into TR.
+		 */
+		static void load();
+
+}__attribute__((packed));
+
+#endif /* _TSS_H_ */
