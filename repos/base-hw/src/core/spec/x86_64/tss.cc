e569a6d1b3fc87e7213659725d4fcf72c41a67c6
Reto Buerki <reet@codelabs.ch>
2015-02-27 13:10:41 +0100

    hw_x86_64: Set interrupt stack in Tss::Setup

--- repos/base-hw/src/core/spec/x86_64/tss.cc
+++ repos/base-hw/src/core/spec/x86_64/tss.cc
@@ -1,14 +1,19 @@
+#include <mtc_util.h>
+
 #include "tss.h"
 
 using namespace Genode;
 
-extern char kernel_stack[];
+extern int _mt_kernel_interrupt_stack;
 
 void Tss::setup(addr_t const virt_base)
 {
-	this->rsp0 = (addr_t)kernel_stack;
-	this->rsp1 = (addr_t)kernel_stack;
-	this->rsp2 = (addr_t)kernel_stack;
+	addr_t const stack_addr = _virt_mtc_addr(virt_base,
+			(addr_t)&_mt_kernel_interrupt_stack);
+
+	this->rsp0 = stack_addr;
+	this->rsp1 = stack_addr;
+	this->rsp2 = stack_addr;
 }
 
 
