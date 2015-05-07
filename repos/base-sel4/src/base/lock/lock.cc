41b99a6b51615651f18c0c216d3d137ed23602b8
Norman Feske <norman.feske@genode-labs.com>
2015-05-07 14:22:42 +0200

    sel4: use yielding spinlock for 'Genode::Lock'

---
+++ repos/base-sel4/src/base/lock/lock.cc
@@ -0,0 +1,44 @@
+/*
+ * \brief  Lock implementation
+ * \author Norman Feske
+ * \date   2007-10-15
+ */
+
+/*
+ * Copyright (C) 2007-2013 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU General Public License version 2.
+ */
+
+/* Genode includes */
+#include <base/cancelable_lock.h>
+#include <cpu/atomic.h>
+#include <cpu/memory_barrier.h>
+
+/* seL4 includes */
+#include <sel4/interfaces/sel4_client.h>
+
+using namespace Genode;
+
+
+Cancelable_lock::Cancelable_lock(Cancelable_lock::State initial)
+: _lock(UNLOCKED)
+{
+	if (initial == LOCKED)
+		lock();
+}
+
+
+void Cancelable_lock::lock()
+{
+	while (!Genode::cmpxchg(&_lock, UNLOCKED, LOCKED))
+		seL4_Yield();
+}
+
+
+void Cancelable_lock::unlock()
+{
+	Genode::memory_barrier();
+	_lock = UNLOCKED;
+}
--- repos/base-sel4/src/base/lock/lock_helper.h
+++ repos/base-sel4/src/base/lock/lock_helper.h
@@ -1,31 +1,21 @@
+
 /*
  * \brief  seL4-specific helper functions for the Lock implementation
  * \author Norman Feske
- * \date   2014-10-14
+ * \date   2015-05-07
+ *
+ * Based on the lock implementation of base-fiasco/src/base/lock/.
  */
 
 /*
- * Copyright (C) 2014 Genode Labs GmbH
+ * Copyright (C) 2015 Genode Labs GmbH
  *
  * This file is part of the Genode OS framework, which is distributed
  * under the terms of the GNU General Public License version 2.
  */
 
-/* Genode includes */
-#include <base/native_types.h>
-#include <base/thread.h>
+/* seL4 includes */
+#include <sel4/interfaces/sel4_client.h>
 
 
-static inline void thread_yield() { }
-
-
-static inline bool thread_check_stopped_and_restart(Genode::Thread_base *)
-{
-	return false;
-}
-
-
-static inline void thread_switch_to(Genode::Thread_base *thread_base) { }
-
-
-static inline void thread_stop_myself() { }
+static inline void thread_yield() { seL4_Yield(); }
