fafa62a1e9d9eb7d29e0856397bd265a422c828a
Josef Söntgen <josef.soentgen@genode-labs.com>
2015-05-08 08:50:22 +0200

    dde_bsd: prefix audio library name
    Issue #1498.

--- repos/dde_bsd/lib/mk/x86_32/dde_bsd_audio.mk
+++ repos/dde_bsd/lib/mk/x86_32/dde_bsd_audio.mk
@@ -1,5 +1,5 @@
 INC_DIR += $(LIB_INC_DIR)/x86_32 $(LIB_INC_DIR)/x86
 
-include $(REP_DIR)/lib/mk/audio.inc
+include $(REP_DIR)/lib/mk/dde_bsd_audio.inc
 
 vpath %.S $(LIB_DIR)/x86_32
--- repos/dde_bsd/src/drivers/audio_out/target.mk
+++ repos/dde_bsd/src/drivers/audio_out/target.mk
@@ -1,5 +1,5 @@
 TARGET   = audio_out_drv
 REQUIRES = x86
 SRC_CC   = main.cc
-LIBS     = audio base config server
+LIBS     = dde_bsd_audio base config server
 INC_DIR += $(REP_DIR)/include
