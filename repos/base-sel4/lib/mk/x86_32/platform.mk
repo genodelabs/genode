51f02340b6582b79585d2aba8a29df7b0cf1a81b
Norman Feske <norman.feske@genode-labs.com>
2015-05-11 19:50:03 +0200

    sel4: avoid superfluous header re-generation

--- repos/base-sel4/lib/mk/x86_32/platform.mk
+++ repos/base-sel4/lib/mk/x86_32/platform.mk
@@ -24,29 +24,24 @@ SEL4_ARCH_INCLUDES := objecttype.h types.h bootinfo.h constants.h functions.h \
                       pfIPC.h syscalls.h exIPC.h invocation.h
 
 SEL4_INCLUDES := objecttype.h types.h bootinfo.h errors.h constants.h \
-                 messages.h sel4.h benchmark.h types.bf macros.h \
+                 messages.h sel4.h benchmark.h macros.h \
                  types_gen.h syscall.h invocation.h
 
 SEL4_INCLUDE_SYMLINKS += $(addprefix $(BUILD_BASE_DIR)/include/sel4/,     $(SEL4_INCLUDES))
 SEL4_INCLUDE_SYMLINKS += $(addprefix $(BUILD_BASE_DIR)/include/sel4/arch/,$(SEL4_ARCH_INCLUDES))
 SEL4_INCLUDE_SYMLINKS += $(BUILD_BASE_DIR)/include/sel4/interfaces/sel4_client.h
 
-SEL4_INCLUDE_DIRS = $(sort $(dir $(SEL4_INCLUDE_SYMLINKS)))
-
 all: $(SEL4_INCLUDE_SYMLINKS)
-
-$(SEL4_INCLUDE_SYMLINKS): $(SEL4_INCLUDE_DIRS)
-
-$(SEL4_INCLUDE_DIRS):
-	$(VERBOSE)mkdir -p $@
 
 #
 # Plain symlinks to existing headers
 #
 $(BUILD_BASE_DIR)/include/sel4/arch/%.h: $(LIBSEL4_DIR)/arch_include/ia32/sel4/arch/%.h
+	$(VERBOSE)mkdir -p $(dir $@)
 	$(VERBOSE)ln -sf $< $@
 
 $(BUILD_BASE_DIR)/include/sel4/%.h: $(LIBSEL4_DIR)/include/sel4/%.h
+	$(VERBOSE)mkdir -p $(dir $@)
 	$(VERBOSE)ln -sf $< $@
 
 #
@@ -54,21 +49,25 @@ $(BUILD_BASE_DIR)/include/sel4/%.h: $(LIBSEL4_DIR)/include/sel4/%.h
 #
 $(BUILD_BASE_DIR)/include/sel4/types_gen.h: $(LIBSEL4_DIR)/include/sel4/types.bf
 	$(MSG_CONVERT)$(notdir $@)
+	$(VERBOSE)mkdir -p $(dir $@)
 	$(VERBOSE)python $(LIBSEL4_DIR)/tools/bitfield_gen.py \
 	                 --environment libsel4 "$<" $@
 
 $(BUILD_BASE_DIR)/include/sel4/syscall.h: $(LIBSEL4_DIR)/include/api/syscall.xml
 	$(MSG_CONVERT)$(notdir $@)
+	$(VERBOSE)mkdir -p $(dir $@)
 	$(VERBOSE)python $(LIBSEL4_DIR)/tools/syscall_header_gen.py \
 	                 --xml $< --libsel4_header $@
 
 $(BUILD_BASE_DIR)/include/sel4/invocation.h: $(LIBSEL4_DIR)/include/interfaces/sel4.xml
 	$(MSG_CONVERT)$(notdir $@)
+	$(VERBOSE)mkdir -p $(dir $@)
 	$(VERBOSE)python $(LIBSEL4_DIR)/tools/invocation_header_gen.py \
 	                 --xml $< --libsel4 --dest $@
 
 $(BUILD_BASE_DIR)/include/sel4/arch/invocation.h: $(LIBSEL4_DIR)/arch_include/ia32/interfaces/sel4arch.xml
 	$(MSG_CONVERT)arch/$(notdir $@)
+	$(VERBOSE)mkdir -p $(dir $@)
 	$(VERBOSE)python $(LIBSEL4_DIR)/tools/invocation_header_gen.py \
 	                 --xml $< --libsel4 --arch --dest $@
 
@@ -77,6 +76,7 @@ SEL4_CLIENT_H_SRC := $(LIBSEL4_DIR)/include/interfaces/sel4.xml \
 
 $(BUILD_BASE_DIR)/include/sel4/interfaces/sel4_client.h: $(SEL4_CLIENT_H_SRC)
 	$(MSG_CONVERT)$(notdir $@)
+	$(VERBOSE)mkdir -p $(dir $@)
 	$(VERBOSE)python $(LIBSEL4_DIR)/tools/syscall_stub_gen.py \
 	                 -a ia32 -o $@ $(SEL4_CLIENT_H_SRC)
 
