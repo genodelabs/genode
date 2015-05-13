691f0eaae7c2428076816b841706982d7a680998
Norman Feske <norman.feske@genode-labs.com>
2015-05-13 11:17:03 +0200

    sel4: link core to different address
    This enables us to debug core and non-core using Qemu's GDB stub.

--- repos/base-sel4/src/core/target.mk
+++ repos/base-sel4/src/core/target.mk
@@ -2,6 +2,8 @@ TARGET = core
 LIBS  += core
 SRC_S  = boot_modules.s
 
+LD_TEXT_ADDR ?= 0x02000000
+
 # XXX hack, based on base-hw/lib/mk/core.mk
 ifneq ($(wildcard $(BUILD_BASE_DIR)/boot_modules.s),)
   BOOT_MODULES_VPATH = $(BUILD_BASE_DIR)
