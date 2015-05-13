d554200e5ae51edfec18f9245fcd068224285ac7
Christian Prochaska <christian.prochaska@genode-labs.com>
2015-05-13 17:56:51 +0200

    x86emu: prevent warnings about uninitialized vars
    Fixes #1522

--- repos/libports/lib/mk/x86emu.mk
+++ repos/libports/lib/mk/x86emu.mk
@@ -4,7 +4,7 @@
 
 X86EMU_DIR  = $(call select_from_ports,x86emu)/src/lib/x86emu/contrib
 INC_DIR    += $(X86EMU_DIR) $(REP_DIR)/include/x86emu
-CC_OPT     += -fomit-frame-pointer
+CC_OPT     += -fomit-frame-pointer -Wno-maybe-uninitialized
 
 SRC_C = decode.c fpu.c ops.c ops2.c prim_ops.c sys.c
 
