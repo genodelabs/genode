cada11b55397f8b42e45cc6369dceb3e91b4fcb1
Norman Feske <norman.feske@genode-labs.com>
2015-05-17 22:35:04 +0200

    Add sel4_x86_32 to create_builddir tool

---
+++ repos/base-sel4/mk/spec-sel4_x86_32.mk
@@ -0,0 +1,8 @@
+#
+# Specifics for the seL4 kernel API x86 32 bit
+#
+
+SPECS += sel4 x86_32
+
+include $(call select_from_repositories,mk/spec-x86_32.mk)
+include $(call select_from_repositories,mk/spec-sel4.mk)
