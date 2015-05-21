12e473b370410e327adb8c5bf021a64767f7e682
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2015-05-21 10:44:17 +0200

    hw_x86: enable building virtualbox

--- repos/ports/lib/mk/spec/linux/virtualbox-hwaccl.mk
+++ repos/ports/lib/mk/spec/linux/virtualbox-hwaccl.mk
@@ -1,8 +1,1 @@
-include $(REP_DIR)/lib/mk/virtualbox-common.inc
-
-SRC_CC = sup.cc
-
-INC_DIR += $(call select_from_repositories,src/lib/libc)
-INC_DIR += $(REP_DIR)/src/virtualbox
-
-vpath sup.cc $(REP_DIR)/src/virtualbox/accloff/
+include $(REP_DIR)/lib/mk/virtualbox-hwaccl-off.inc
