8258fd9dffe21a0e7c57434769f482fc0dbfb9d5
Josef Söntgen <josef.soentgen@genode-labs.com>
2015-08-12 14:21:24 +0200

    Rename audio_out_drv to audio_drv
    With the introducation of the Audio_in session interface it makes
    sense to rename the current available audio drivers. At the moment
    only the dde_bsd audio_drv supports Audio_out as well as Audio_in.
    The Linux audio_drv only supports Audio_out (there is no demand for
    Audio_in support currently) but is renamed nonetheless to make it
    easiert to write generic run scripts.
    
    Issue #1644.

--- repos/dde_bsd/src/drivers/audio/target.mk
+++ repos/dde_bsd/src/drivers/audio/target.mk
@@ -1,5 +1,5 @@
-TARGET   = audio_out_drv
 REQUIRES = x86
+TARGET   = audio_drv
 SRC_CC   = main.cc
 LIBS     = dde_bsd_audio base config server
 INC_DIR += $(REP_DIR)/include
--- repos/os/src/drivers/audio/linux/target.mk
+++ repos/os/src/drivers/audio/linux/target.mk
@@ -1,5 +1,5 @@
 REQUIRES = linux
-TARGET   = audio_out_drv
+TARGET   = audio_drv
 LIBS     = lx_hybrid
 SRC_CC   = main.cc
 SRC_C    = alsa.c
