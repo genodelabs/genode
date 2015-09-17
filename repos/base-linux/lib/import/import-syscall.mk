50164336afbd9918c558cc48883c1b5002ca1373
Christian Helmuth <christian.helmuth@genode-labs.com>
2015-09-17 11:23:19 +0200

    linux: explicitly add well-known include paths
    Unfortunately, the 'gcc -dumpmachine' magic does not suffice on all
    Linux distros, so we just add the well-known paths for i386-linux-gnu
    and x86_64-linux-gnu.
    
    Fixes #1694

--- repos/base-linux/lib/import/import-syscall.mk
+++ repos/base-linux/lib/import/import-syscall.mk
@@ -3,7 +3,13 @@ HOST_INC_DIR += /usr/include
 
 # needed for Ubuntu >= 11.04
 HOST_INC_DIR += /usr/include/$(shell gcc -dumpmachine)
+
+#
+# Explicitly add some well-known paths as the dumpmachine magic above does not
+# suffice on all Linux distros (e.g., Debian Stretch).
+#
 HOST_INC_DIR += /usr/include/i386-linux-gnu
+HOST_INC_DIR += /usr/include/x86_64-linux-gnu
 
 #
 # Some header files installed on GNU/Linux test for the GNU compiler. For
