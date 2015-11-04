83e5a648642954c56fc9c5542dd54c2cb00e0d2e
Josef Söntgen <josef.soentgen@genode-labs.com>
2015-11-04 17:49:58 +0100

    os: extend Audio_out with queue query method
    Issue #1767.

--- repos/os/include/audio_out_session/audio_out_session.h
+++ repos/os/include/audio_out_session/audio_out_session.h
@@ -178,6 +178,21 @@ class Audio_out::Stream
 		unsigned tail() const { return _tail; }
 
 		/**
+		 * Number of packets between playback and allocation position
+		 *
+		 * \return number
+		 */
+		unsigned queued() const
+		{
+			if (_tail > _pos)
+				return _tail - _pos;
+			else if (_pos > _tail)
+				return QUEUE_SIZE - (_pos - _tail);
+			else
+				return 0;
+		}
+
+		/**
 		 * Retrieve next packet for given packet
 		 *
 		 * \param packet  preceding packet
