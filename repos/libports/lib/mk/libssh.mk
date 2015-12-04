48738bbf6397366e818f0439e317dddc9c6aab58
Josef Söntgen <jws@sysret.de>
2015-12-04 21:45:35 +0100

    libports: update libssh to 0.7.2
    Fixes #1811.

--- repos/libports/lib/mk/libssh.mk
+++ repos/libports/lib/mk/libssh.mk
@@ -4,42 +4,58 @@ SRC_C = \
         agent.c \
         auth.c \
         base64.c \
+        bignum.c \
+        bind.c \
         buffer.c \
         callbacks.c \
         channels.c \
         client.c \
         config.c \
         connect.c \
-        crc32.c \
-        crypt.c \
+        curve25519.c \
         dh.c \
+        ecdh.c \
         error.c \
         getpass.c \
-        gcrypt_missing.c \
         gzip.c \
         init.c \
         kex.c \
-        keyfiles.c \
-        keys.c \
         known_hosts.c \
         legacy.c \
         libcrypto.c \
-        libgcrypt.c \
         log.c \
         match.c \
         messages.c \
         misc.c \
         options.c \
         packet.c \
+        packet_cb.c \
+        packet_crypt.c \
         pcap.c \
         pki.c \
+        pki_container_openssh.c \
+        pki_crypto.c \
+        pki_ed25519.c \
         poll.c \
-        session.c \
         scp.c \
+        server.c \
+        session.c \
+        sftp.c \
+        sftpserver.c \
         socket.c \
         string.c \
         threads.c \
         wrapper.c
+
+# external/
+SRC_C += \
+         bcrypt_pbkdf.c \
+         blowfish.c \
+         curve25519_ref.c \
+         ed25519.c \
+         fe25519.c \
+         ge25519.c \
+         sc25519.c
 
 INC_DIR += $(LIBSSH_PORT_DIR)/include
 INC_DIR += $(REP_DIR)/src/lib/libssh
@@ -51,3 +67,4 @@ LIBS += libc zlib libcrypto
 SHARED_LIB = yes
 
 vpath %.c $(LIBSSH_PORT_DIR)/src/lib/libssh/src
+vpath %.c $(LIBSSH_PORT_DIR)/src/lib/libssh/src/external
--- repos/libports/src/lib/libssh/config.h
+++ repos/libports/src/lib/libssh/config.h
@@ -2,7 +2,7 @@
 #define PACKAGE "libssh"
 
 /* Version number of package */
-#define VERSION "0.5.2"
+#define VERSION "0.7.2"
 
 /* #undef LOCALEDIR */
 #define DATADIR "/share/libssh"
@@ -15,11 +15,29 @@
 /* Define to 1 if you have the <argp.h> header file. */
 #define HAVE_ARGP_H 1
 
+/* Define to 1 if you have the <aprpa/inet.h> header file. */
+#define HAVE_ARPA_INET_H 1
+
 /* Define to 1 if you have the <pty.h> header file. */
 #define HAVE_PTY_H 1
 
+/* Define to 1 if you have the <utmp.h> header file. */
+#define HAVE_UTMP_H 1
+
+/* Define to 1 if you have the <util.h> header file. */
+/* #undef HAVE_UTIL_H */
+
+/* Define to 1 if you have the <libutil.h> header file. */
+/* #undef HAVE_LIBUTIL_H */
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
 /* Define to 1 if you have the <termios.h> header file. */
 #define HAVE_TERMIOS_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
 
 /* Define to 1 if you have the <openssl/aes.h> header file. */
 #define HAVE_OPENSSL_AES_H 1
@@ -33,9 +51,26 @@
 /* Define to 1 if you have the <openssl/des.h> header file. */
 #define HAVE_OPENSSL_DES_H 1
 
+/* Define to 1 if you have the <openssl/ecdh.h> header file. */
+#define HAVE_OPENSSL_ECDH_H 1
+
+/* Define to 1 if you have the <openssl/ec.h> header file. */
+#define HAVE_OPENSSL_EC_H 1
+
+/* Define to 1 if you have the <openssl/ecdsa.h> header file. */
+#define HAVE_OPENSSL_ECDSA_H 1
+
 /* Define to 1 if you have the <pthread.h> header file. */
 /* #undef HAVE_PTHREAD_H */
 
+/* Define to 1 if you have eliptic curve cryptography in openssl */
+#define HAVE_OPENSSL_ECC 1
+
+/* Define to 1 if you have eliptic curve cryptography in gcrypt */
+/* #undef HAVE_GCRYPT_ECC */
+
+/* Define to 1 if you have eliptic curve cryptography */
+#define HAVE_ECC 1
 
 /*************************** FUNCTIONS ***************************/
 
@@ -57,29 +92,41 @@
 /* Define to 1 if you have the `_vsnprintf_s' function. */
 /* #undef HAVE__VSNPRINTF_S */
 
+/* Define to 1 if you have the `isblank' function. */
+#define HAVE_ISBLANK 1
+
 /* Define to 1 if you have the `strncpy' function. */
 #define HAVE_STRNCPY 1
 
 /* Define to 1 if you have the `cfmakeraw' function. */
-/* #unfdef HAVE_CFMAKERAW */
+/* #undef HAVE_CFMAKERAW */
 
 /* Define to 1 if you have the `getaddrinfo' function. */
 #define HAVE_GETADDRINFO 1
 
 /* Define to 1 if you have the `poll' function. */
-/* #undef HAVE_POLL */
+#define HAVE_POLL 1
 
 /* Define to 1 if you have the `select' function. */
 #define HAVE_SELECT 1
-
-/* Define to 1 if you have the `regcomp' function. */
-#define HAVE_REGCOMP 1
 
 /* Define to 1 if you have the `clock_gettime' function. */
 #define HAVE_CLOCK_GETTIME 1
 
 /* Define to 1 if you have the `ntohll' function. */
 /* #undef HAVE_NTOHLL */
+
+/* Define to 1 if you have the `htonll' function. */
+/* #undef HAVE_HTONLL */
+
+/* Define to 1 if you have the `strtoull' function. */
+#define HAVE_STRTOULL 1
+
+/* Define to 1 if you have the `__strtoull' function. */
+/* #undef HAVE___STRTOULL */
+
+/* Define to 1 if you have the `_strtoui64' function. */
+/* #undef HAVE__STRTOUI64 */
 
 /*************************** LIBRARIES ***************************/
 
@@ -89,17 +136,25 @@
 /* Define to 1 if you have the `gcrypt' library (-lgcrypt). */
 /* #undef HAVE_LIBGCRYPT */
 
-/* Define to 1 if you have the `z' library (-lz). */
-#define HAVE_LIBZ 1
-
 /* Define to 1 if you have the `pthread' library (-lpthread). */
 /* #undef HAVE_PTHREAD */
 
-
 /**************************** OPTIONS ****************************/
 
+/* #undef HAVE_GCC_THREAD_LOCAL_STORAGE */
+/* #undef HAVE_MSC_THREAD_LOCAL_STORAGE */
+
+#define HAVE_GCC_VOLATILE_MEMORY_PROTECTION 1
+#define HAVE_GCC_NARG_MACRO 1
+
+#define HAVE_COMPILER__FUNC__ 1
+#define HAVE_COMPILER__FUNCTION__ 1
+
+/* Define to 1 if you want to enable GSSAPI */
+/* #undef WITH_GSSAPI */
+
 /* Define to 1 if you want to enable ZLIB */
-#define WITH_LIBZ 1
+#define WITH_ZLIB 1
 
 /* Define to 1 if you want to enable SFTP */
 #define WITH_SFTP 1
@@ -108,7 +163,7 @@
 /* #undef WITH_SSH1 */
 
 /* Define to 1 if you want to enable server support */
-/* #undef WITH_SERVER */
+#define WITH_SERVER 1
 
 /* Define to 1 if you want to enable debug output for crypto functions */
 /* #undef DEBUG_CRYPTO */
@@ -117,7 +172,10 @@
 /* #undef WITH_PCAP */
 
 /* Define to 1 if you want to enable calltrace debug output */
-#define DEBUG_CALLTRACE 1
+/* #undef DEBUG_CALLTRACE */
+
+/* Define to 1 if you want to enable NaCl support */
+/* #undef WITH_NACL */
 
 /*************************** ENDIAN *****************************/
 
