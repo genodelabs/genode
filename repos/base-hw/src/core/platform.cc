5afad64586111f3c5794e260a61c95b910bd1710
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2016-01-15 15:47:34 +0100

    hw: preserve core's context-area (Fix #1853)
    Remove core's context-area from its virtual memory allocator

--- repos/base-hw/src/core/platform.cc
+++ repos/base-hw/src/core/platform.cc
@@ -136,6 +136,11 @@ Platform::Platform()
 	init_alloc(_core_mem_alloc.virt_alloc(), virt_region,
 	           _core_only_ram_regions, get_page_size_log2());
 
+	/* preserve context area in core's virtual address space */
+	_core_mem_alloc.virt_alloc()->remove_range(
+		Native_config::context_area_virtual_base(),
+		Native_config::context_area_virtual_size());
+
 	_init_io_port_alloc();
 
 	/* make all non-kernel interrupts available to the interrupt allocator */
