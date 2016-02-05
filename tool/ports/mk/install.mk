992a5ec1ae7fb57e1b2df04a9b372c81debc9748
Martin Stein <martin.stein@genode-labs.com>
2016-02-05 11:28:44 +0100

    prepare_port: prefix git clone output
    To raise readability when preparing multiple ports in parallel we prefix
    also the git clone output with the port name dark-yellow-coloured. To
    achieve this we sed the git output. In sed \x1b[ resolves to an escape
    sequence and \033[, that we use normally, doesn't. The echo command, at
    the other hand, resolves both to an escape sequence. Thus we use the
    sed-compatible version in general. This commit inhibits the progress
    output of git clone as it can't be redirected to sed.
    
    Ref #1872

--- tool/ports/mk/install.mk
+++ tool/ports/mk/install.mk
@@ -160,7 +160,7 @@ _git_dir = $(call _assert,$(DIR($1)),Missing declaration of DIR($*))
 		($(ECHO) "Error: Undefined URL for $*"; false);
 	$(VERBOSE)dir=$(call _git_dir,$*);\
 		test -d $$dir || $(MSG_DOWNLOAD)$(URL($*)); \
-		test -d $$dir || git clone $(URL($*)) $$dir; \
+		test -d $$dir || git clone $(URL($*)) $$dir &> >(sed 's/^/$(MSG_GIT)/'); \
 		$(MSG_UPDATE)$$dir; \
 		cd $$dir && git fetch && git reset -q --hard HEAD && git checkout -q $(REV($*))
 
