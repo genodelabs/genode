234d8cca5a4af2e5b2faa2d715cc53301c135225
Reto Buerki <reet@codelabs.ch>
2016-02-16 17:08:14 +0100

    hw_x86_64_muen: Extend sinfo API with device info
    The new Sinfo::get_dev_info function can be used to retrieve information
    for a PCI device with given source-id (SID). The function returns false
    if no device information for the specified device exists.

--- repos/base-hw/src/core/spec/x86_64/muen/musinfo.h
+++ repos/base-hw/src/core/spec/x86_64/muen/musinfo.h
@@ -42,18 +42,31 @@ struct resource_type {
 	char padding[6];
 } __attribute__((packed, aligned (8)));
 
+struct dev_info_type {
+	uint16_t sid;
+	uint16_t irte_start;
+	uint8_t irq_start;
+	uint8_t ir_count;
+	uint8_t flags;
+	char padding[1];
+} __attribute__((packed, aligned (8)));
+
+#define DEV_MSI_FLAG  (1 << 0)
+
 struct subject_info_type {
 	uint64_t magic;
 	uint8_t resource_count;
 	uint8_t memregion_count;
 	uint8_t channel_info_count;
-	char padding[5];
+	uint8_t dev_info_count;
+	char padding[4];
 	uint64_t tsc_khz;
 	uint64_t tsc_schedule_start;
 	uint64_t tsc_schedule_end;
 	struct resource_type resources[MAX_RESOURCE_COUNT];
 	struct memregion_type memregions[MAX_RESOURCE_COUNT];
 	struct channel_info_type channels_info[MAX_RESOURCE_COUNT];
+	struct dev_info_type dev_info[MAX_RESOURCE_COUNT];
 } __attribute__((packed, aligned (8)));
 
 #endif /* MUSINFO_H_  */
