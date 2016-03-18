5833743b9e64a14013102558e3e175cfcde32316
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2016-03-18 19:41:00 +0100

    net: add write accessors to Ipv4_packet

--- repos/os/include/net/ipv4.h
+++ repos/os/include/net/ipv4.h
@@ -60,6 +60,8 @@ class Net::Ipv4_packet
 
 		static Ipv4_address ip_from_string(const char *ip);
 
+		static Genode::uint16_t calculate_checksum(const Ipv4_packet &packet);
+
 	private:
 
 		/************************
@@ -157,6 +159,20 @@ class Net::Ipv4_packet
 
 		void *data() { return &_data; }
 
+		/********************************
+		 ** IPv4 field write-accessors **
+		 ********************************/
+
+		void version(Genode::size_t version)    { _version = version; }
+		void header_length(Genode::size_t len)  { _header_length = len; }
+
+		void total_length(Genode::uint16_t len) { _total_length = host_to_big_endian(len); }
+		void time_to_live(Genode::uint8_t ttl)  { _time_to_live = ttl; }
+
+		void checksum(Genode::uint16_t checksum) { _header_checksum = host_to_big_endian(checksum); }
+
+		void dst(Ipv4_address ip) { ip.copy(&_dst_addr); }
+		void src(Ipv4_address ip) { ip.copy(&_src_addr); }
 
 		/***************
 		 ** Operators **
