2127c8acf2d9fdc72a0b73468dfedaee26e0081d
Norman Feske <norman.feske@genode-labs.com>
2016-07-13 14:14:55 +0200

    arm: prevent 64K default alignment of text segment

--- repos/base/mk/spec/arm.mk
+++ repos/base/mk/spec/arm.mk
@@ -11,4 +11,12 @@ SPECS += 32bit
 #
 CC_OPT += -Wno-psabi
 
+#
+# By default, the linker produces ELF binaries where the test segment is
+# aligned to the largest page size. On ARM this is 64K. As a result, the
+# produced binaries contain almost 64K zeros preceding the text segment.
+# The patch enforces the text segment to be aligned to 4K instead.
+#
+LD_MARCH += -z max-page-size=0x1000
+
 include $(call select_from_repositories,mk/spec/32bit.mk)
