d354f7a8b905cde74d405451a68966db29ad64a5
Emery Hemingway <emery@vfemail.net>
2016-08-16 18:40:19 +0200

    server/vfs: upgrade logging to new API
    Issue #1987

--- repos/os/src/server/vfs/assert.h
+++ repos/os/src/server/vfs/assert.h
@@ -18,12 +18,6 @@
 #include <vfs/directory_service.h>
 #include <file_system_session/file_system_session.h>
 
-static bool const verbose = false;
-#define PDBGV(...) if (verbose) PDBG(__VA_ARGS__)
-#define PERRV(...) if (verbose) PERR(__VA_ARGS__)
-
-#define PERR_THROW(e) PERRV("%s - %s", __func__, #e); throw e();
-
 namespace File_system {
 
 	using namespace Vfs;
@@ -33,11 +27,11 @@ namespace File_system {
 		typedef Directory_service::Mkdir_result Result;
 
 		switch (r) {
-		case Result::MKDIR_ERR_NAME_TOO_LONG: PERR_THROW(Name_too_long);
-		case Result::MKDIR_ERR_NO_ENTRY:      PERR_THROW(Lookup_failed);
-		case Result::MKDIR_ERR_NO_SPACE:      PERR_THROW(No_space);
-		case Result::MKDIR_ERR_NO_PERM:       PERR_THROW(Permission_denied);
-		case Result::MKDIR_ERR_EXISTS:        PERR_THROW(Node_already_exists);
+		case Result::MKDIR_ERR_NAME_TOO_LONG: throw Name_too_long();
+		case Result::MKDIR_ERR_NO_ENTRY:      throw Lookup_failed();
+		case Result::MKDIR_ERR_NO_SPACE:      throw No_space();
+		case Result::MKDIR_ERR_NO_PERM:       throw Permission_denied();
+		case Result::MKDIR_ERR_EXISTS:        throw Node_already_exists();
 		case Result::MKDIR_OK: break;
 		}
 	}
@@ -47,11 +41,11 @@ namespace File_system {
 		typedef Directory_service::Open_result Result;
 
 		switch (r) {
-		case Result::OPEN_ERR_NAME_TOO_LONG: PERR_THROW(Invalid_name);
-		case Result::OPEN_ERR_UNACCESSIBLE:  PERR_THROW(Lookup_failed);
-		case Result::OPEN_ERR_NO_SPACE:      PERR_THROW(No_space);
-		case Result::OPEN_ERR_NO_PERM:       PERR_THROW(Permission_denied);
-		case Result::OPEN_ERR_EXISTS:        PERR_THROW(Node_already_exists);
+		case Result::OPEN_ERR_NAME_TOO_LONG: throw Invalid_name();
+		case Result::OPEN_ERR_UNACCESSIBLE:  throw Lookup_failed();
+		case Result::OPEN_ERR_NO_SPACE:      throw No_space();
+		case Result::OPEN_ERR_NO_PERM:       throw Permission_denied();
+		case Result::OPEN_ERR_EXISTS:        throw Node_already_exists();
 		case Result::OPEN_OK: break;
 		}
 	}
@@ -61,11 +55,11 @@ namespace File_system {
 		typedef Directory_service::Symlink_result Result;
 
 		switch (r) {
-		case Result::SYMLINK_ERR_NAME_TOO_LONG: PERR_THROW(Invalid_name);
-		case Result::SYMLINK_ERR_NO_ENTRY:      PERR_THROW(Lookup_failed);
-		case Result::SYMLINK_ERR_NO_SPACE:      PERR_THROW(No_space);
-		case Result::SYMLINK_ERR_NO_PERM:       PERR_THROW(Permission_denied);
-		case Result::SYMLINK_ERR_EXISTS:        PERR_THROW(Node_already_exists);
+		case Result::SYMLINK_ERR_NAME_TOO_LONG: throw Invalid_name();
+		case Result::SYMLINK_ERR_NO_ENTRY:      throw Lookup_failed();
+		case Result::SYMLINK_ERR_NO_SPACE:      throw No_space();
+		case Result::SYMLINK_ERR_NO_PERM:       throw Permission_denied();
+		case Result::SYMLINK_ERR_EXISTS:        throw Node_already_exists();
 		case Result::SYMLINK_OK: break;
 		}
 	}
@@ -75,8 +69,8 @@ namespace File_system {
 		typedef Directory_service::Readlink_result Result;
 
 		switch (r) {
-		case Result::READLINK_ERR_NO_ENTRY: PERR_THROW(Lookup_failed);
-		case Result::READLINK_ERR_NO_PERM:  PERR_THROW(Permission_denied);
+		case Result::READLINK_ERR_NO_ENTRY: throw Lookup_failed();
+		case Result::READLINK_ERR_NO_PERM:  throw Permission_denied();
 		case Result::READLINK_OK: break;
 		}
 	}
@@ -86,9 +80,9 @@ namespace File_system {
 		typedef File_io_service::Ftruncate_result Result;
 
 		switch (r) {
-		case Result::FTRUNCATE_ERR_INTERRUPT: PERR_THROW(Invalid_handle);
-		case Result::FTRUNCATE_ERR_NO_SPACE:  PERR_THROW(No_space);
-		case Result::FTRUNCATE_ERR_NO_PERM:   PERR_THROW(Permission_denied);
+		case Result::FTRUNCATE_ERR_INTERRUPT: throw Invalid_handle();
+		case Result::FTRUNCATE_ERR_NO_SPACE:  throw No_space();
+		case Result::FTRUNCATE_ERR_NO_PERM:   throw Permission_denied();
 		case Result::FTRUNCATE_OK: break;
 		}
 	}
@@ -97,9 +91,9 @@ namespace File_system {
 	{
 		typedef Directory_service::Unlink_result Result;
 		switch (r) {
-		case Result::UNLINK_ERR_NO_ENTRY:  PERR_THROW(Lookup_failed);
-		case Result::UNLINK_ERR_NO_PERM:   PERR_THROW(Permission_denied);
-		case Result::UNLINK_ERR_NOT_EMPTY: PERR_THROW(Not_empty);
+		case Result::UNLINK_ERR_NO_ENTRY:  throw Lookup_failed();
+		case Result::UNLINK_ERR_NO_PERM:   throw Permission_denied();
+		case Result::UNLINK_ERR_NOT_EMPTY: throw Not_empty();
 		case Result::UNLINK_OK: break;
 		}
 	}
@@ -108,8 +102,8 @@ namespace File_system {
 	{
 		typedef Directory_service::Stat_result Result;
 		switch (r) {
-		case Result::STAT_ERR_NO_ENTRY: PERR_THROW(Lookup_failed);
-		case Result::STAT_ERR_NO_PERM:  PERR_THROW(Permission_denied);
+		case Result::STAT_ERR_NO_ENTRY: throw Lookup_failed();
+		case Result::STAT_ERR_NO_PERM:  throw Permission_denied();
 		case Result::STAT_OK: break;
 		}
 	}
@@ -118,9 +112,9 @@ namespace File_system {
 	{
 		typedef Directory_service::Rename_result Result;
 		switch (r) {
-		case Result::RENAME_ERR_NO_ENTRY: PERR_THROW(Lookup_failed);
-		case Result::RENAME_ERR_CROSS_FS: PERR_THROW(Permission_denied);
-		case Result::RENAME_ERR_NO_PERM:  PERR_THROW(Permission_denied);
+		case Result::RENAME_ERR_NO_ENTRY: throw Lookup_failed();
+		case Result::RENAME_ERR_CROSS_FS: throw Permission_denied();
+		case Result::RENAME_ERR_NO_PERM:  throw Permission_denied();
 		case Result::RENAME_OK: break;
 		}
 	}
