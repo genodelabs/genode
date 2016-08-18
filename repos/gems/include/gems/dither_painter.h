31850ee2e11876a9959a626bea95738d47652541
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2016-08-18 12:43:30 +0200

    gems: extend dither_painter to be used with vbox5
    Issue #2059

--- repos/gems/include/gems/dither_painter.h
+++ repos/gems/include/gems/dither_painter.h
@@ -26,50 +26,73 @@ struct Dither_painter
 	 * Surface and texture must have the same size
 	 */
 	template <typename DST_PT, typename SRC_PT>
-	static inline void paint(Genode::Surface<DST_PT>       &surface,
-	                         Genode::Texture<SRC_PT> const &texture)
+	static inline void paint(Genode::Surface<DST_PT>           &surface,
+	                         Genode::Texture<SRC_PT>     const &texture,
+	                         Genode::Surface_base::Point const &pos)
 	{
-		if (surface.size() != texture.size()) return;
-
 		Genode::Surface_base::Rect const clipped = surface.clip();
 
 		if (!clipped.valid()) return;
 
-		unsigned const offset = surface.size().w()*clipped.y1() + clipped.x1();
+		using Genode::min;
+		using Genode::max;
 
-		DST_PT              *dst,       *dst_line       = surface.addr()  + offset;
-		SRC_PT        const *src_pixel, *src_pixel_line = texture.pixel() + offset;
-		unsigned char const *src_alpha, *src_alpha_line = texture.alpha() + offset;
+		unsigned const dst_x = max(pos.x(), clipped.x1());
+		unsigned const dst_y = max(pos.y(), clipped.y1());
 
-		unsigned const line_len = surface.size().w();
+		unsigned const dst_line_len = surface.size().w();
+		unsigned const dst_offset = dst_line_len*dst_y + dst_x;
 
-		for (int y = clipped.y1(), h = clipped.h() ; h--; y++) {
+		unsigned const src_line_len = texture.size().w();
+		unsigned const src_offset = src_line_len*clipped.y1() + clipped.x1();
+
+		DST_PT              *dst,       *dst_line       = surface.addr()  + dst_offset;
+		SRC_PT        const *src_pixel, *src_pixel_line = texture.pixel() + src_offset;
+		unsigned char const *src_alpha, *src_alpha_line = texture.alpha() + src_offset;
+		bool          const  src_has_alpha = texture.alpha() != nullptr;
+
+		unsigned const x_max = min((unsigned)clipped.x2(), dst_x + texture.size().w() - 1);
+		unsigned const y_max = min((unsigned)clipped.y2(), dst_y + texture.size().h() - 1);
+
+		for (unsigned y = dst_y; y <= y_max; y++) {
 
 			src_pixel = src_pixel_line;
 			src_alpha = src_alpha_line;
 			dst       = dst_line;
 
-			for (int x = clipped.x1(), w = clipped.w(); w--; x++) {
+			if (src_has_alpha) {
+				for (unsigned x = dst_x; x <= x_max; x++) {
 
-				int const v = Genode::Dither_matrix::value(x, y) >> 4;
+					int const v = Genode::Dither_matrix::value(x, y) >> 4;
 
-				SRC_PT        const pixel = *src_pixel++;
-				unsigned char const alpha = *src_alpha++;
+					SRC_PT        const pixel = *src_pixel++;
+					unsigned char const alpha = *src_alpha++;
 
-				int const r = pixel.r() - v;
-				int const g = pixel.g() - v;
-				int const b = pixel.b() - v;
-				int const a = alpha ? (int)alpha - v : 0;
+					int const r = pixel.r() - v;
+					int const g = pixel.g() - v;
+					int const b = pixel.b() - v;
+					int const a = alpha ? (int)alpha - v : 0;
 
-				using Genode::min;
-				using Genode::max;
+					*dst++ = DST_PT(max(0, r), max(0, g), max(0, b), max(0, a));
+				}
+			} else {
+				for (unsigned x = dst_x; x <= x_max; x++) {
 
-				*dst++ = DST_PT(max(0, r), max(0, g), max(0, b), max(0, a));
+					int const v = Genode::Dither_matrix::value(x, y) >> 4;
+
+					SRC_PT const pixel = *src_pixel++;
+
+					int const r = pixel.r() - v;
+					int const g = pixel.g() - v;
+					int const b = pixel.b() - v;
+
+					*dst++ = DST_PT(max(0, r), max(0, g), max(0, b));
+				}
 			}
 
-			src_pixel_line += line_len;
-			src_alpha_line += line_len;
-			dst_line       += line_len;
+			src_pixel_line += src_line_len;
+			src_alpha_line += src_line_len;
+			dst_line       += dst_line_len;
 		}
 	}
 };
