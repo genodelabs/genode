00c518b55eee3a3e8bd6c19c44a28dc8d648e5aa
Martin Stein <martin.stein@genode-labs.com>
2016-09-07 15:12:53 +0200

    hw_rpi: remove debug code from USB IRQ handling
    Fixes #2088

--- repos/base-hw/src/core/include/spec/rpi/pic.h
+++ repos/base-hw/src/core/include/spec/rpi/pic.h
@@ -98,32 +98,19 @@ class Genode::Usb_dwc_otg : Mmio
 			if (!_is_sof())
 				return false;
 
-			static int cnt, stat_cnt, filter_cnt, trigger_cnt, kick_cnt;
+			static int cnt = 0;
 
-			stat_cnt++;
-			if (stat_cnt == 8000) {
-				log("kicked: ", kick_cnt, " filtered: ", filter_cnt, " triggered: ", trigger_cnt);
-				stat_cnt = 0;
-			}
-
-			cnt++;
-			if (cnt == 8*20) {
+			if (++cnt == 8*20) {
 				cnt = 0;
 				return false;
 			}
 
-			if (read<Guid::Kick>())
-				kick_cnt++;
-
 			if (!read<Guid::Num_valid>() || read<Guid::Kick>())
 				return false;
 
-			if (_need_trigger_sof(read<Host_frame_number::Num>(), read<Guid::Num>())) {
-				trigger_cnt++;
+			if (_need_trigger_sof(read<Host_frame_number::Num>(),
+			                      read<Guid::Num>()))
 				return false;
-			}
-
-			filter_cnt++;
 
 			write<Core_irq_status::Sof>(1);
 
