3c8d31f8fbe4a0d91b93e9e1b905368ac7da1d9c
Josef Söntgen <josef.soentgen@genode-labs.com>
2016-09-16 14:16:45 +0200

    net: fix mac_from_string
    The MAC tokens are given in hex so treat them as such.
    
    Fixes #2107.

--- repos/os/src/lib/net/mac_address.cc
+++ repos/os/src/lib/net/mac_address.cc
@@ -22,29 +22,29 @@ using namespace Genode;
 struct Scanner_policy_number
 {
 	static bool identifier_char(char c, unsigned  i ) {
-		return is_digit(c) && c !=':'; }
+		return is_digit(c, true) && c !=':'; }
 };
 
-typedef Token<Scanner_policy_number> Token;
+typedef Token<Scanner_policy_number> Mac_token;
 
 
 Mac_address Net::mac_from_string(const char * mac)
 {
 	Mac_address   mac_addr;
-	Token         t(mac);
+	Mac_token     t(mac);
 	char          tmpstr[3];
 	int           cnt = 0;
 	unsigned char ipb[6] = {0};
 
-	while(t) {
-		if (t.type() == Token::WHITESPACE || t[0] == ':') {
+	while (t) {
+		if (t.type() == Mac_token::WHITESPACE || t[0] == ':') {
 			t = t.next();
 			continue;
 		}
 		t.string(tmpstr, sizeof(tmpstr));
 
 		unsigned long tmpc = 0;
-		ascii_to(tmpstr, tmpc);
+		ascii_to_unsigned(tmpstr, tmpc, 16);
 		ipb[cnt] = tmpc & 0xFF;
 		t = t.next();
 
