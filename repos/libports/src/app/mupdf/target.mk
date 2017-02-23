993a48e573b14770ce1dd9c9376145e72739888d
Josef Söntgen <josef.soentgen@genode-labs.com>
2017-02-23 15:37:06 +0100

    mupdf: remove env deprecated warnings
    Issue #2280.

--- repos/libports/src/app/mupdf/target.mk
+++ repos/libports/src/app/mupdf/target.mk
@@ -2,7 +2,7 @@ MUPDF_DIR := $(call select_from_ports,mupdf)/src/lib/mupdf
 TARGET    := mupdf
 SRC_C     := pdfapp.c
 SRC_CC    := main.cc
-LIBS      := posix mupdf
+LIBS      := libc mupdf
 INC_DIR   += $(MUPDF_DIR)/apps
 
 vpath pdfapp.c $(MUPDF_DIR)/apps
