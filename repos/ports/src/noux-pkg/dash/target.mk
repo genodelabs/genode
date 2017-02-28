8adaeeb8dae3b16308b5b91de7c4f9d50b05d8f1
Christian Helmuth <christian.helmuth@genode-labs.com>
2017-02-28 12:30:50 +0100

    Remove port of the DASH shell
    It seems the download site vanished. Also, the utility remained unused
    since years now.

--- repos/ports/src/noux-pkg/dash/target.mk
+++
@@ -1,8 +0,0 @@
-#
-# Prevent double definition of BSD. This definition is specified at the
-# compiler command line, but also in the 'sys/param.h' header of the FreeBSD
-# libc.
-#
-CFLAGS = -UBSD
-
-include $(REP_DIR)/mk/noux.mk
