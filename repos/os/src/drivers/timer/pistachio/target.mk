c1f4dad811717ef4a7ebbf370ee75f1886d5ae0a
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2017-03-08 16:51:00 +0100

    base-pistachio: use PIT in Genode timer
    on x86_32
    
    issue #2308

--- repos/os/src/drivers/timer/pistachio/target.mk
+++
@@ -1,8 +0,0 @@
-TARGET   = pistachio_timer_drv
-REQUIRES = pistachio
-INC_DIR += $(REP_DIR)/src/drivers/timer/include
-INC_DIR += $(REP_DIR)/src/drivers/timer/periodic
-SRC_CC  += periodic/time_source.cc pistachio/time_source.cc main.cc
-LIBS    += syscall-pistachio base-pistachio timeout
-
-vpath %.cc $(REP_DIR)/src/drivers/timer
