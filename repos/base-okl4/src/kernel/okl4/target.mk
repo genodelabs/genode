9afe248e4948b481c3ae880bb137d0fa1c1a87f4
Norman Feske <norman.feske@genode-labs.com>
2017-04-06 21:21:10 +0200

    fixup "mk: strip binaries at <build-dir>/bin/" (hook to bypass strip, needed for okl4)

--- repos/base-okl4/src/kernel/okl4/target.mk
+++ repos/base-okl4/src/kernel/okl4/target.mk
@@ -2,10 +2,17 @@ TARGET = okl4
 LIBS   = kernel-okl4
 SRC_C  = dummy.c
 
+#
+# Prevent the kernel binary from being stripped. Otherwise, elfweaver would
+# complain with the following error:
+#
+# Error: Symbol "tcb_size" not found in kernel ELF file.  Needed for XIP support.
+#
+STRIP_TARGET_CMD = cp -f $< $@
+
 LD_TEXT_ADDR    := 0xf0100000
 LD_SCRIPT_STATIC = $(REP_DIR)/contrib/generated/x86/linker.ld
 
 $(TARGET): dummy.c
-
 dummy.c:
 	@touch $@
