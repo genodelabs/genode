962c853e6799c2db7ff374c2af7987eeef788172
Norman Feske <norman.feske@genode-labs.com>
2017-04-06 19:36:58 +0200

    hw: rename bootstrap.o to bootstrap-hw.o
    In order to deliver base-hw as a binary archive, we need to install the
    bootstap.o file to bin/. Since bin/ is a global name space shared by all
    kernels, this patch renames the object file to bootstap-hw.o and thereby
    clarifies the association of the file with base-hw.

--- repos/base-hw/src/bootstrap/hw/target.mk
+++ repos/base-hw/src/bootstrap/hw/target.mk
@@ -1,9 +1,21 @@
 TARGET        = bootstrap
 LIBS          = bootstrap-hw
-BOOTSTRAP_OBJ = bootstrap.o
+BOOTSTRAP_OBJ = bootstrap-hw.o
 
-$(TARGET): $(BOOTSTRAP_OBJ)
+$(TARGET):
 	$(VERBOSE)true
+
+ifneq ($(INSTALL_DIR),)
+ifneq ($(DEBUG_DIR),)
+$(TARGET): $(INSTALL_DIR)/$(BOOTSTRAP_OBJ)
+
+$(INSTALL_DIR)/$(BOOTSTRAP_OBJ) : $(DEBUG_DIR)/$(BOOTSTRAP_OBJ)
+	$(VERBOSE)$(STRIP) --strip-debug -o $@ $<
+
+$(DEBUG_DIR)/$(BOOTSTRAP_OBJ) : $(BOOTSTRAP_OBJ)
+	$(VERBOSE)ln -sf $(CURDIR)/$(BOOTSTRAP_OBJ) $(DEBUG_DIR)/$(BOOTSTRAP_OBJ)
+endif
+endif
 
 .PHONY: $(BOOTSTRAP_OBJ)
 $(BOOTSTRAP_OBJ):
