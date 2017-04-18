50c53a23eb19597e166dd5778d56a8e265ed12b4
Norman Feske <norman.feske@genode-labs.com>
2017-04-18 14:24:31 +0200

    foc,fiasco: symlink kernel files to bin/

--- repos/base-foc/lib/mk/spec/x86_64/syscall-foc.mk
+++ repos/base-foc/lib/mk/spec/x86_64/syscall-foc.mk
@@ -4,4 +4,6 @@ L4_INC_TARGETS := amd64/l4/sys amd64/l4f/l4/sys amd64/l4/vcpu
 
 CC_OPT += -Iinclude/amd64
 
+L4_BIN_DIR := $(LIB_CACHE_DIR)/syscall-foc/build/bin/amd64_K8
+
 include $(REP_DIR)/lib/mk/spec/x86/syscall-foc.inc
