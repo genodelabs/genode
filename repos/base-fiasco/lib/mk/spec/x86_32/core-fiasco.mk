b3bd2eeb00569119fe3835187a27223b6dd3aa13
Norman Feske <norman.feske@genode-labs.com>
2017-04-18 15:56:31 +0200

    pistachio,fiasco: don't rely on BASE_DIR for vpath
    This patch corresponds to the patch "okl4: don't rely on BASE_DIR for
    vpath", addressing the L4/Fiasco and L4ka::Pistachio kernels.

--- repos/base-fiasco/lib/mk/spec/x86_32/core-fiasco.mk
+++ repos/base-fiasco/lib/mk/spec/x86_32/core-fiasco.mk
@@ -7,3 +7,4 @@ vpath io_port_session_component.cc $(GEN_CORE_DIR)/spec/x86
 vpath io_port_session_support.cc   $(GEN_CORE_DIR)/spec/x86
 vpath platform_services.cc         $(GEN_CORE_DIR)/spec/x86
 vpath platform_x86.cc              $(REP_DIR)/src/core/spec/x86
+vpath %.cc                         $(REP_DIR)/src/core
