df6f1a68b4094b840e9ba506519ea1ba85e461a7
Norman Feske <norman.feske@genode-labs.com>
2017-05-08 17:09:44 +0200

    core: simplify core_env.h
    Issue #2398

--- repos/base/src/core/include/core_env.h
+++ repos/base/src/core/include/core_env.h
@@ -34,36 +34,21 @@ namespace Genode { void init_stack_area(); }
 namespace Genode {
 
 	/**
-	 * Lock-guarded version of a RAM-session implementation
+	 * Lock-guarded wrapper for a RAM session
 	 *
-	 * \param RAM_SESSION_IMPL  non-thread-safe RAM-session class
-	 *
-	 * In contrast to normal processes, core's 'env()->ram_session()' is not
-	 * synchronized by an RPC interface. However, it is accessed by different
-	 * threads using the 'env()->heap()' and the sliced heap used for
-	 * allocating sessions to core's services.
+	 * In contrast to normal components, core's RAM session is not
+	 * synchronized by an RPC interface.
 	 */
-	template <typename RAM_SESSION_IMPL>
-	class Synchronized_ram_session : public RAM_SESSION_IMPL
+	class Synced_ram_session : public Ram_session
 	{
 		private:
 
 			Lock mutable _lock;
+			Ram_session &_ram_session;
 
 		public:
 
-			/**
-			 * Constructor
-			 */
-			Synchronized_ram_session(Rpc_entrypoint  *ds_ep,
-			                         Rpc_entrypoint  *ram_session_ep,
-			                         Range_allocator *ram_alloc,
-			                         Allocator       *md_alloc,
-			                         const char      *args,
-			                         size_t           quota_limit = 0)
-			:
-				RAM_SESSION_IMPL(ds_ep, ram_session_ep, ram_alloc, md_alloc, args, quota_limit)
-			{ }
+			Synced_ram_session(Ram_session &ram_session) : _ram_session(ram_session) { }
 
 
 			/***************************
@@ -73,43 +58,43 @@ namespace Genode {
 			Ram_dataspace_capability alloc(size_t size, Cache_attribute cached) override
 			{
 				Lock::Guard lock_guard(_lock);
-				return RAM_SESSION_IMPL::alloc(size, cached);
+				return _ram_session.alloc(size, cached);
 			}
 
 			void free(Ram_dataspace_capability ds) override
 			{
 				Lock::Guard lock_guard(_lock);
-				RAM_SESSION_IMPL::free(ds);
+				_ram_session.free(ds);
 			}
 
 			size_t dataspace_size(Ram_dataspace_capability ds) const override
 			{
 				Lock::Guard lock_guard(_lock);
-				return RAM_SESSION_IMPL::dataspace_size(ds);
+				return _ram_session.dataspace_size(ds);
 			}
 
 			void ref_account(Ram_session_capability session) override
 			{
 				Lock::Guard lock_guard(_lock);
-				RAM_SESSION_IMPL::ref_account(session);
+				_ram_session.ref_account(session);
 			}
 
 			void transfer_quota(Ram_session_capability session, Ram_quota amount) override
 			{
 				Lock::Guard lock_guard(_lock);
-				RAM_SESSION_IMPL::transfer_quota(session, amount);
+				_ram_session.transfer_quota(session, amount);
 			}
 
 			Ram_quota ram_quota() const override
 			{
 				Lock::Guard lock_guard(_lock);
-				return RAM_SESSION_IMPL::ram_quota();
+				return _ram_session.ram_quota();
 			}
 
 			Ram_quota used_ram() const override
 			{
 				Lock::Guard lock_guard(_lock);
-				return RAM_SESSION_IMPL::used_ram();
+				return _ram_session.used_ram();
 			}
 	};
 
@@ -117,8 +102,6 @@ namespace Genode {
 	class Core_env : public Env_deprecated
 	{
 		private:
-
-			typedef Synchronized_ram_session<Ram_session_component> Core_ram_session;
 
 			enum { ENTRYPOINT_STACK_SIZE = 2048 * sizeof(Genode::addr_t) };
 
@@ -129,10 +112,11 @@ namespace Genode {
 			bool _init_stack_area() { init_stack_area(); return true; }
 			bool _stack_area_initialized = _init_stack_area();
 
-			Rpc_entrypoint               _entrypoint;
-			Core_region_map              _region_map;
-			Core_ram_session             _ram_session;
-			Ram_session_capability const _ram_session_cap;
+			Rpc_entrypoint         _entrypoint;
+			Core_region_map        _region_map;
+			Ram_session_component  _ram_session;
+			Ram_session_capability _ram_session_cap;
+			Synced_ram_session     _synced_ram_session { _ram_session };
 
 			/*
 			 * The core-local PD session is provided by a real RPC object
@@ -144,7 +128,7 @@ namespace Genode {
 			Core_pd_session_component _pd_session_component;
 			Pd_session_client         _pd_session_client;
 
-			Heap _heap;
+			Heap _heap { _synced_ram_session, _region_map };
 
 			Registry<Service> _services;
 
@@ -163,9 +147,8 @@ namespace Genode {
 				             platform()->ram_alloc(), platform()->core_mem_alloc(),
 				             "ram_quota=4M", platform()->ram_alloc()->avail()),
 				_ram_session_cap(_entrypoint.manage(&_ram_session)),
-				_pd_session_component(_entrypoint /* XXX use a different entrypoint */),
-				_pd_session_client(_entrypoint.manage(&_pd_session_component)),
-				_heap(&_ram_session, &_region_map)
+				_pd_session_component(_entrypoint),
+				_pd_session_client(_entrypoint.manage(&_pd_session_component))
 			{ }
 
 			/**
@@ -173,19 +156,20 @@ namespace Genode {
 			 */
 			~Core_env() { parent()->exit(0); }
 
-			Rpc_entrypoint *entrypoint()  { return &_entrypoint; }
+
+			Rpc_entrypoint *entrypoint() { return &_entrypoint; }
 
 
-			/*******************
-			 ** Env interface **
-			 *******************/
+			/******************************
+			 ** Env_deprecated interface **
+			 ******************************/
 
 			Parent                 *parent()          override { return &_core_parent; }
-			Ram_session            *ram_session()     override { return &_ram_session; }
-			Ram_session_capability  ram_session_cap() override { return  _ram_session_cap; }
+			Ram_session            *ram_session()     override { return &_synced_ram_session; }
+			Ram_session_capability  ram_session_cap() override { return  _ram_session.cap(); }
 			Region_map             *rm_session()      override { return &_region_map; }
 			Pd_session             *pd_session()      override { return &_pd_session_client; }
-			Allocator              *heap()            override { return &_heap; }
+			Allocator              *heap()            override { log(__func__, ": not implemented"); return nullptr; }
 
 			Cpu_session *cpu_session() override
 			{
