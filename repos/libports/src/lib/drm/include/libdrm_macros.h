947235ee34200a9d315dea0c2529635dd34d9dd2
Josef Söntgen <josef.soentgen@genode-labs.com>
2017-05-10 16:07:52 +0200

    drm: use Gpu session for i965 driver
    Issue #2507.

--- repos/libports/src/lib/drm/include/libdrm_macros.h
+++ repos/libports/src/lib/drm/include/libdrm_macros.h
@@ -3,18 +3,8 @@
 
 #include <sys/mman.h>
 
-/**
- * On Genode the virtual address of MMAP_GTT is stored in the offset
- */
-static inline void *drm_mmap(void *addr, size_t length, int prot, int flags,
-                             int fd, off_t offset)
-{
-	return (void *)offset;
-}
-
-static inline int drm_munmap(void *addr, size_t length)
-{
-	return 0;
-}
+void *drm_mmap(void *addr, size_t length, int prot, int flags,
+                             int fd, off_t offset);
+int drm_munmap(void *addr, size_t length);
 
 #endif /* _INCLUDE__LIBDRM_MACROS_H_ */
