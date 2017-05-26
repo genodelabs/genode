febfb3bfe6c3c40660beb8066a7674f26424100b
Christian Helmuth <christian.helmuth@genode-labs.com>
2017-05-26 16:57:44 +0200

    Fix source file location in RPi GPIO driver
    The explicit relative location of the file instructed both target builds
    to generate ../main.o which gloriously fails with parallel builds. The
    produced range of error messages was astonishing ranging from "file
    truncated" to "TLS reference in ../main.o mismatches non-TLS reference
    in ../main.o".

--- repos/os/src/drivers/gpio/spec/rpi/foc/target.mk
+++ repos/os/src/drivers/gpio/spec/rpi/foc/target.mk
@@ -1,5 +1,7 @@
 TARGET   = foc_gpio_drv
 REQUIRES = rpi
-SRC_CC  += ../main.cc
-LIBS    += base
+SRC_CC  += main.cc
+LIBS     = base
 INC_DIR += $(PRG_DIR) $(PRG_DIR)/..
+
+vpath main.cc $(PRG_DIR)/..
--- repos/os/src/drivers/gpio/spec/rpi/hw/target.mk
+++ repos/os/src/drivers/gpio/spec/rpi/hw/target.mk
@@ -1,5 +1,7 @@
 TARGET   = hw_gpio_drv
 REQUIRES = rpi
-SRC_CC  += ../main.cc
+SRC_CC  += main.cc
 LIBS     = base
 INC_DIR += $(PRG_DIR) $(PRG_DIR)/..
+
+vpath main.cc $(PRG_DIR)/..
