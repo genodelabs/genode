1db709e8276468fee2af3d663b02bdd8f2f09d8d
Johannes Kliemann <jk@jkliemann.de>
2017-07-26 14:46:22 +0200

    qt5: added example for qtvirtualkeyboard

---
+++ repos/libports/src/lib/qt5/qtdeclarative/src/imports/folderlistmodel/target.mk
@@ -0,0 +1,4 @@
+# the plugin gets loaded via 'dlopen()', therefore it is built separately
+
+TARGET = dummy-qt5_qmlfolderlistmodelplugin
+LIBS = qt5_qmlfolderlistmodelplugin
---
+++ repos/libports/src/lib/qt5/qtvirtualkeyboard/src/imports/qtvirtualkeyboard/styles/target.mk
@@ -0,0 +1,4 @@
+# the plugin gets loaded via 'dlopen()', therefore it is built separately
+
+TARGET = dummy-qt5_qtvirtualkeyboardstylesplugin
+LIBS = qt5_qtvirtualkeyboardstylesplugin
