94f428a8b6dc4c9baeccb99be2f542c9c660e125
Norman Feske <norman.feske@genode-labs.com>
2017-08-09 19:39:23 +0200

    depot: update src/menu_view recipe
    The menu-view component uses the gems/include/polygon_gfx/ headers now.

--- repos/gems/recipes/src/menu_view/content.mk
+++ repos/gems/recipes/src/menu_view/content.mk
@@ -1,8 +1,12 @@
 SRC_DIR := src/app/menu_view
 include $(GENODE_DIR)/repos/base/recipes/src/content.inc
 
-content: include/decorator
+content: include/decorator include/polygon_gfx
 
 include/decorator:
 	mkdir -p $@
 	cp $(GENODE_DIR)/repos/os/include/decorator/* $@
+
+include/polygon_gfx:
+	mkdir -p $@
+	cp $(GENODE_DIR)/repos/gems/include/polygon_gfx/* $@
