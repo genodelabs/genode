4ac0bd514f64347c3964cacf6f1f49ad9e393c87
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2017-08-16 20:06:54 +0200

    sel4: add uefi boot support via mbi2
    Multiboot2 provides the ACPI RSDP pointer from the GRUB2 bootloader.
    
    Issue #2242

--- repos/base-sel4/src/kernel/sel4/target.mk
+++ repos/base-sel4/src/kernel/sel4/target.mk
@@ -2,4 +2,4 @@ TARGET = sel4
 LIBS   = kernel-sel4
 
 $(INSTALL_DIR)/$(TARGET):
-	$(VERBOSE)ln -sf $(LIB_CACHE_DIR)/kernel-sel4/kernel.elf $@
+	$(VERBOSE)ln -sf $(LIB_CACHE_DIR)/kernel-sel4/kernel.elf.strip $@
