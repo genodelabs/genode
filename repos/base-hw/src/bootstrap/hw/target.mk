362337a9e8aabb58e931469c99caecdba072a956
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2017-09-08 17:25:32 +0200

    hw: remove bootstrap in clean rules
    Ref #2091

--- repos/base-hw/src/bootstrap/hw/target.mk
+++ repos/base-hw/src/bootstrap/hw/target.mk
@@ -9,8 +9,11 @@ ifneq ($(INSTALL_DIR),)
 ifneq ($(DEBUG_DIR),)
 $(TARGET): $(INSTALL_DIR)/$(BOOTSTRAP_OBJ)
 
-$(INSTALL_DIR)/$(BOOTSTRAP_OBJ) : $(DEBUG_DIR)/$(BOOTSTRAP_OBJ)
+$(BOOTSTRAP_OBJ).stripped: $(BOOTSTRAP_OBJ)
 	$(VERBOSE)$(STRIP) --strip-debug -o $@ $<
+
+$(INSTALL_DIR)/$(BOOTSTRAP_OBJ) : $(BOOTSTRAP_OBJ).stripped
+	$(VERBOSE)ln -sf $(CURDIR)/$< $@
 
 $(DEBUG_DIR)/$(BOOTSTRAP_OBJ) : $(BOOTSTRAP_OBJ)
 	$(VERBOSE)ln -sf $(CURDIR)/$(BOOTSTRAP_OBJ) $(DEBUG_DIR)/$(BOOTSTRAP_OBJ)
@@ -22,4 +25,4 @@ $(BOOTSTRAP_OBJ):
 	$(VERBOSE)$(LD) $(LD_MARCH) -u _start --whole-archive -r $(LINK_ITEMS) $(LIBCXX_GCC) -o $@
 
 clean cleanall:
-	$(VERBOSE)rm -f $(BOOTSTRAP_OBJ)
+	$(VERBOSE)rm -f $(BOOTSTRAP_OBJ) $(BOOTSTRAP_OBJ).stripped
