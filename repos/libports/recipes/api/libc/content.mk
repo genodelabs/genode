4835da0f4d0a635675bf5365d7d3368663e06c54
Norman Feske <norman.feske@genode-labs.com>
2017-09-13 16:33:34 +0200

    depot: add posix lib to api/libc archive

--- repos/libports/recipes/api/libc/content.mk
+++ repos/libports/recipes/api/libc/content.mk
@@ -1,15 +1,23 @@
-content: include lib/import/import-libc.mk lib/symbols/libc lib/symbols/libm
+MIRROR_FROM_REP_DIR := lib/import/import-libc.mk \
+                       lib/symbols/libc \
+                       lib/symbols/libm \
+                       lib/mk/pthread.mk \
+                       src/lib/pthread
+
+content: $(MIRROR_FROM_REP_DIR)
+
+$(MIRROR_FROM_REP_DIR):
+	$(mirror_from_rep_dir)
 
 PORT_DIR := $(call port_dir,$(REP_DIR)/ports/libc)
+
+content: include
 
 include:
 	mkdir $@
 	cp -r $(PORT_DIR)/include/* $@/
 	cp -r $(REP_DIR)/include/libc $@/
 	cp -r $(REP_DIR)/include/libc-genode $@/
-
-lib/import/import-libc.mk lib/symbols/libc lib/symbols/libm:
-	$(mirror_from_rep_dir)
 
 content: LICENSE
 
