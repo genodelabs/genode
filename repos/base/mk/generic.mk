170b532892c046a6aba90f743329ff21cd81fefa
Christian Helmuth <christian.helmuth@genode-labs.com>
2017-09-28 14:49:22 +0200

    Support read-only data symbols in ABI
    This also adapts existing symbol files

--- repos/base/mk/generic.mk
+++ repos/base/mk/generic.mk
@@ -175,7 +175,7 @@ endif
 		sed -e "s/^\(\w\+\) D \(\w\+\)\$$/.data; .global \1; .type \1,%object; .size \1,\2; \1:/p" \
 		    -e "s/^\(\w\+\) V/.data; .weak \1; .type \1,%object; \1:/p" \
 		    -e "s/^\(\w\+\) T/.text; .global \1; .type \1,%function; \1:/p" \
-		    -e "s/^\(\w\+\) R/.section .rodata; .global \1; \1:/p" \
+		    -e "s/^\(\w\+\) R \(\w\+\)\$$/.section .rodata; .global \1; .type \1,%object; .size \1,\2; \1:/p" \
 		    -e "s/^\(\w\+\) W/.text; .weak \1; .type \1,%function; \1:/p" \
 		    -e "s/^\(\w\+\) B \(\w\+\)\$$/.bss; .global \1; .type \1,%object; .size \1,\2; \1:/p" \
 		    -e "s/^\(\w\+\) U/.text; .global \1; $(ASM_SYM_DEPENDENCY)/p" \
