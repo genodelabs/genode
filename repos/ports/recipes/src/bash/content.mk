bd18152d32ee780e3ff2dbb555e5a2a8b31d9dc1
Norman Feske <norman.feske@genode-labs.com>
2017-11-02 15:32:50 +0100

    depot: preserve timestamps in src/bash

--- repos/ports/recipes/src/bash/content.mk
+++ repos/ports/recipes/src/bash/content.mk
@@ -4,8 +4,8 @@ PORT_DIR := $(call port_dir,$(REP_DIR)/ports/bash)
 
 src/noux-pkg/bash:
 	mkdir -p $@
-	cp -r $(PORT_DIR)/src/noux-pkg/bash/* $@
-	cp -r  $(REP_DIR)/src/noux-pkg/bash/* $@
+	cp -a $(PORT_DIR)/src/noux-pkg/bash/* $@
+	cp -a  $(REP_DIR)/src/noux-pkg/bash/* $@
 
 LICENSE:
 	cp $(PORT_DIR)/src/noux-pkg/bash/COPYING $@
