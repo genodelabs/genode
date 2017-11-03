94f4e2a276c688e95d4ba3200a88617d25c686a2
Martin Stein <martin.stein@genode-labs.com>
2017-11-03 13:58:37 +0100

    os/recipes: nic_router
    Fixes #2560

---
+++ repos/os/recipes/src/nic_router/content.mk
@@ -0,0 +1,9 @@
+SRC_DIR = src/server/nic_router
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc
+
+MIRROR_FROM_REP_DIR := lib/mk/net.mk include/net include/nic_bridge src/lib/net
+
+content: $(MIRROR_FROM_REP_DIR)
+
+$(MIRROR_FROM_REP_DIR):
+	$(mirror_from_rep_dir)
