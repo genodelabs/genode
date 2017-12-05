58cbd11f6ac32f1da03fa86327aa963839dc84aa
Christian Prochaska <christian.prochaska@genode-labs.com>
2017-12-05 17:07:06 +0100

    drivers_interactive-pbxa9: add input_filter
    Fixes #2604

--- repos/os/recipes/raw/drivers_interactive-pbxa9/content.mk
+++ repos/os/recipes/raw/drivers_interactive-pbxa9/content.mk
@@ -1,4 +1,7 @@
-content: drivers.config
+content: drivers.config input_filter.config en_us.chargen
 
-drivers.config:
+drivers.config input_filter.config:
 	cp $(REP_DIR)/recipes/raw/drivers_interactive-pbxa9/$@ $@
+
+en_us.chargen:
+	cp $(REP_DIR)/src/server/input_filter/$@ $@
