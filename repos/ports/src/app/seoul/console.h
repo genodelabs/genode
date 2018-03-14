57fcd5e0c4b09347752d01526dc0270d370b0a89
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2018-03-14 23:04:43 +0100

    seoul: adjust vga model to framebuffer host size
    Configure the available vga/vesa memory based on the given Genode framebuffer
    size and not the way around.
    
    Issue #2715

--- repos/ports/src/app/seoul/console.h
+++ repos/ports/src/app/seoul/console.h
@@ -24,7 +24,6 @@
 /* base includes */
 #include <base/env.h>
 #include <dataspace/client.h>
-#include <util/reconstructible.h>
 #include <util/string.h>
 
 /* os includes */
@@ -41,7 +40,6 @@
 
 namespace Seoul {
 	class Console;
-	using Genode::Constructible;
 	using Genode::Pixel_rgb565;
 }
 
@@ -52,17 +50,16 @@ class Seoul::Console : public StaticReceiver<Seoul::Console>
 		Genode::Env                 &_env;
 		Motherboard                 &_unsynchronized_motherboard;
 		Synced_motherboard          &_motherboard;
-		Constructible<Framebuffer::Connection>        _framebuffer { };
-		Constructible<Genode::Surface<Pixel_rgb565> > _surface { };
+		Framebuffer::Connection     &_framebuffer;
+		Genode::Dataspace_capability _guest_fb_ds;
+		Framebuffer::Mode            _fb_mode; 
+		size_t                       _fb_size; 
+		short                       *_pixels; 
+		Genode::Surface<Pixel_rgb565> _surface; 
 		Input::Connection            _input    { _env };
 		Keyboard                     _vkeyb    { _motherboard };
-		short                       *_pixels   { nullptr };
 		char                        *_guest_fb { nullptr };
-		unsigned long                _fb_size  { 0 };
-		Genode::Dataspace_capability _fb_ds;
-		Genode::size_t               _vm_fb_size;
 		VgaRegs                     *_regs     { nullptr };
-		Framebuffer::Mode            _fb_mode  { };
 		bool                         _left     { false };
 		bool                         _middle   { false };
 		bool                         _right    { false };
@@ -95,7 +92,7 @@ class Seoul::Console : public StaticReceiver<Seoul::Console>
 		 * Constructor
 		 */
 		Console(Genode::Env &env, Synced_motherboard &, Motherboard &,
-		        Genode::size_t vm_fb_size, Genode::Dataspace_capability fb_ds);
+		        Framebuffer::Connection &, Genode::Dataspace_capability fb_ds);
 };
 
 #endif /* _CONSOLE_H_ */
