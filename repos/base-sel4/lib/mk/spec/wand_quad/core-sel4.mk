a25ec92a300fd878216685d879d4050b1241f990
Christian Helmuth <christian.helmuth@genode-labs.com>
2018-04-18 16:42:08 +0200

    sel4: explicit vpath for each file in core
    The pattern rule picked up the wrong platform_services.cc in the
    depot-archive case only because the archive integrates generic and
    sel4-specific source files into REP_DIR.

--- repos/base-sel4/lib/mk/spec/wand_quad/core-sel4.mk
+++ repos/base-sel4/lib/mk/spec/wand_quad/core-sel4.mk
@@ -1,8 +1,13 @@
-SRC_CC += $(addprefix spec/arm/, boot_info.cc thread.cc platform.cc irq.cc \
-                                 vm_space.cc platform_thread.cc)
+GEN_SRC_CC = platform_services.cc
+
+REP_SRC_CC = \
+             spec/arm/boot_info.cc \
+             spec/arm/irq.cc \
+             spec/arm/platform.cc \
+             spec/arm/platform_thread.cc \
+             spec/arm/thread.cc \
+             spec/arm/vm_space.cc
 
 INC_DIR += $(REP_DIR)/src/core/spec/arm
 
 include $(REP_DIR)/lib/mk/core-sel4.inc
-
-vpath platform_services.cc $(GEN_CORE_DIR)
--- repos/base-sel4/lib/mk/spec/x86_64/core-sel4.mk
+++ repos/base-sel4/lib/mk/spec/x86_64/core-sel4.mk
@@ -1,13 +1,18 @@
-SRC_CC += $(addprefix spec/x86_64/, boot_info.cc thread.cc platform.cc \
-                                    platform_pd.cc vm_space.cc)
-SRC_CC += $(addprefix spec/x86/, irq.cc vm_space.cc platform_thread.cc)
+GEN_SRC_CC = \
+             spec/x86/platform_services.cc \
+             spec/x86/io_port_session_component.cc
 
-SRC_CC += io_port_session_component.cc
-SRC_CC += io_port_session_support.cc
+REP_SRC_CC = \
+             spec/x86/io_port_session_support.cc \
+             spec/x86/irq.cc \
+             spec/x86/platform_thread.cc \
+             spec/x86/vm_space.cc \
+             spec/x86_64/boot_info.cc \
+             spec/x86_64/platform.cc \
+             spec/x86_64/platform_pd.cc \
+             spec/x86_64/thread.cc \
+             spec/x86_64/vm_space.cc
 
 INC_DIR += $(REP_DIR)/src/core/spec/x86
 
 include $(REP_DIR)/lib/mk/core-sel4.inc
-
-vpath io_port_session_component.cc $(GEN_CORE_DIR)/spec/x86
-vpath platform_services.cc         $(GEN_CORE_DIR)/spec/x86
