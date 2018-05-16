65a1e01e7deb34f857188099d1639e4765d98dc0
Norman Feske <norman.feske@genode-labs.com>
2018-05-16 14:18:48 +0200

    depot: recipe for api/jitterentropy
    The API archive makes the library usable for src/gpt_write.

--- repos/libports/lib/import/import-jitterentropy.mk
+++ repos/libports/lib/import/import-jitterentropy.mk
@@ -8,7 +8,12 @@ else ifeq ($(filter-out $(SPECS),arm_v7),)
 	TARGET_CPUARCH=arm_v7
 endif
 
+ifeq ($(CONTRIB_DIR),)
+REP_INC_DIR += include/jitterentropy
+else
 INC_DIR += $(call select_from_ports,jitterentropy)/include/jitterentropy
+endif
+
 INC_DIR += $(call select_from_repositories,src/lib/jitterentropy)
 INC_DIR += $(call select_from_repositories,src/lib/jitterentropy/spec/$(TARGET_CPUARCH))
 
