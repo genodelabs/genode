77b8ab305e6c60942ae6e477c1c9286aa357216b
Martin Stein <martin.stein@genode-labs.com>
2018-05-17 16:57:07 +0200

    net: get rid of unused mac_from_string method
    Issue #2815

--- repos/os/lib/mk/net.mk
+++ repos/os/lib/mk/net.mk
@@ -1,4 +1,4 @@
-SRC_CC += ethernet.cc ipv4.cc dhcp.cc arp.cc udp.cc tcp.cc mac_address.cc
+SRC_CC += ethernet.cc ipv4.cc dhcp.cc arp.cc udp.cc tcp.cc
 SRC_CC += icmp.cc internet_checksum.cc
 
 vpath %.cc $(REP_DIR)/src/lib/net
