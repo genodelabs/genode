4bfc32789e9d00f4bb55588d67acc9641ffe5ca1
Christian Helmuth <christian.helmuth@genode-labs.com>
2018-06-01 12:12:26 +0200

    sculpt: manager depends on x86 (currently)
    The sculpt manager uses the platform-session interface which is not
    defined for other platforms currently.

--- repos/gems/src/app/sculpt_manager/target.mk
+++ repos/gems/src/app/sculpt_manager/target.mk
@@ -5,3 +5,5 @@ SRC_CC  += $(addprefix view/,   $(notdir $(wildcard $(PRG_DIR)/view/*.cc)))
 SRC_CC  += $(addprefix model/,  $(notdir $(wildcard $(PRG_DIR)/model/*.cc)))
 LIBS    += base
 INC_DIR += $(PRG_DIR) $(REP_DIR)/src/app/depot_deploy
+
+REQUIRES = x86
