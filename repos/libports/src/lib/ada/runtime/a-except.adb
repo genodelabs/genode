db6e4e71151565db0822c5ede58439acd39bf2a4
Johannes Kliemann <kliemann@componolit.com>
2018-07-03 15:01:07 +0200

    Ada: fixup: license headers, beautify

--- repos/libports/src/lib/ada/runtime/a-except.adb
+++ repos/libports/src/lib/ada/runtime/a-except.adb
@@ -1,3 +1,13 @@
+--
+--  \brief  Ada exceptions
+--  \author Johannes Kliemann
+--  \date   2018-06-25
+--
+--  Copyright (C) 2018 Componolit GmbH
+--  This file is part of the Genode OS framework, which is distributed
+--  under the terms of the GNU Affero General Public License version 3.
+--
+
 package body Ada.Exceptions is
 
    ----------------------------
