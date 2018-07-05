4c96d697d516e0344425d9b77a7fc9b5f6cd6491
Norman Feske <norman.feske@genode-labs.com>
2018-07-05 16:55:00 +0200

    sculpt_manager: reduce compile time
    This little tweak reduces the compile time of the component to the half
    by avoiding the repeated parsing of header files.

---
+++ repos/gems/src/app/sculpt_manager/runtime.cc
@@ -0,0 +1,29 @@
+/*
+ * \brief  Sculpt runtime-configuration utilites
+ * \author Norman Feske
+ * \date   2018-07-06
+ *
+ * This file is used to compile all '*.cc' files in the 'runtime/' directory at
+ * once, which saves about 50% compile time.
+ */
+
+/*
+ * Copyright (C) 2018 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#include <runtime/chroot.cc>
+#include <runtime/depot_query.cc>
+#include <runtime/e2fs.cc>
+#include <runtime/file_browser.cc>
+#include <runtime/file_system.cc>
+#include <runtime/fs_rom.cc>
+#include <runtime/gpt_write.cc>
+#include <runtime/nic_drv.cc>
+#include <runtime/nic_router.cc>
+#include <runtime/prepare.cc>
+#include <runtime/ram_fs.cc>
+#include <runtime/update.cc>
+#include <runtime/wifi_drv.cc>
--- repos/gems/src/app/sculpt_manager/target.mk
+++ repos/gems/src/app/sculpt_manager/target.mk
@@ -1,9 +1,6 @@
 TARGET  := sculpt_manager
 SRC_CC  := $(notdir $(wildcard $(PRG_DIR)/*.cc))
-SRC_CC  += $(addprefix runtime/,$(notdir $(wildcard $(PRG_DIR)/runtime/*.cc)))
-SRC_CC  += $(addprefix view/,   $(notdir $(wildcard $(PRG_DIR)/view/*.cc)))
-SRC_CC  += $(addprefix model/,  $(notdir $(wildcard $(PRG_DIR)/model/*.cc)))
+SRC_CC  += $(addprefix view/, $(notdir $(wildcard $(PRG_DIR)/view/*.cc)))
 LIBS    += base
 INC_DIR += $(PRG_DIR) $(REP_DIR)/src/app/depot_deploy
-
 REQUIRES = x86
