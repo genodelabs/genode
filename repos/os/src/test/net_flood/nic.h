7b47c8f0c629e987b56b4996259b08702ceb89fc
Martin Stein <martin.stein@genode-labs.com>
2018-07-12 15:26:30 +0200

    net_flood: fix CRC error, make more precise
    Issue #2953

--- repos/os/src/test/net_flood/nic.h
+++ repos/os/src/test/net_flood/nic.h
@@ -51,7 +51,7 @@ class Net::Nic
 		using Signal_handler = Genode::Signal_handler<Nic>;
 
 		enum { PKT_SIZE = ::Nic::Packet_allocator::DEFAULT_PACKET_SIZE };
-		enum { BUF_SIZE = 90 * PKT_SIZE };
+		enum { BUF_SIZE = 1000 * PKT_SIZE };
 
 		Genode::Env            &_env;
 		Genode::Allocator      &_alloc;
@@ -101,20 +101,16 @@ class Net::Nic
 		void send(Genode::size_t pkt_size,
 		          FUNC        && write_to_pkt)
 		{
-			try {
-				Packet_descriptor  pkt      = _source().alloc_packet(pkt_size);
-				void              *pkt_base = _source().packet_content(pkt);
+			Packet_descriptor  pkt      = _source().alloc_packet(pkt_size);
+			void              *pkt_base = _source().packet_content(pkt);
+			Size_guard size_guard(pkt_size);
+			write_to_pkt(pkt_base, size_guard);
+			_source().submit_packet(pkt);
+			if (_verbose) {
 				Size_guard size_guard(pkt_size);
-				write_to_pkt(pkt_base, size_guard);
-				_source().submit_packet(pkt);
-				if (_verbose) {
-					Size_guard size_guard(pkt_size);
-					try { Genode::log("snd ", Ethernet_frame::cast_from(pkt_base, size_guard)); }
-					catch (Size_guard::Exceeded) { Genode::log("snd ?"); }
-				}
+				try { Genode::log("snd ", Ethernet_frame::cast_from(pkt_base, size_guard)); }
+				catch (Size_guard::Exceeded) { Genode::log("snd ?"); }
 			}
-			catch (Net::Packet_stream_source::Packet_alloc_failed) {
-				Genode::warning("failed to allocate packet"); }
 		}
 
 
