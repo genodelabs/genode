ce414a5c5a08a1f79478cde9cee0ee5207428ac5
Norman Feske <norman.feske@genode-labs.com>
2018-08-17 15:02:46 +0200

    fs_rom: revert workaround for possible livelock
    The change "base: rm first-class support for static binaries" alleviates
    the need for the workaround.

--- repos/os/src/server/fs_rom/main.cc
+++ repos/os/src/server/fs_rom/main.cc
@@ -495,19 +495,6 @@ class Fs_rom::Rom_root : public Root_component<Fs_rom::Rom_session_component>
 
 void Component::construct(Genode::Env &env)
 {
-	/*
-	 * Print message to force the creation of the env LOG session, which would
-	 * otherwise be created on the first (error) message. The latter becomes a
-	 * problem when using the fs_rom as a provider for the env ROM sessions
-	 * (like depot_rom in the sculpt scenario). Here, an error message printed
-	 * in the synchronously called 'Rom_session::dataspace' RPC function may
-	 * create a livelock on the attempt to obtain the LOG session.
-	 *
-	 * XXX This workaround can be removed with the eager creation of the
-	 *     the env log session.
-	 */
-	Genode::log("--- fs_rom ---");
-
 	static Genode::Sliced_heap sliced_heap(env.ram(), env.rm());
 	static Fs_rom::Rom_root inst(env, sliced_heap);
 }
