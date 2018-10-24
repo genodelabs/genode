3a4950fd02eddc3f1f599f0beb253e2f009d0360
Martin Stein <martin.stein@genode-labs.com>
2018-10-24 18:34:45 +0200

    libports/recipes: src/ and pkg/test-libc_vfs* (FIXME: qemu specific config)

---
+++ repos/libports/recipes/src/test-libc_vfs/content.mk
@@ -0,0 +1,2 @@
+SRC_DIR := src/test/libc_vfs
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc
---
+++ repos/libports/recipes/src/test-libc_vfs_block/content.mk
@@ -0,0 +1,2 @@
+SRC_DIR := src/test/libc_vfs_block
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc
