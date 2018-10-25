ebdb3c4c327c878ad1f72ec96d2b43d63cd31410
Martin Stein <martin.stein@genode-labs.com>
2018-10-25 17:27:51 +0200

    ada lib: prepare for creation of packages

--- repos/libports/lib/mk/ada.mk
+++ repos/libports/lib/mk/ada.mk
@@ -1,13 +1,13 @@
 
-include $(REP_DIR)/lib/import/import-ada.mk
-ADALIB = $(ADA_RTS)/adalib
+include $(REP_DIR)/lib/mk/ada.inc
+
+ADALIB     = $(ADA_RTS)/adalib
 ADAINCLUDE = $(ADA_RTS)/adainclude
 
 PACKAGES = system s-stoele s-secsta a-except s-conca2 s-arit64
 
 body_exists := $(filter $1.adb,$(shell if [ -e $2/$1.adb ]; then echo $1.adb; fi))
 
-ADA_RTS_SOURCE = $(call select_from_ports,gcc)/src/noux-pkg/gcc/gcc/ada
 SRC_ADS += $(foreach package, $(PACKAGES), $(package).ads)
 SRC_ADB += $(foreach package, $(PACKAGES), $(body_exists, $(package), $(ADA_RTS_SOURCE)))
 SRC_ADB += $(foreach package, $(PACKAGES), $(body_exists, $(package), $(REP_DIR)/src/lib/ada/runtime))
@@ -31,12 +31,3 @@ vpath %.adb $(REP_DIR)/src/lib/ada/runtime $(ADA_RTS_SOURCE) $(REP_DIR)/src/lib/
 vpath %.ads $(REP_DIR)/src/lib/ada/runtime $(ADA_RTS_SOURCE)
 
 SHARED_LIB = yes
-
-all: ada_source_path ada_object_path
-
-ada_source_path: ada_object_path
-	$(VERBOSE)$(shell echo $(CONTRIB_DIR)/gcc-$(shell echo $(call _hash_of_port,gcc) | cut -d" " -f1)/src/noux-pkg/gcc/gcc/ada > $(ADA_RTS)/ada_source_path)
-
-ada_object_path:
-	$(VERBOSE)$(shell mkdir -p $(ADA_RTS))
-	$(VERBOSE)$(shell echo $(ADA_RTS) > $(ADA_RTS)/ada_object_path)
