9c8e76b1904f50e42056c79d89613f2c7c232995
Christian Prochaska <christian.prochaska@genode-labs.com>
2018-12-06 05:19:19 +0100

    base: 'sanitizer' test
    Issue #3072

---
+++ repos/base/src/test/sanitizer/main.cc
@@ -0,0 +1,34 @@
+/*
+ * \brief  Sanitizer test
+ * \author Christian Prochaska
+ * \date   2018-12-06
+ */
+
+/*
+ * Copyright (C) 2018 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#include <base/component.h>
+#include <base/log.h>
+
+extern void sanitizer_init(Genode::Env &);
+
+void Component::construct(Genode::Env &env)
+{
+	env.exec_static_constructors();
+	sanitizer_init(env);
+
+	/* test array out-of-bounds access detection */
+
+	int array[1];
+	int volatile i = 2;
+	Genode::log("array[", i, "] = ", array[i]);
+
+	/* test null pointer access detection */
+
+	int * volatile ptr = nullptr;
+	*ptr = 0x55;
+}
---
+++ repos/base/src/test/sanitizer/target.mk
@@ -0,0 +1,4 @@
+TARGET             = test-sanitizer
+SRC_CC             = main.cc
+LIBS               = base
+SANITIZE_UNDEFINED = yes
