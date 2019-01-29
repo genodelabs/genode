158650ff017eeef35c5519b2c32cb8335d56d4f6
Norman Feske <norman.feske@genode-labs.com>
2019-01-29 15:25:15 +0100

    base-nova: remove signal-delivery failure warning
    Fixes #3136

--- repos/base-nova/src/lib/base/signal_transmitter.cc
+++ repos/base-nova/src/lib/base/signal_transmitter.cc
@@ -46,7 +46,5 @@ void Signal_transmitter::submit(unsigned cnt)
 	if (res == NOVA_OK)
 		return;
 
-	warning("submitting signal failed - error ", res, " - context=", _context);
-
 	_context = Signal_context_capability();
 }
