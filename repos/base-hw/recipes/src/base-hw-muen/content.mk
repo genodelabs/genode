2e1e7b8270d7cfb999de088cac451891b2eec8bf
Norman Feske <norman.feske@genode-labs.com>
2019-02-05 15:29:44 +0100

    recipes/src/base-hw-muen: add static ACPI ROM
    As we don't execute the acpi_drv on Muen, we have to supply a static
    'acpi' info as boot module. This is normally done by the
    base/run/platform.inc include. However, when using base-hw-muen kernel
    from a depot archive - as done by modern run scripts like
    depot_download.run - the platform.inc magic is not applied.
    
    This patch enhances the src archive of base-hw-muen with a mechanism
    that creates a pre-defined acpi info at the bin directory via an
    artificial src/acpi/target.mk file. This way, the static acpi ROM ends
    up as boot module when importing the base-hw-muen archive into a
    run script.

--- repos/base-hw/recipes/src/base-hw-muen/content.mk
+++ repos/base-hw/recipes/src/base-hw-muen/content.mk
@@ -3,3 +3,9 @@ include $(GENODE_DIR)/repos/base-hw/recipes/src/base-hw_content.inc
 content: enable_board_spec
 enable_board_spec: etc/specs.conf
 	echo "SPECS += muen" >> etc/specs.conf
+
+content: src/acpi/target.mk
+src/acpi/target.mk:
+	mkdir $(dir $@)
+	cp $(REP_DIR)/recipes/src/base-hw-muen/acpi_target_mk $@
+
