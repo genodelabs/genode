e9a53690b16c21da50bf5f8980289307e251dc37
Alexander Senier <senier@componolit.com>
2019-02-06 16:16:37 +0100

    Cleanup spark.mk

--- repos/libports/lib/import/import-spark.mk
+++ repos/libports/lib/import/import-spark.mk
@@ -1,1 +1,10 @@
 ADA_RTS = $(BUILD_BASE_DIR)/var/libcache/spark
+
+ADA_RTS_SOURCE = $(call select_from_ports,ada-runtime)/ada-runtime/contrib/gcc-6.3.0
+ADA_RUNTIME_DIR = $(call select_from_ports,ada-runtime)/ada-runtime/src
+ADA_RUNTIME_LIB_DIR = $(call select_from_ports,ada-runtime)/ada-runtime/src/lib
+ADA_RUNTIME_PLATFORM_DIR = $(call select_from_ports,ada-runtime)/ada-runtime/platform
+
+INC_DIR += $(ADA_RUNTIME_DIR)
+INC_DIR += $(ADA_RUNTIME_LIB_DIR)
+INC_DIR += $(ADA_RTS_SOURCE)
--- repos/libports/lib/mk/spark.mk
+++ repos/libports/lib/mk/spark.mk
@@ -3,27 +3,19 @@ include $(REP_DIR)/lib/mk/spark.inc
 ADALIB     = $(ADA_RTS)/adalib
 ADAINCLUDE = $(ADA_RTS)/adainclude
 
-ADA_RTS_SOURCE = $(call select_from_ports,ada-runtime)/ada-runtime/contrib/gcc-6.3.0
-ADA_RUNTIME_DIR = $(call select_from_ports,ada-runtime)/ada-runtime/src
-ADA_RUNTIME_LIB_DIR = $(call select_from_ports,ada-runtime)/ada-runtime/src/lib
-ADA_RUNTIME_PLATFORM_DIR = $(call select_from_ports,ada-runtime)/ada-runtime/platform
-
 SRC_ADS += system.ads \
 	   s-soflin.ads \
 	   s-imgint.ads \
 	   s-stoele.ads \
-	   s-secsta.ads \
+	   s-unstyp.ads \
 	   interfac.ads \
 	   a-except.ads \
-	   gnat.ads
+	   gnat.ads \
+	   ada.ads \
+	   ada_exceptions.ads
 
-SRC_ADB += g-io.adb
-
-CUSTOM_ADA_FLAGS   = -c -gnatg -gnatp -gnatpg -gnatn2
-CUSTOM_ADA_OPT     = $(CC_ADA_OPT)
-CUSTOM_ADA_INCLUDE = -I- -I$(ADA_RUNTIME_DIR) -I$(ADA_RTS_SOURCE) -I$(ADA_RUNTIME_LIB_DIR)
-
-INC_DIR += $(ADA_RUNTIME_LIB_DIR)
+SRC_ADB += g-io.adb s-stalib.adb s-secsta.adb s-parame.adb
+CUSTOM_ADA_FLAGS = -c -gnatg -gnatp -gnatpg -gnatn2
 
 # C runtime glue code
 SRC_CC += genode.cc
@@ -33,25 +25,21 @@ SRC_ADB += ss_utils.adb string_utils.adb platform.adb
 
 vpath %.cc  $(ADA_RUNTIME_PLATFORM_DIR)
 
-vpath system.ads $(ADA_RTS_SOURCE)
 vpath s-soflin.ads $(ADA_RUNTIME_DIR)
-vpath s-stoele.ads $(ADA_RTS_SOURCE)
-vpath s-secsta.ads $(ADA_RUNTIME_DIR)
-vpath s-imgint.ads $(ADA_RTS_SOURCE)
 vpath a-except.ads $(ADA_RUNTIME_DIR)
-vpath interfac.ads $(ADA_RTS_SOURCE)
-vpath gnat.ads     $(ADA_RTS_SOURCE)
-vpath g-io.ads     $(ADA_RTS_SOURCE)
 
-vpath s-stoele.adb $(ADA_RTS_SOURCE)
 vpath s-secsta.adb $(ADA_RUNTIME_DIR)
 vpath s-soflin.adb $(ADA_RUNTIME_DIR)
-vpath s-imgint.adb $(ADA_RTS_SOURCE)
+vpath s-stalib.adb $(ADA_RUNTIME_DIR)
+vpath s-parame.adb $(ADA_RUNTIME_DIR)
 vpath a-except.adb $(ADA_RUNTIME_DIR)
-vpath g-io.adb     $(ADA_RTS_SOURCE)
+
+vpath %.ads $(ADA_RTS_SOURCE)
+vpath %.adb $(ADA_RTS_SOURCE)
 
 vpath platform.% $(ADA_RUNTIME_LIB_DIR)
 vpath string_utils.% $(ADA_RUNTIME_LIB_DIR)
 vpath ss_utils.% $(ADA_RUNTIME_LIB_DIR)
+vpath ada_exceptions.ads $(ADA_RUNTIME_LIB_DIR)
 
 SHARED_LIB = yes
