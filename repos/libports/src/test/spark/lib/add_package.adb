16800da0c7bc1c6e661afc057d573576d48dcefe
Alexander Senier <senier@componolit.com>
2019-02-07 13:43:31 +0100

    Remove data flow warnings in examples

--- repos/libports/src/test/spark/lib/add_package.adb
+++ repos/libports/src/test/spark/lib/add_package.adb
@@ -1,14 +1,14 @@
 package body add_package is
 
-procedure Add(A: in Integer;
-              B: in Integer;
-              R: out Integer)
+procedure Add (A : in Integer;
+               B : in Integer;
+               R : out Integer)
 is
-	procedure ext_c_print_add(a, b : Integer; result : out Integer);
-	pragma import(C, ext_c_print_add, "print_add");
+	procedure Ext_C_Print_Add (A, B : Integer; Result : out Integer);
+	pragma Import (C, Ext_C_Print_Add, "print_add");
 begin
-    ext_c_print_add(A, B, R);
-    R := A + B;
+    Ext_C_Print_Add (A, B, R);
+    R := R + 1;
 end Add;
 
 end add_package;
