0108fdba12eeac2ee190f226d439c73ccce7cbf5
Alexander Senier <senier@componolit.com>
2019-02-18 15:57:51 +0100

    Use runtime .ali files from port

--- repos/libports/recipes/api/spark/content.mk
+++ repos/libports/recipes/api/spark/content.mk
@@ -1,4 +1,5 @@
-ADA_RT_DIR := $(call port_dir,$(GENODE_DIR)/repos/libports/ports/ada-runtime)
+ADA_RT_DIR  := $(call port_dir,$(GENODE_DIR)/repos/libports/ports/ada-runtime)
+ADA_ALI_DIR := $(call port_dir,$(GENODE_DIR)/repos/libports/ports/ada-runtime-alis)/ada-runtime-alis/alis
 
 MIRROR_FROM_ADA_RT_DIR := \
 	$(addprefix ada-runtime/contrib/gcc-6.3.0/,\
@@ -20,17 +21,39 @@ MIRROR_FROM_ADA_RT_DIR := \
 		ss_utils.ads \
 	)
 
-content: $(MIRROR_FROM_ADA_RT_DIR)
+MIRROR_FROM_ADA_ALI_DIR := \
+	ada.ali \
+	ada_exceptions.ali \
+	a-except.ali \
+	g-io.ali \
+	gnat.ali \
+	interfac.ali \
+	platform.ali \
+	s-imgint.ali \
+	s-parame.ali \
+	s-secsta.ali \
+	s-soflin.ali \
+	s-stalib.ali \
+	s-stoele.ali \
+	ss_utils.ali \
+	string_utils.ali \
+	s-unstyp.ali \
+	system.ali \
+
+content: $(MIRROR_FROM_ADA_RT_DIR) $(MIRROR_FROM_ADA_ALI_DIR)
 
 $(MIRROR_FROM_ADA_RT_DIR):
 	mkdir -p include
 	cp $(ADA_RT_DIR)/$@ include/
 
+$(MIRROR_FROM_ADA_ALI_DIR):
+	mkdir -p lib/ali/spark
+	cp $(ADA_ALI_DIR)/$@ lib/ali/spark/
+
 MIRROR_FROM_REP_DIR := \
 	lib/import/import-spark.mk \
 	include/ada \
 	lib/symbols/spark \
-	lib/ali/spark
 
 content: $(MIRROR_FROM_REP_DIR)
 
