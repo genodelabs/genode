c5896601823556b14a3d311757ae81264366fd17
Oleg Girko <ol@infoserver.lv>
2019-03-04 01:24:10 +0000

    Replace ENV make variable with MKENV.
    ENV variable conflicts with Environment Modules.
    
    Signed-off-by: Oleg Girko <ol@infoserver.lv>
    
    Fixes #3535

--- repos/ports/mk/gnu_build.mk
+++ repos/ports/mk/gnu_build.mk
@@ -145,7 +145,7 @@ Makefile reconfigure: $(MAKEFILE_LIST)
 #
 Makefile reconfigure: env.sh $(SHARED_LIBS)
 	@$(MSG_CONFIG)$(TARGET)
-	$(VERBOSE)source env.sh && $(CONFIGURE_SCRIPT) $(ENV) $(CONFIGURE_ARGS) $(CONFIGURE_OUTPUT_FILTER)
+	$(VERBOSE)source env.sh && $(CONFIGURE_SCRIPT) $(MKENV) $(CONFIGURE_ARGS) $(CONFIGURE_OUTPUT_FILTER)
 
 env.sh:
 	$(VERBOSE)rm -f $@
