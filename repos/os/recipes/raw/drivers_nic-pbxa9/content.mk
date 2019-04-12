5d4064fed413e2efc59d8ed34fd2b81cfec4f254
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2019-04-12 20:07:22 +0200

    depot: add recipe for drivers_nic-pbxa9
    Ref #3180

---
+++ repos/os/recipes/raw/drivers_nic-pbxa9/content.mk
@@ -0,0 +1,4 @@
+content: drivers.config
+
+drivers.config:
+	cp $(REP_DIR)/recipes/raw/drivers_nic-pbxa9/$@ $@
---
+++ repos/os/recipes/src/lan9118_nic_drv/content.mk
@@ -0,0 +1,2 @@
+SRC_DIR = src/drivers/nic/lan9118
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc
