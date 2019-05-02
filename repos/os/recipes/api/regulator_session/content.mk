c55663b923aeb54d5a275b25a2f99c0784d7b6f3
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2019-05-02 13:08:33 +0200

    os: extend regulator session api package
    Ref #2190
    Ref #3180

--- repos/os/recipes/api/regulator_session/content.mk
+++ repos/os/recipes/api/regulator_session/content.mk
@@ -1,2 +1,5 @@
-MIRRORED_FROM_REP_DIR := include/regulator_session
+MIRRORED_FROM_REP_DIR := include/regulator_session \
+                         include/regulator \
+                         include/spec/exynos4/regulator \
+                         include/spec/exynos5/regulator
 include $(REP_DIR)/recipes/api/session.inc
