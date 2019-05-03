e6f83d4df23b90067de1a4fa4701ff5fbdcca23b
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2019-05-03 15:47:48 +0200

    depot: add recipe for drivers_interactive-rpi
    Ref #2190

---
+++ repos/dde_linux/recipes/raw/drivers_interactive-rpi/content.mk
@@ -0,0 +1,4 @@
+content: drivers.config fb_drv.config
+
+drivers.config fb_drv.config:
+	cp $(REP_DIR)/recipes/raw/drivers_interactive-rpi/$@ $@
---
+++ repos/os/recipes/src/rpi_fb_drv/content.mk
@@ -0,0 +1,2 @@
+SRC_DIR = src/drivers/framebuffer/spec/rpi
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc
