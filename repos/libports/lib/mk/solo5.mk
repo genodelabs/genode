879f217168bbe15a23fe8cdd4c6b4d0cb0e3b676
Christian Prochaska <christian.prochaska@genode-labs.com>
2019-05-06 14:28:35 +0200

    libports: fix 'solo5' compile error with GCC 8.3.0
    Fixes #3344

--- repos/libports/lib/mk/solo5.mk
+++ repos/libports/lib/mk/solo5.mk
@@ -4,6 +4,11 @@ SHARED_LIB = yes
 
 CC_OPT += -D__SOLO5_BINDINGS__ -Drestrict=__restrict__
 
+# GCC 8 complains about a mismatch of the 'log()' function declaration.
+# Since the solo5 'log()' function is unrelated to the builtin function,
+# we disable the error message.
+CC_OPT += -Wno-builtin-declaration-mismatch
+
 SRC_CC = bindings.cc
 
 SOLO5_PORT_DIR := $(call select_from_ports,solo5)
