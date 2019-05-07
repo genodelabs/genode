be053ed257de33f1e9355b7c7c1ada31cbc8b4f8
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2019-05-07 13:45:57 +0200

    depot: add recipe for drivers_interactive-muen

---
+++ repos/os/recipes/raw/drivers_interactive-muen/content.mk
@@ -0,0 +1,7 @@
+content: drivers.config fb_drv.config input_filter.config en_us.chargen special.chargen
+
+drivers.config fb_drv.config input_filter.config:
+	cp $(REP_DIR)/recipes/raw/drivers_interactive-muen/$@ $@
+
+en_us.chargen special.chargen:
+	cp $(REP_DIR)/src/server/input_filter/$@ $@
