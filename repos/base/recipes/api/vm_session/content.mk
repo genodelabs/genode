9f700845241c16e6eb38c95f0244c50b8a279e6a
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2019-05-20 11:31:33 +0200

    base: add vm session API recipe
    Issue #3111

---
+++ repos/base/recipes/api/vm_session/content.mk
@@ -0,0 +1,9 @@
+MIRRORED_FROM_REP_DIR := include/vm_session
+
+content: $(MIRRORED_FROM_REP_DIR) LICENSE
+
+$(MIRRORED_FROM_REP_DIR):
+	$(mirror_from_rep_dir)
+
+LICENSE:
+	cp $(GENODE_DIR)/LICENSE $@
