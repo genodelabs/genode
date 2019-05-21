e60b597af5cd2c7a61faaa018a39eec9ed8714d5
Christian Helmuth <christian.helmuth@genode-labs.com>
2019-05-21 17:13:24 +0200

    linux: 32-bit compatibility with GCC versions before 7
    GCC version 7 and above generate calls to __divmoddi for 64bit integer
    division on 32-bit. Unfortunately, libgcc liberaries of older compilers
    lack this symbol and are still in use by Debian/Ubuntu LTS at least.

--- repos/base-linux/lib/mk/lx_hybrid.mk
+++ repos/base-linux/lib/mk/lx_hybrid.mk
@@ -1,8 +1,10 @@
 SRC_CC += lx_hybrid.cc new_delete.cc capability_space.cc
 SRC_CC += signal_transmitter.cc signal.cc
+SRC_C  += libgcc.c
 
 vpath new_delete.cc $(BASE_DIR)/src/lib/cxx
 vpath lx_hybrid.cc   $(REP_DIR)/src/lib/lx_hybrid
+vpath libgcc.c       $(REP_DIR)/src/lib/lx_hybrid
 
 # add parts of the base library that are shared with core
 LIBS += base-linux-common timeout
