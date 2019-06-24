48a361107f00287421e79c2afaa31e00712a4765
Norman Feske <norman.feske@genode-labs.com>
2019-06-24 11:20:42 +0200

    gpt_write: fix false-positive fallthrough warning

--- repos/gems/src/app/gpt_write/util.h
+++ repos/gems/src/app/gpt_write/util.h
@@ -92,8 +92,8 @@ namespace Util {
 		/* handle suffixes */
 		if (i > 0)
 			switch (s[i]) {
-				case 'G': res *= 1024;
-				case 'M': res *= 1024;
+				case 'G': res *= 1024; [[fallthrough]];
+				case 'M': res *= 1024; [[fallthrough]];
 				case 'K': res *= 1024; i++;
 				default: break;
 			}
