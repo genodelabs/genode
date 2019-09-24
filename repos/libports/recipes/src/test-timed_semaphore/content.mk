ad8c36fff6fe8775fc78c70a79b3384178eb7bcf
Norman Feske <norman.feske@genode-labs.com>
2019-09-24 09:38:35 +0200

    fixup "libc: split task.cc into multiple files" (test-timed_semaphore pkg)

--- repos/libports/recipes/src/test-timed_semaphore/content.mk
+++ repos/libports/recipes/src/test-timed_semaphore/content.mk
@@ -1,7 +1,7 @@
 SRC_DIR = src/test/timed_semaphore
 include $(GENODE_DIR)/repos/base/recipes/src/content.inc
 
-MIRROR_FROM_REP_DIR := src/lib/libc/timed_semaphore.h
+MIRROR_FROM_REP_DIR := src/lib/libc/internal/timed_semaphore.h
 
 content: $(MIRROR_FROM_REP_DIR)
 
--- repos/libports/src/test/timed_semaphore/target.mk
+++ repos/libports/src/test/timed_semaphore/target.mk
@@ -1,4 +1,4 @@
 TARGET   = test-timed_semaphore
 SRC_CC   = main.cc
 LIBS     = base
-INC_DIR += $(REP_DIR)/src/lib/libc
+INC_DIR += $(REP_DIR)/src/lib/libc/internal
