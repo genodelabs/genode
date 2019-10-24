d28fe9e9385f9103262ae12369243b32a055279c
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2019-10-24 15:11:38 +0200

    libports: enable gmp 6.1.2 for ARMv8
    issue #3537

--- repos/libports/lib/import/import-gmp.mk
+++ repos/libports/lib/import/import-gmp.mk
@@ -9,6 +9,10 @@ ifeq ($(filter-out $(SPECS),arm),)
 	INC_DIR += $(GMP_PORT_DIR)/include/spec/arm
 endif
 
+ifeq ($(filter-out $(SPECS),arm_64),)
+	REP_INC_DIR += include/spec/64bit/gmp
+	INC_DIR += $(GMP_PORT_DIR)/include/spec/arm_64
+endif
 ifeq ($(filter-out $(SPECS),x86_32),)
 	REP_INC_DIR += include/spec/32bit/gmp
 	INC_DIR += $(GMP_PORT_DIR)/include/spec/x86_32
