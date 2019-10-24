db18fc42fea152944f30314eceb2773eae266ed5
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2019-10-24 15:52:01 +0200

    libports: update mpfr to 4.0.2
    issue #3537

--- repos/libports/lib/mk/mpfr.mk
+++ repos/libports/lib/mk/mpfr.mk
@@ -1,14 +1,18 @@
 MPFR_PORT_DIR := $(call select_from_ports,mpfr)
 
 LIBS      = libc gmp
-CC_OPT   += -DHAVE_STDARG -DHAVE_VA_COPY -DHAVE_INTTYPES_H
-INC_DIR  += $(REP_DIR)/include/mpfr $(MPFR_PORT_DIR)/include/mpfr
 
-MPFR_SRC_C := $(notdir $(wildcard $(MPFR_PORT_DIR)/src/lib/mpfr/*.c))
-FILTER_OUT := ansi2knr.c jyn_asympt.c round_raw_generic.c speed.c tuneup.c
+CC_OPT   += -DHAVE_STDARG -DHAVE_VA_COPY -DHAVE_INTTYPES_H -DHAVE_LITTLE_ENDIAN
+
+INC_DIR  += $(REP_DIR)/include/mpfr \
+            $(MPFR_PORT_DIR)/include/mpfr \
+            $(MPFR_PORT_DIR)/include
+
+MPFR_SRC_C := $(notdir $(wildcard $(MPFR_PORT_DIR)/src/lib/mpfr/src/*.c))
+FILTER_OUT := jyn_asympt.c round_raw_generic.c
 SRC_C      := $(filter-out $(FILTER_OUT),$(MPFR_SRC_C))
 
-vpath %.c $(MPFR_PORT_DIR)/src/lib/mpfr
+vpath %.c $(MPFR_PORT_DIR)/src/lib/mpfr/src
 
 SHARED_LIB = 1
 
