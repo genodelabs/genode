2e2b3f58b7a1b7ce0ba1313f9b42813a639e426f
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2019-11-04 11:41:33 +0100

    fixup: 'libports: update mpfr to 4.0.2'
    Fix includes in mpfr API package
    
    issue #3537

--- repos/libports/recipes/api/mpfr/content.mk
+++ repos/libports/recipes/api/mpfr/content.mk
@@ -5,6 +5,9 @@ PORT_DIR := $(call port_dir,$(REP_DIR)/ports/mpfr)
 include:
 	mkdir $@
 	cp -r $(PORT_DIR)/include/mpfr/* $@/
+	cp -r $(PORT_DIR)/include/generic $@/
+	cp -r $(PORT_DIR)/include/arm $@/
+	cp -r $(PORT_DIR)/include/x86 $@/
 
 lib/symbols/mpfr:
 	$(mirror_from_rep_dir)
