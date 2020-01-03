e1e1fa23b7e889d334230033e60f291f69c060e9
Norman Feske <norman.feske@genode-labs.com>
2020-01-03 15:31:30 +0100

    zlib: fix compile warning
    This patch includes 'unistd.h' to avoid warnings about the implicit
    declaration of the functions ‘read’, 'close', and 'write.

--- repos/libports/lib/mk/zlib.mk
+++ repos/libports/lib/mk/zlib.mk
@@ -5,6 +5,7 @@ SRC_C    = adler32.c compress.c crc32.c deflate.c gzclose.c \
            gzlib.c gzread.c gzwrite.c infback.c inffast.c inflate.c \
            inftrees.c trees.c uncompr.c zutil.c
 CC_WARN  =
+CC_OPT  += -DZ_HAVE_UNISTD_H
 
 vpath %.c $(ZLIB_DIR)
 
