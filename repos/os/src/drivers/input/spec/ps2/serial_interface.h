a036d2373a2b4d0f729183238aac0e729561de00
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2020-01-21 11:15:53 +0100

    x86/ps2: disable devices during commands
    Especially the initial reset ended up with an unusable keyboard in rare
    cases before. This commit disables keyboard and mouse handling of i8042
    during command sequences according to a note from
    
      https://www.win.tue.nl/~aeb/linux/kbd/scancodes-12.html

--- repos/os/src/drivers/input/spec/ps2/serial_interface.h
+++ repos/os/src/drivers/input/spec/ps2/serial_interface.h
@@ -27,6 +27,24 @@ struct Serial_interface : Genode::Interface
 	 * (Re-)enable device interrupt
 	 */
 	virtual void enable_irq() { }
+
+	protected:
+
+		virtual void _begin_commands() { }
+		virtual void _end_commands() { }
+
+	public:
+
+		/**
+		 * Don't interfere with incoming events on command sequence
+		 */
+		template <typename FN>
+		void apply_commands(FN const &fn)
+		{
+			_begin_commands();
+			fn();
+			_end_commands();
+		}
 };
 
 #endif /* _SERIAL_INTERFACE_H_ */
