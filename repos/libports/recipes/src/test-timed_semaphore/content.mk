604f4c666b28fa25b2a0b97179aea4fe66f75933
Christian Helmuth <christian.helmuth@genode-labs.com>
2020-02-05 16:03:37 +0100

    Remove Timed_semaphore from libc incl test package
    Issue #3550

--- repos/libports/recipes/src/test-timed_semaphore/content.mk
+++
@@ -1,9 +0,0 @@
-SRC_DIR = src/test/timed_semaphore
-include $(GENODE_DIR)/repos/base/recipes/src/content.inc
-
-MIRROR_FROM_REP_DIR := src/lib/libc/internal/timed_semaphore.h
-
-content: $(MIRROR_FROM_REP_DIR)
-
-$(MIRROR_FROM_REP_DIR):
-	$(mirror_from_rep_dir)
--- repos/libports/src/test/timed_semaphore/target.mk
+++
@@ -1,4 +0,0 @@
-TARGET   = test-timed_semaphore
-SRC_CC   = main.cc
-LIBS     = base
-INC_DIR += $(REP_DIR)/src/lib/libc/internal
