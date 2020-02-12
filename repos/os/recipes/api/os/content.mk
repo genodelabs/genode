89f813f1133aa1473ab796cbbaf28093d294a0b3
Norman Feske <norman.feske@genode-labs.com>
2020-02-12 13:18:40 +0100

    os: add sandbox lib to api/os

--- repos/os/recipes/api/os/content.mk
+++ repos/os/recipes/api/os/content.mk
@@ -1,6 +1,6 @@
 INCLUDE_SUB_DIRS := os util packet_stream_rx packet_stream_tx
 
-MIRRORED_FROM_REP_DIR := $(addprefix include/,$(INCLUDE_SUB_DIRS))
+MIRRORED_FROM_REP_DIR := $(addprefix include/,$(INCLUDE_SUB_DIRS)) lib/symbols
 
 content: $(MIRRORED_FROM_REP_DIR) LICENSE
 
