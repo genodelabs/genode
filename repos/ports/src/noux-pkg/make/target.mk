3b427824a70eb78550f157f3372e3c813e65393f
Norman Feske <norman.feske@genode-labs.com>
2020-03-13 20:20:26 +0100

    ports/make: avoid aliasing with libc symbols
    This patch prefixes a few symbols in the make binary that are offered by
    both the libc and the make binary. The clash of symbol names produces
    confusing runtime linkage otherwise. This becomes a problem during
    fork/evecve.

--- repos/ports/src/noux-pkg/make/target.mk
+++ repos/ports/src/noux-pkg/make/target.mk
@@ -3,4 +3,11 @@
 #
 CPPFLAGS += -D__FreeBSD__
 
+#
+# Avoid aliasing with libc symbols to prevent the dynamic linker from
+# resolving libc-internal references to the make binary, which causes
+# trouble for fork/execve.
+#
+CPPFLAGS += $(foreach S, optarg optind opterr optopt, -D$S=mk_$S)
+
 include $(call select_from_repositories,mk/noux.mk)
