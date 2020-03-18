208294b0d383346e5ba9b9b6e2cc581ea30b93e1
Norman Feske <norman.feske@genode-labs.com>
2020-03-18 11:01:05 +0100

    Turn pkg/noux-system into pkg/system_shell
    The new package does no longer require noux.
    
    Issue #3696

--- repos/ports/recipes/raw/noux-system/content.mk
+++
@@ -1,4 +0,0 @@
-content: noux-system.config
-
-noux-system.config:
-	cp $(REP_DIR)/recipes/raw/noux-system/$@ $@
---
+++ repos/ports/recipes/raw/system_shell/content.mk
@@ -0,0 +1,4 @@
+content: system_shell.config
+
+system_shell.config:
+	cp $(REP_DIR)/recipes/raw/system_shell/$@ $@
