f051065582c198f36be12ec6fe0833e1ff9561c1
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2020-03-23 14:45:58 +0100

    Retire OMAP4 support
    Fix #2850

--- repos/base-foc/lib/mk/spec/panda/kernel-foc.mk
+++
@@ -1,3 +0,0 @@
-KERNEL_CONFIG := $(REP_DIR)/config/panda.kernel
-
-include $(REP_DIR)/lib/mk/kernel-foc.inc
--- repos/base-hw/recipes/src/base-hw-panda/content.mk
+++
@@ -1,7 +0,0 @@
-BOARD = panda
-
-include $(GENODE_DIR)/repos/base-hw/recipes/src/base-hw_content.inc
-
-content: enable_board_spec
-enable_board_spec: etc/specs.conf
-	echo "SPECS += panda" >> etc/specs.conf
