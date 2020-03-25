b484999789aabd19125395f77cdcf74aae335f2b
Norman Feske <norman.feske@genode-labs.com>
2020-03-25 10:00:26 +0100

    sculpt: replace noux-based e2fsprogs by fs_utils
    With this patch, Sculpt executes the e2fs file-system utilities
    directly instead of hosting them within the noux runtime.
    
    Issue #3696

--- repos/gems/src/app/sculpt_manager/runtime/e2fs.cc
+++ repos/gems/src/app/sculpt_manager/runtime/e2fs.cc
@@ -19,6 +19,11 @@ namespace Sculpt {
 	void gen_e2fs_start_content(Xml_generator &, Storage_target const &,
 	                            Rom_name const &, GEN_ARGS_FN const &);
 
+	template <typename ARG>
+	void gen_arg(Xml_generator &xml, ARG const &arg)
+	{
+		xml.node("arg", [&] () { xml.attribute("value", arg); });
+	}
 }
 
 
@@ -31,31 +36,33 @@ void Sculpt::gen_e2fs_start_content(Xml_generator        &xml,
 	gen_common_start_content(xml, String<64>(target.label(), ".", tool),
 	                         Cap_quota{500}, Ram_quota{100*1024*1024});
 
-	gen_named_node(xml, "binary", "noux");
+	gen_named_node(xml, "binary", tool);
 
 	xml.node("config", [&] () {
-		xml.attribute("stdout", "/dev/log");
-		xml.attribute("stderr", "/dev/log");
-		xml.attribute("stdin",  "/dev/null");
-		xml.node("fstab", [&] () {
-			gen_named_node(xml, "tar", "e2fsprogs-minimal.tar");
+		xml.node("libc", [&] () {
+			xml.attribute("stdout", "/dev/log");
+			xml.attribute("stderr", "/dev/log");
+			xml.attribute("stdin",  "/dev/null");
+			xml.attribute("rtc",    "/dev/rtc");
+		});
+		xml.node("vfs", [&] () {
 			gen_named_node(xml, "dir", "dev", [&] () {
 				gen_named_node(xml, "block", "block", [&] () {
 					xml.attribute("label", "default");
 					xml.attribute("block_buffer_count", 128);
 				});
+				gen_named_node(xml, "inline", "rtc", [&] () {
+					xml.append("2018-01-01 00:01");
+				});
 				xml.node("null", [&] () {});
 				xml.node("log",  [&] () {});
 			});
 		});
-		gen_named_node(xml, "start", Rom_name("/bin/", tool), [&] () {
-			gen_args_fn(xml); });
+		gen_args_fn(xml);
 	});
 
 	xml.node("route", [&] () {
 		target.gen_block_session_route(xml);
-		gen_parent_rom_route(xml, "noux");
-		gen_parent_rom_route(xml, "ld.lib.so");
 		gen_parent_route<Cpu_session>    (xml);
 		gen_parent_route<Pd_session>     (xml);
 		gen_parent_route<Log_session>    (xml);
@@ -69,11 +76,12 @@ void Sculpt::gen_fsck_ext2_start_content(Xml_generator &xml,
                                          Storage_target const &target)
 {
 	auto gen_args = [&] (Xml_generator &xml) {
-		xml.node("arg", [&] () { xml.attribute("value", "-yv"); });
-		xml.node("arg", [&] () { xml.attribute("value", "/dev/block"); });
+		gen_arg(xml, "fsck.ext2");
+		gen_arg(xml, "-yv");
+		gen_arg(xml, "/dev/block");
 	};
 
-	gen_e2fs_start_content(xml, target, "fsck.ext2", gen_args);
+	gen_e2fs_start_content(xml, target, "e2fsck", gen_args);
 }
 
 
@@ -81,11 +89,12 @@ void Sculpt::gen_mkfs_ext2_start_content(Xml_generator &xml,
                                          Storage_target const &target)
 {
 	auto gen_args = [&] (Xml_generator &xml) {
-		xml.node("arg", [&] () { xml.attribute("value", "-F"); });
-		xml.node("arg", [&] () { xml.attribute("value", "/dev/block"); });
+		gen_arg(xml, "mkfs.ext2");
+		gen_arg(xml, "-F");
+		gen_arg(xml, "/dev/block");
 	};
 
-	gen_e2fs_start_content(xml, target, "mkfs.ext2", gen_args);
+	gen_e2fs_start_content(xml, target, "mke2fs", gen_args);
 }
 
 
@@ -93,14 +102,10 @@ void Sculpt::gen_resize2fs_start_content(Xml_generator &xml,
                                          Storage_target const &target)
 {
 	auto gen_args = [&] (Xml_generator &xml) {
-
-		auto gen_arg = [&] (char const *arg) {
-			xml.node("arg", [&] () {
-				xml.attribute("value", arg); }); };
-
-		gen_arg("-f");
-		gen_arg("-p");
-		gen_arg("/dev/block");
+		gen_arg(xml, "resize2fs");
+		gen_arg(xml, "-f");
+		gen_arg(xml, "-p");
+		gen_arg(xml, "/dev/block");
 	};
 
 	gen_e2fs_start_content(xml, target, "resize2fs", gen_args);
