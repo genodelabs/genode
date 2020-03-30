c6efef1d894335c5e4edbdfbd13582f52baa3cb4
Norman Feske <norman.feske@genode-labs.com>
2020-03-30 17:23:07 +0200

    fixup "window layouter: restore screen tiling" (implement multiple screen targets)
    Issues #3646

--- repos/gems/src/app/window_layouter/target.h
+++ repos/gems/src/app/window_layouter/target.h
@@ -26,19 +26,23 @@ class Window_layouter::Target : Noncopyable
 
 		typedef String<64> Name;
 
+		enum class Visible { YES, NO };
+
 	private:
 
 		Name     const _name;
 		unsigned const _layer;
 		Rect     const _geometry;
+		bool     const _visible;
 
 	public:
 
-		Target(Xml_node target, Rect geometry)
+		Target(Xml_node target, Rect geometry, Visible visible)
 		:
 			_name (target.attribute_value("name", Name())),
 			_layer(target.attribute_value("layer", 9999UL)),
-			_geometry(geometry)
+			_geometry(geometry),
+			_visible(visible == Visible::YES)
 		{ }
 
 		/* needed to use class as 'Registered<Target>' */
@@ -47,6 +51,7 @@ class Window_layouter::Target : Noncopyable
 		Name     name()     const { return _name; }
 		unsigned layer()    const { return _layer; }
 		Rect     geometry() const { return _geometry; }
+		bool     visible()  const { return _visible; }
 };
 
 #endif /* _TARGET_H_ */
