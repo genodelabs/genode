7193902cc084816aed3c96f2848ec303a53f90bb
Josef Söntgen <josef.soentgen@genode-labs.com>
2020-06-19 14:49:29 +0200

    dde_bsd: properly name PCI audio driver component
    Instead of the generic name, call the PCI driver 'pci_audio_drv'.
    
    This is preliminary clean-up work before introducing the USB audio
    driver.
    
    Issue #3929.

--- repos/dde_bsd/src/drivers/audio/pci_audio_drv/target.mk
+++ repos/dde_bsd/src/drivers/audio/pci_audio_drv/target.mk
@@ -1,7 +1,9 @@
 REQUIRES = x86 pci
-TARGET   = audio_drv
+TARGET   = pci_audio_drv
 SRC_CC   = main.cc
 LIBS     = dde_bsd_audio dde_bsd_audio_pci base
 INC_DIR += $(REP_DIR)/include
+
+vpath %.cc $(REP_DIR)/src/drivers/audio
 
 CC_CXX_WARN_STRICT =
