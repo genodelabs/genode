49e907e5f60cb7059d72c65335ceef75c5e4fded
Josef Söntgen <josef.soentgen@genode-labs.com>
2020-06-29 17:28:36 +0200

    libc: remove trailing slash in lstat and stat
    Fixes #3814.

--- repos/libports/src/lib/libc/file_operations.cc
+++ repos/libports/src/lib/libc/file_operations.cc
@@ -374,6 +374,7 @@ extern "C" int lstat(const char *path, struct stat *buf)
 	try {
 		Absolute_path resolved_path;
 		resolve_symlinks_except_last_element(path, resolved_path);
+		resolved_path.remove_trailing('/');
 		FNAME_FUNC_WRAPPER(stat, resolved_path.base(), buf);
 	} catch (Symlink_resolve_error) {
 		return -1;
@@ -649,6 +650,7 @@ extern "C" int stat(const char *path, struct stat *buf)
 	try {
 		Absolute_path resolved_path;
 		resolve_symlinks(path, resolved_path);
+		resolved_path.remove_trailing('/');
 		FNAME_FUNC_WRAPPER(stat, resolved_path.base(), buf);
 	} catch(Symlink_resolve_error) {
 		return -1;
