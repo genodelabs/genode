1aed881313f2bc11a4b973f54d641a9f668312b3
Norman Feske <norman.feske@genode-labs.com>
2020-08-14 12:16:10 +0200

    Remove drivers/gpio/spec directory
    Issue #2190

--- repos/os/recipes/src/imx53_qsb_drivers/content.mk
+++ repos/os/recipes/src/imx53_qsb_drivers/content.mk
@@ -7,8 +7,8 @@ include/gpio:
 	cp -r $(REP_DIR)/include/gpio $@
 
 src/drivers:
-	mkdir -p $@/framebuffer $@/gpio/spec
-	cp -r $(REP_DIR)/src/drivers/gpio/spec/imx $@/gpio/spec/
-	cp -r $(REP_DIR)/src/drivers/gpio/spec/imx53 $@/gpio/spec/
+	mkdir -p $@/framebuffer $@/gpio
+	cp -r $(REP_DIR)/src/drivers/gpio/imx $@/gpio/
+	cp -r $(REP_DIR)/src/drivers/gpio/imx53 $@/gpio/
 	cp -r $(REP_DIR)/src/drivers/framebuffer/imx53 $@/framebuffer/
 	cp -r $(REP_DIR)/include/spec/imx53/imx_framebuffer_session $@/framebuffer/imx53/
--- repos/os/src/drivers/gpio/spec/imx6/target.mk
+++
@@ -1,7 +0,0 @@
-TARGET   = imx6_gpio_drv
-REQUIRES = arm_v7
-SRC_CC   = main.cc
-LIBS     = base
-INC_DIR += $(PRG_DIR) $(REP_DIR)/src/drivers/gpio/spec/imx
-
-vpath main.cc $(REP_DIR)/src/drivers/gpio/spec/imx
