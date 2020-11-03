eafbfb8edfa80502f4307369e49bc6455b5940fa
Christian Prochaska <christian.prochaska@genode-labs.com>
2020-11-03 15:53:44 +0100

    base-linux: socket descriptor caps for RPC (BOARD=pc)
    Fixes #3932

--- repos/base-linux/src/core/spec/pc/dataspace_component.cc
+++ repos/base-linux/src/core/spec/pc/dataspace_component.cc
@@ -64,13 +64,13 @@ Dataspace_component::Dataspace_component(const char *args)
 : _fname(_file_name(args)),
   _size(_file_size()),
   _addr(0),
-  _fd(lx_open(_fname.buf, O_RDONLY | LX_O_CLOEXEC, S_IRUSR | S_IXUSR)),
+  _cap(_fd_to_cap(lx_open(_fname.buf, O_RDONLY | LX_O_CLOEXEC, S_IRUSR | S_IXUSR))),
   _writable(false),
   _owner(0) { }
 
 Dataspace_component::Dataspace_component(size_t size, addr_t, addr_t phys_addr,
         Cache_attribute, bool writable, Dataspace_owner *_owner) :
-    _size(size), _addr(phys_addr), _fd(-1), _writable(writable), _owner(_owner)
+    _size(size), _addr(phys_addr), _cap(), _writable(writable), _owner(_owner)
 {
     _fname.buf[0] = 0;
 }
