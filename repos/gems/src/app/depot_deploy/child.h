1f2905592714bfd03c7ce4ec786c601d98ff04f1
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2021-02-25 11:38:18 +0100

    depot_deploy: support providing CPU and PD service
    Issue #3837

--- repos/gems/src/app/depot_deploy/child.h
+++ repos/gems/src/app/depot_deploy/child.h
@@ -440,6 +440,8 @@ void Depot_deploy::Child::gen_start_node(Xml_generator &xml, Xml_node common,
 					_gen_provides_sub_node(xml, service, "rom",         "ROM");
 					_gen_provides_sub_node(xml, service, "terminal",    "Terminal");
 					_gen_provides_sub_node(xml, service, "timer",       "Timer");
+					_gen_provides_sub_node(xml, service, "pd",          "PD");
+					_gen_provides_sub_node(xml, service, "cpu",         "CPU");
 				});
 			});
 		}
