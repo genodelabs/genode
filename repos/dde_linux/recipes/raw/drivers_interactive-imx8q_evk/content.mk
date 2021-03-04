7ce1f8e92d958c648b92e0fca332359d1f89db71
Christian Helmuth <christian.helmuth@genode-labs.com>
2021-03-04 09:20:07 +0100

    Enable en_us chargen in drivers_interactive-imx8q_evk
    Issue #4038

--- repos/dde_linux/recipes/raw/drivers_interactive-imx8q_evk/content.mk
+++ repos/dde_linux/recipes/raw/drivers_interactive-imx8q_evk/content.mk
@@ -1,4 +1,7 @@
-content: drivers.config fb_drv.config
+content: drivers.config fb_drv.config event_filter.config en_us.chargen special.chargen
 
-drivers.config fb_drv.config:
+drivers.config fb_drv.config event_filter.config:
 	cp $(REP_DIR)/recipes/raw/drivers_interactive-imx8q_evk/$@ $@
+
+en_us.chargen special.chargen:
+	cp $(GENODE_DIR)/repos/os/src/server/event_filter/$@ $@
