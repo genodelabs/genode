56e231fd09a42541e6193a7c195eac20495bbe9b
Tomasz Gajewski <tomga@wp.pl>
2021-04-01 00:36:22 +0200

    app/menu_view: use CUSTOM_TARGET_DEPS

--- repos/gems/src/app/menu_view/target.mk
+++ repos/gems/src/app/menu_view/target.mk
@@ -3,8 +3,7 @@ SRC_CC   = main.cc
 LIBS     = base libc libm vfs libpng zlib blit file
 INC_DIR += $(PRG_DIR)
 
-.PHONY: menu_view_styles.tar
+CUSTOM_TARGET_DEPS += menu_view_styles.tar
 
-all: menu_view_styles.tar
 menu_view_styles.tar:
 	$(VERBOSE)cd $(PRG_DIR); tar cf $(PWD)/bin/$@ styles
