d5e4ffd1912cf8a042ae79edb1024eafd04ea949
Christian Prochaska <christian.prochaska@genode-labs.com>
2021-04-04 23:42:20 +0200

    sanitizer: update to version 10.3.0
    Issue #4094

--- repos/libports/lib/mk/libsanitizer_common.mk
+++ repos/libports/lib/mk/libsanitizer_common.mk
@@ -2,31 +2,33 @@ SANITIZER_PORT_DIR := $(call select_from_ports,sanitizer)
 
 SANITIZER_DIR := $(SANITIZER_PORT_DIR)/src/lib/sanitizer
 
-SRC_CC = sanitizer_allocator.cc \
+SRC_CC = sanitizer_allocator.cpp \
          sanitizer_common.cc \
-         sanitizer_common_libcdep.cc \
-         sanitizer_coverage_libcdep_new.cc \
-         sanitizer_file.cc \
-         sanitizer_flag_parser.cc \
-         sanitizer_flags.cc \
+         sanitizer_common_libcdep.cpp \
+         sanitizer_coverage_libcdep_new.cpp \
+         sanitizer_file.cpp \
+         sanitizer_flag_parser.cpp \
+         sanitizer_flags.cpp \
          sanitizer_genode.cc \
-         sanitizer_libc.cc \
-         sanitizer_persistent_allocator.cc \
-         sanitizer_printf.cc \
-         sanitizer_stackdepot.cc \
-         sanitizer_stacktrace.cc \
-         sanitizer_stacktrace_libcdep.cc \
-         sanitizer_stacktrace_printer.cc \
-         sanitizer_suppressions.cc \
-         sanitizer_symbolizer.cc \
-         sanitizer_symbolizer_libbacktrace.cc \
-         sanitizer_symbolizer_libcdep.cc \
-         sanitizer_symbolizer_posix_libcdep.cc \
-         sanitizer_termination.cc
+         sanitizer_libc.cpp \
+         sanitizer_persistent_allocator.cpp \
+         sanitizer_printf.cpp \
+         sanitizer_stackdepot.cpp \
+         sanitizer_stacktrace.cpp \
+         sanitizer_stacktrace_libcdep.cpp \
+         sanitizer_stacktrace_printer.cpp \
+         sanitizer_suppressions.cpp \
+         sanitizer_symbolizer.cpp \
+         sanitizer_symbolizer_libbacktrace.cpp \
+         sanitizer_symbolizer_libcdep.cpp \
+         sanitizer_symbolizer_posix_libcdep.cpp \
+         sanitizer_symbolizer_report.cpp \
+         sanitizer_termination.cpp
 
 INC_DIR += $(SANITIZER_DIR)
 
-vpath %.cc $(SANITIZER_DIR)/sanitizer_common
+vpath %.cc  $(SANITIZER_DIR)/sanitizer_common
+vpath %.cpp $(SANITIZER_DIR)/sanitizer_common
 
 SHARED_LIB = yes
 
--- repos/libports/lib/mk/libubsan.mk
+++ repos/libports/lib/mk/libubsan.mk
@@ -2,14 +2,15 @@ SANITIZER_PORT_DIR := $(call select_from_ports,sanitizer)
 
 SANITIZER_DIR := $(SANITIZER_PORT_DIR)/src/lib/sanitizer
 
-SRC_CC = ubsan_diag.cc \
-         ubsan_flags.cc \
-         ubsan_handlers.cc \
-         ubsan_handlers_cxx.cc \
-         ubsan_init.cc \
-         ubsan_type_hash.cc \
-         ubsan_type_hash_itanium.cc \
-         ubsan_value.cc
+SRC_CC = ubsan_diag.cpp \
+         ubsan_flags.cpp \
+         ubsan_handlers.cpp \
+         ubsan_handlers_cxx.cpp \
+         ubsan_init.cpp \
+         ubsan_monitor.cpp \
+         ubsan_type_hash.cpp \
+         ubsan_type_hash_itanium.cpp \
+         ubsan_value.cpp
 
 CC_OPT += -DCAN_SANITIZE_UB=1 -DUBSAN_CAN_USE_CXXABI=1
 
@@ -17,7 +18,7 @@ LIBS += libsanitizer_common
 
 INC_DIR += $(SANITIZER_DIR)
 
-vpath %.cc $(SANITIZER_DIR)/ubsan
+vpath %.cpp $(SANITIZER_DIR)/ubsan
 
 SHARED_LIB = yes
 
