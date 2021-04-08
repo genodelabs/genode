373b45a0f037555a02aac52bc06d89847c3d6346
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2021-04-08 16:37:57 +0200

    depot: fix content.mk of drivers_managed-pc
    Was still using the event_filter.config from drivers_interactive-pc
    although a dedicated file is present in the raw archive.
    
    The fix is just for consistency reasons, as sculpt manager is generating the
    event_filter.config anyway.

--- repos/gems/recipes/raw/drivers_managed-pc/content.mk
+++ repos/gems/recipes/raw/drivers_managed-pc/content.mk
@@ -1,10 +1,10 @@
 content: drivers.config fb_drv.config event_filter.config en_us.chargen \
          special.chargen numlock_remap.config
 
-drivers.config numlock_remap.config:
+drivers.config numlock_remap.config event_filter.config:
 	cp $(REP_DIR)/recipes/raw/drivers_managed-pc/$@ $@
 
-fb_drv.config event_filter.config:
+fb_drv.config:
 	cp $(GENODE_DIR)/repos/os/recipes/raw/drivers_interactive-pc/$@ $@
 
 en_us.chargen special.chargen:
