471d34a367c7bebf51427b748abe47d9e6206e87
Christian Prochaska <christian.prochaska@genode-labs.com>
2021-04-23 14:30:26 +0200

    Remove mpc and mpfr ports
    These ports are not needed anymore with the 21.05 tool chain update.
    
    Issue #4094

--- repos/libports/lib/import/import-mpc.mk
+++
@@ -1,2 +0,0 @@
-REP_INC_DIR += include/mpc
-INC_DIR     += $(call select_from_ports,mpc)/include/mpc
--- repos/libports/lib/import/import-mpfr.mk
+++
@@ -1,2 +0,0 @@
-REP_INC_DIR += include/mpfr
-INC_DIR     += $(call select_from_ports,mpfr)/include/mpfr
