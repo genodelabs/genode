52a4293bbccb989a4eb26dc5f5c45c52b3ad5eea
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2021-06-16 14:28:20 +0200

    depot: add usb_modem_drv archives
    genodelabs/genode#4201

---
+++ repos/dde_linux/recipes/src/usb_modem_drv/content.mk
@@ -0,0 +1,32 @@
+PORT_DIR := $(call port_dir,$(REP_DIR)/ports/dde_linux)
+
+MIRROR_FROM_REP_DIR := src/drivers/usb_modem \
+                       src/lx_kit \
+                       src/include \
+                       lib/import/import-usb_modem_include.mk \
+                       lib/import/import-usb_arch_include.mk \
+                       lib/mk/usb_modem_include.mk \
+                       $(foreach SPEC, \
+                                 arm arm_64 x86_32 x86_64, \
+                                 lib/mk/spec/$(SPEC)/lx_kit_setjmp.mk)
+
+MIRROR_FROM_PORT_DIR := $(shell cd $(PORT_DIR); \
+                                find src/drivers/usb_modem -type f | \
+                                     grep -v ".git")
+
+MIRROR_FROM_PORT_DIR := $(filter-out $(MIRROR_FROM_REP_DIR), \
+                                     $(MIRROR_FROM_PORT_DIR))
+
+content: $(MIRROR_FROM_REP_DIR) $(MIRROR_FROM_PORT_DIR)
+
+$(MIRROR_FROM_REP_DIR):
+	$(mirror_from_rep_dir)
+
+$(MIRROR_FROM_PORT_DIR):
+	mkdir -p $(dir $@)
+	cp $(PORT_DIR)/$@ $@
+
+content: LICENSE
+LICENSE:
+	( echo "GNU General Public License version 2, see:"; \
+	  echo "https://www.kernel.org/pub/linux/kernel/COPYING" ) > $@
