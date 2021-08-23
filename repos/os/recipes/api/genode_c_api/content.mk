436d946300dd2db6cdc9c889379cdbc870892013
Norman Feske <norman.feske@genode-labs.com>
2021-08-23 18:21:40 +0200

    recipes/api/genode_c_api: add src/lib/genode_c_api
    The source codes are needed to use the API because they are statically
    linked.

--- repos/os/recipes/api/genode_c_api/content.mk
+++ repos/os/recipes/api/genode_c_api/content.mk
@@ -1,6 +1,9 @@
-content: include/genode_c_api LICENSE
+MIRROR_FROM_REP_DIR := include/genode_c_api \
+                       src/lib/genode_c_api
 
-include/genode_c_api:
+content: $(MIRROR_FROM_REP_DIR) LICENSE
+
+$(MIRROR_FROM_REP_DIR):
 	$(mirror_from_rep_dir)
 
 LICENSE:
