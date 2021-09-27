222105ea261829e171b4d00e219f91da0444beff
Tomasz Gajewski <tomga@wp.pl>
2021-09-27 21:30:44 +0200

    ssh_terminal: moved to genode world
    Fixes #4258

--- repos/gems/recipes/src/ssh_terminal/content.mk
+++
@@ -1,8 +0,0 @@
-SRC_DIR := src/server/ssh_terminal
-include $(GENODE_DIR)/repos/base/recipes/src/content.inc
-
-content: $(MIRROR_FROM_LIBPORTS)
-
-$(MIRROR_FROM_LIBPORTS):
-	mkdir -p $(dir $@)
-	cp -r $(GENODE_DIR)/repos/libports/$@ $(dir $@)
