b12b0ed93d4649047baf5e426f97d90c71e072b8
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2021-10-18 11:41:24 +0200

    imx8q_evk: remove board support from repository
    Moved to separate repo at https://github.com/skalk/genode-imx/
    
    Fix #4301

--- repos/dde_linux/recipes/raw/drivers_interactive-imx8q_evk/content.mk
+++
@@ -1,7 +0,0 @@
-content: drivers.config fb_drv.config event_filter.config en_us.chargen special.chargen
-
-drivers.config fb_drv.config event_filter.config:
-	cp $(REP_DIR)/recipes/raw/drivers_interactive-imx8q_evk/$@ $@
-
-en_us.chargen special.chargen:
-	cp $(GENODE_DIR)/repos/os/src/server/event_filter/$@ $@
