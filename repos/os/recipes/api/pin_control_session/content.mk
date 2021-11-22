09d020508c952059a106726190bb944513600659
Norman Feske <norman.feske@genode-labs.com>
2021-11-22 12:33:42 +0100

    depot: recipes for pin-control/state session APIs
    Issue #4315

---
+++ repos/os/recipes/api/pin_control_session/content.mk
@@ -0,0 +1,2 @@
+MIRRORED_FROM_REP_DIR := include/pin_control_session
+include $(REP_DIR)/recipes/api/session.inc
---
+++ repos/os/recipes/api/pin_state_session/content.mk
@@ -0,0 +1,2 @@
+MIRRORED_FROM_REP_DIR := include/pin_state_session
+include $(REP_DIR)/recipes/api/session.inc
