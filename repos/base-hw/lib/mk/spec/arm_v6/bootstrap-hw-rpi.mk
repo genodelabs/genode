6bd7f167a4cb7b605e86f0581cb9553824e4b8b8
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-01-04 15:57:38 +0100

    hw: remove rpi1/3 board support
    The code moved to https://github.com/skalk/genode-rpi
    
    Ref #4363

--- repos/base-hw/lib/mk/spec/arm_v6/bootstrap-hw-rpi.mk
+++
@@ -1,8 +0,0 @@
-REP_INC_DIR += src/bootstrap/board/rpi
-
-SRC_CC  += bootstrap/board/rpi/platform.cc
-SRC_CC  += bootstrap/spec/arm/arm_v6_cpu.cc
-SRC_CC  += hw/spec/32bit/memory_map.cc
-SRC_S   += bootstrap/spec/arm/crt0.s
-
-include $(call select_from_repositories,lib/mk/bootstrap-hw.inc)
