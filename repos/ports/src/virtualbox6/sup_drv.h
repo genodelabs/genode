5fed1641a4c5b1bcff53951916876b309452fa6c
Christian Prochaska <christian.prochaska@genode-labs.com>
2022-01-04 22:44:46 +0100

    virtualbox6: reduce 'Vm_connection' priority
    Fixes #4361

--- repos/ports/src/virtualbox6/sup_drv.h
+++ repos/ports/src/virtualbox6/sup_drv.h
@@ -55,7 +55,8 @@ class Sup::Drv
 
 		Cpu_virt const _cpu_virt { _cpu_virt_from_rom() };
 
-		Vm_connection _vm_connection { _env };
+		Vm_connection _vm_connection { _env, "",
+		                               Cpu_session::PRIORITY_LIMIT / 2 };
 
 		Gip _gip { _env, _cpu_count, _cpu_freq_khz_from_rom() };
 		Gmm _gmm { _env, _vm_connection };
