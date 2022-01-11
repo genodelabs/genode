3ba316b015f2e28b89985c09f81654c5767d6265
Norman Feske <norman.feske@genode-labs.com>
2022-01-11 14:40:01 +0100

    base-hw: declare build artifact for core

--- repos/base-hw/src/core/hw/target.mk
+++ repos/base-hw/src/core/hw/target.mk
@@ -1,6 +1,8 @@
 BOARD    ?= unknown
-TARGET   := core_hw_$(BOARD)
+TARGET   := core-hw-$(BOARD)
 LIBS     := core-hw-$(BOARD)
 CORE_LIB := core-hw-$(BOARD).a
+
+BUILD_ARTIFACTS := $(CORE_LIB)
 
 include $(BASE_DIR)/src/core/target.inc
