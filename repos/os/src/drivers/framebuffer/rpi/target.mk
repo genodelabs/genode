7248957553e4ac38cfb672089b8cc88a86294b32
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-01-11 14:02:14 +0100

    rpi_fb_drv: avoid alignment fault by own blit func
    Fix #4331

--- repos/os/src/drivers/framebuffer/rpi/target.mk
+++ repos/os/src/drivers/framebuffer/rpi/target.mk
@@ -1,6 +1,6 @@
 TARGET   = rpi_fb_drv
 REQUIRES = arm_v6
 SRC_CC   = main.cc
-LIBS     = base blit
+LIBS     = base
 INC_DIR += $(PRG_DIR)
 INC_DIR += $(call select_from_repositories,include/spec/rpi)
