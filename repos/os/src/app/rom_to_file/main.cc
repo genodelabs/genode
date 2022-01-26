fd936c36fe89069dbf11f40101b6d3cb706ac9b5
Norman Feske <norman.feske@genode-labs.com>
2022-01-26 17:32:34 +0100

    rom_to_file: use VFS instead of fs session
    This patch replaces the former direct use of a file-system session by
    the use of the VFS API.
    
    Issue #4390

--- repos/os/src/app/rom_to_file/main.cc
+++ repos/os/src/app/rom_to_file/main.cc
@@ -14,17 +14,9 @@
 /* Genode includes */
 #include <base/component.h>
 #include <base/heap.h>
-#include <rom_session/connection.h>
-#include <base/signal.h>
 #include <base/attached_rom_dataspace.h>
-#include <util/print_lines.h>
-#include <util/reconstructible.h>
-
 #include <base/heap.h>
-#include <base/allocator_avl.h>
-
-#include <file_system_session/connection.h>
-#include <file_system/util.h>
+#include <os/vfs.h>
 
 
 namespace Rom_to_file {
@@ -32,29 +24,21 @@ namespace Rom_to_file {
 	using namespace Genode;
 
 	struct Main;
-
-	enum  {
-		BLOCK_SIZE   = 512,
-		QUEUE_SIZE   = File_system::Session::TX_QUEUE_SIZE,
-		TX_BUF_SIZE  = BLOCK_SIZE * (QUEUE_SIZE*2 + 1)
-	};
 }
 
 
 struct Rom_to_file::Main
 {
-	Genode::Env  &_env;
-	Genode::Heap  _heap { _env.ram(), _env.rm() };
+	Env  &_env;
+	Heap  _heap { _env.ram(), _env.rm() };
 
-	Genode::Attached_rom_dataspace _config_rom { _env, "config" };
+	Attached_rom_dataspace _config_rom { _env, "config" };
 
-	Allocator_avl _alloc { &_heap };
-
-	File_system::Connection _fs { _env, _alloc, "", "/", true, TX_BUF_SIZE };
+	Root_directory _root_dir { _env, _heap, _config_rom.xml().sub_node("vfs") };
 
 	Constructible<Attached_rom_dataspace> _rom_ds { };
 
-	typedef Genode::String<100> Rom_name;
+	typedef String<100> Rom_name;
 
 	/**
 	 * Name of currently requested ROM module
@@ -72,7 +56,7 @@ struct Rom_to_file::Main
 	Signal_handler<Main> _update_dispatcher {
 		_env.ep(), *this, &Main::_handle_update };
 
-	Main(Genode::Env &env) : _env(env)
+	Main(Env &env) : _env(env)
 	{
 		_config_rom.sigh(_update_dispatcher);
 		_handle_update();
@@ -84,7 +68,7 @@ void Rom_to_file::Main::_handle_update()
 {
 	_config_rom.update();
 
-	Genode::Xml_node config = _config_rom.xml();
+	Xml_node config = _config_rom.xml();
 
 	/*
 	 * Query name of ROM module from config
@@ -114,53 +98,17 @@ void Rom_to_file::Main::_handle_update()
 		_rom_ds->update();
 
 		if (_rom_ds->valid()) {
-			using namespace File_system;
-
-			char dir_path[] = "/";
-			const char *file_name = _rom_name.string();
 
 			try {
-				Dir_handle   dir_handle = ensure_dir(_fs, dir_path);
-				Handle_guard dir_guard(_fs, dir_handle);
-				Constructible<File_handle> handle;
+				New_file new_file { _root_dir, _rom_name };
 
-				try {
-					handle.construct(_fs.file(dir_handle, file_name, File_system::WRITE_ONLY, true));
-				} catch (Node_already_exists) {
-					handle.construct(_fs.file(dir_handle, file_name, File_system::WRITE_ONLY, false));
-				}
+				size_t const len = max(strlen(_rom_ds->local_addr<char>()),
+				                       _rom_ds->size());
 
-				_fs.truncate(*handle, 0);
-
-				size_t len     = max(strlen(_rom_ds->local_addr<char>()), _rom_ds->size());
-				size_t written = write(_fs, *handle, _rom_ds->local_addr<void>(), len, 0);
-
-				if (written < len) {
-					warning(written, " of ", len, " bytes have been written");
-				}
-
-				_fs.close(*handle);
-
-			} catch (Permission_denied) {
-				error(Cstring(dir_path), file_name, ": permission denied");
-
-			} catch (No_space) {
-				error("file system out of space");
-
-			} catch (Out_of_ram) {
-				error("server ran out of RAM");
-
-			} catch (Out_of_caps) {
-				error("server ran out of caps");
-
-			} catch (Invalid_name) {
-				error(Cstring(dir_path), file_name, ": invalid path");
-
-			} catch (Name_too_long) {
-				error(Cstring(dir_path), file_name, ": name too long");
+				new_file.append(_rom_ds->local_addr<char>(), len);
 
 			} catch (...) {
-				error("cannot open file ", Cstring(dir_path), file_name);
+				error("cannot create file ", _rom_name);
 				throw;
 			}
 		} else {
