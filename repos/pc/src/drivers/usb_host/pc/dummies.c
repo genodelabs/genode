25488301408db15ae145a38614bff740dc3f3dfb
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2022-03-15 13:57:30 +0100

    pc_linux: add ACPI config
    required by the upcoming update of the intel display driver. Make this addition
    explicit, because it triggers adjustment also on the new pc_usb_host_drv.
    
    Issue #4450

--- repos/pc/src/drivers/usb_host/pc/dummies.c
+++ repos/pc/src/drivers/usb_host/pc/dummies.c
@@ -13,6 +13,8 @@
 
 #include <lx_emul.h>
 
+const struct attribute_group pci_dev_acpi_attr_group;
+
 #include <linux/cpuhotplug.h>
 
 int __cpuhp_setup_state(enum cpuhp_state state,const char * name,bool invoke,int (* startup)(unsigned int cpu),int (* teardown)(unsigned int cpu),bool multi_instance)
@@ -461,3 +463,104 @@ void pci_fixup_device(enum pci_fixup_pass pass,struct pci_dev * dev)
 }
 
 
+int acpi_platform_notify(struct device *dev, enum kobject_action action)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+int pci_acpi_program_hp_params(struct pci_dev *dev)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
+
+
+bool acpi_driver_match_device(struct device *dev, const struct device_driver *drv)
+{
+	lx_emul_trace(__func__);
+	return false;
+}
+
+
+bool is_acpi_device_node(const struct fwnode_handle *fwnode)
+{
+	lx_emul_trace(__func__);
+	return false;
+}
+
+
+enum dev_dma_attr acpi_get_dma_attr(struct acpi_device *adev)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+int acpi_device_uevent_modalias(struct device *dev, struct kobj_uevent_env *event)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+int acpi_dma_configure_id(struct device *dev, enum dev_dma_attr attr, const u32 *input_id)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+int acpi_device_modalias(struct device *device, char * x, int y)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+bool pciehp_is_native(struct pci_dev *bridge)
+{
+	lx_emul_trace_and_stop(__func__);
+}
+
+
+struct irq_domain *pci_host_bridge_acpi_msi_domain(struct pci_bus *bus)
+{
+	lx_emul_trace(__func__);
+	return NULL;
+}
+
+
+typedef void * acpi_handle;
+
+union acpi_object *acpi_evaluate_dsm(acpi_handle handle, const guid_t *guid,
+                                     u64 rev, u64 func, union acpi_object *argv4)
+{
+	lx_emul_trace(__func__);
+	return NULL;
+}
+
+
+int usb_acpi_register(void)
+{
+	lx_emul_trace(__func__);
+	return 0;
+};
+
+
+void usb_acpi_unregister(void)
+{
+	lx_emul_trace(__func__);
+};
+
+
+struct usb_device;
+bool usb_acpi_power_manageable(struct usb_device *hdev, int index)
+{
+	lx_emul_trace(__func__);
+	return false;
+}
+
+
+int usb_acpi_set_power_state(struct usb_device *hdev, int index, bool enable)
+{
+	lx_emul_trace(__func__);
+	return 0;
+}
