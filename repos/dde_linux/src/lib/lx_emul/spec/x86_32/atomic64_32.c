77cdceabaabff80c2f96c8dc3a259ddb29de9bb8
Josef Söntgen <josef.soentgen@genode-labs.com>
2022-03-18 17:00:29 +0100

    lx_emul: add additional atomic64 functions
    Needed by the WLAN driver on x86_32.
    
    Issue #4455.

--- repos/dde_linux/src/lib/lx_emul/spec/x86_32/atomic64_32.c
+++ repos/dde_linux/src/lib/lx_emul/spec/x86_32/atomic64_32.c
@@ -30,3 +30,15 @@ s64 arch_atomic64_fetch_add(s64 i, atomic64_t *v)
 	v->counter += i;
 	return v->counter;
 }
+
+
+s64 arch_atomic64_dec_return(atomic64_t *v)
+{
+	return arch_atomic64_sub(1, v);
+}
+
+
+s64 arch_atomic64_inc_return(atomic64_t *v)
+{
+	return arch_atomic64_add(1, v);
+}
