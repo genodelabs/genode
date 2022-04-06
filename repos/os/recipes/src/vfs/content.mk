0c5d8f1156a573c807aeaed04a570bfe21d826a3
Norman Feske <norman.feske@genode-labs.com>
2022-04-06 13:12:53 +0200

    depot: exclude plugins from vfs src archive
    This fixes the following warning when building the binary archive:
    
      Library-description file vfs_capture.mk is missing
      Library-description file vfs_tap.mk is missing

--- repos/os/recipes/src/vfs/content.mk
+++ repos/os/recipes/src/vfs/content.mk
@@ -1,7 +1,10 @@
 SRC_DIR = include/file_system src/server/vfs
 include $(GENODE_DIR)/repos/base/recipes/src/content.inc
 
+LIB_SRC := $(notdir $(wildcard $(REP_DIR)/src/lib/vfs/*.h $(REP_DIR)/src/lib/vfs/*.cc))
+
 MIRROR_FROM_REP_DIR += $(addprefix include/ram_fs/,chunk.h param.h) \
+                       $(addprefix src/lib/vfs/,$(LIB_SRC)) \
                        lib/mk/vfs.mk src/lib/vfs
 
 content: $(MIRROR_FROM_REP_DIR)
