fd14cf9f1eb8a5b6ddac1fe12ba91f2d27f0ab57
Josef Söntgen <josef.soentgen@genode-labs.com>
2022-05-17 17:39:54 +0200

    Move libnl to own port
    Issue #4508.

--- repos/pc/recipes/src/pc_wifi_drv/content.mk
+++ repos/pc/recipes/src/pc_wifi_drv/content.mk
@@ -22,6 +22,8 @@ MIRROR_FROM_OS_DIR  := src/lib/genode_c_api/uplink.cc
 DDE_LINUX_REP_DIR  := $(GENODE_DIR)/repos/dde_linux
 DDE_LINUX_PORT_DIR := $(call port_dir,$(DDE_LINUX_REP_DIR)/ports/dde_linux)
 
+LIBNL_PORT_DIR := $(call port_dir,$(DDE_LINUX_REP_DIR)/ports/libnl)
+
 DDE_LINUX_LIB_MK := \
           $(addprefix lib/mk/,libnl.inc libnl_include.mk) \
           $(foreach SPEC,x86_32 x86_64,lib/mk/spec/$(SPEC)/libnl.mk) \
@@ -35,11 +37,11 @@ MIRROR_FROM_DDE_LINUX_DIR := $(DDE_LINUX_LIB_MK) \
                              $(shell cd $(DDE_LINUX_REP_DIR); find src/lib/wpa_driver_nl80211 -type f) \
                              $(shell cd $(DDE_LINUX_REP_DIR); find src/lib/wpa_supplicant -type f)
 
-MIRROR_FROM_DDE_LINUX_PORT_DIR := $(shell cd $(DDE_LINUX_PORT_DIR); find src/lib/libnl -type f) \
-                                  $(shell cd $(DDE_LINUX_PORT_DIR); find src/app/wpa_supplicant -type f)
+MIRROR_FROM_DDE_LINUX_PORT_DIR := $(shell cd $(DDE_LINUX_PORT_DIR); find src/app/wpa_supplicant -type f)
+MIRROR_FROM_LIBNL_PORT_DIR     := $(shell cd $(LIBNL_PORT_DIR); find src/lib/libnl -type f)
 
 content: $(MIRROR_FROM_REP_DIR) $(MIRROR_FROM_OS_DIR) $(MIRROR_FROM_DDE_LINUX_DIR) \
-         $(MIRROR_FROM_DDE_LINUX_PORT_DIR) cleanup-wpa
+         $(MIRROR_FROM_DDE_LINUX_PORT_DIR) $(MIRROR_FROM_LIBNL_PORT_DIR) cleanup-wpa
 
 $(MIRROR_FROM_REP_DIR):
 	$(mirror_from_rep_dir)
@@ -55,6 +57,10 @@ $(MIRROR_FROM_DDE_LINUX_DIR):
 $(MIRROR_FROM_DDE_LINUX_PORT_DIR):
 	mkdir -p $(dir $@)
 	cp -r $(DDE_LINUX_PORT_DIR)/$@ $@
+
+$(MIRROR_FROM_LIBNL_PORT_DIR):
+	mkdir -p $(dir $@)
+	cp -r $(LIBNL_PORT_DIR)/$@ $@
 
 cleanup-wpa: $(MIRROR_FROM_DDE_LINUX_PORT_DIR)
 	@for dir in .git doc eap_example hs20 mac80211_hwsim radius_example \
