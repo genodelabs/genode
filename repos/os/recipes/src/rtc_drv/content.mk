7cb0986c1fa4e87c63afb36bc12fbdc79d2bf4ca
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-08-26 10:49:38 +0200

    rtc_drv: extend src package to dummy driver

--- repos/os/recipes/src/rtc_drv/content.mk
+++ repos/os/recipes/src/rtc_drv/content.mk
@@ -1,10 +1,2 @@
-SRC_DIR = src/drivers/rtc/spec/x86
+SRC_DIR = src/drivers/rtc
 include $(GENODE_DIR)/repos/base/recipes/src/content.inc
-
-CONTENT = src/drivers/rtc/README src/drivers/rtc/rtc.h src/drivers/rtc/main.cc \
-          src/drivers/rtc/target.inc
-
-content: $(CONTENT)
-
-$(CONTENT):
-	$(mirror_from_rep_dir)
