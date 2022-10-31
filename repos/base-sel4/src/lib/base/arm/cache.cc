d8d9f940751d579ba0df9e86af68dc1bdf8e5a61
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-10-31 11:53:13 +0100

    base-sel4: implement clean cache operations
    This commit uses the seL4_BenchmarkFlushCaches syscall to partly implement
    cache maintaince for this kernel.
    
    Ref genodelabs/genode#4651

--- repos/base-sel4/src/lib/base/arm/cache.cc
+++ repos/base-sel4/src/lib/base/arm/cache.cc
@@ -14,15 +14,19 @@
 #include <base/log.h>
 #include <cpu/cache.h>
 
+/* seL4 includes */
+#include <sel4/arch/syscalls.h>
+
+
 void Genode::cache_coherent(Genode::addr_t, Genode::size_t)
 {
-	error(__func__, " not implemented for this kernel!");
+	seL4_BenchmarkFlushCaches();
 }
 
 
 void Genode::cache_clean_invalidate_data(Genode::addr_t, Genode::size_t)
 {
-	error(__func__, " not implemented for this kernel!");
+	seL4_BenchmarkFlushCaches();
 }
 
 
