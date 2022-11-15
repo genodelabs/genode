9e61fb90c57cebfd399ac4dc8531ad539e99df21
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-11-15 15:59:31 +0100

    Remove legacy platform_drv, API, and platform.inc
    Fix genodelabs/genode#4671

--- repos/os/recipes/api/platform_session/content.mk
+++ repos/os/recipes/api/platform_session/content.mk
@@ -1,9 +1,3 @@
-INCLUDE_SUB_DIRS := platform_session \
-                    legacy/x86/platform_session \
-                    legacy/x86/platform_device
-
-INCLUDE_DIRS := $(addprefix include/,$(INCLUDE_SUB_DIRS))
-
-MIRRORED_FROM_REP_DIR := $(INCLUDE_DIRS)
+MIRRORED_FROM_REP_DIR := include/platform_session
 
 include $(REP_DIR)/recipes/api/session.inc
