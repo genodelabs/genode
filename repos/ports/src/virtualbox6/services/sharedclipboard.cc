2c92fc4b6f31ee8c6277436cdd8e6666e7f1e397
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2022-11-17 11:30:50 +0100

    vbox6: add null pointer check for clipboard
    Check 'pv == nullptr' in 'ShClSvcImplWriteData' and return
    VERR_INVALID_POINTER if invalid (as is done, for example, in the X11
    implementation).
    
    issue #4666

--- repos/ports/src/virtualbox6/services/sharedclipboard.cc
+++ repos/ports/src/virtualbox6/services/sharedclipboard.cc
@@ -164,6 +164,9 @@ int ShClSvcImplWriteData(PSHCLCLIENT, PSHCLCLIENTCMDCTX, SHCLFORMAT fFormat, voi
 	if (!(fFormat & VBOX_SHCL_FMT_UNICODETEXT))
 		return VERR_NOT_IMPLEMENTED;
 
+	if (pv == nullptr)
+		return VERR_INVALID_POINTER;
+
 	Clipboard::Guard guard;
 
 	PCRTUTF16 const utf16_string = (PCRTUTF16)pv;
