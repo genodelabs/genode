0b28c9ae6f506b1e7873b6b73b3447880a08130b
Josef Söntgen <josef.soentgen@genode-labs.com>
2023-04-12 09:17:22 +0200

    lib/vfs/wifi: remove dependency on wifi library
    The wifi VFS plugin is solely there to provide a hook for calling
    'Lx_kit::initialize()' prior to the static ctors. As the corresponding
    symbol can be resolved at run-time there is no need to add the
    library as a build-time dependency.
    
    Issue #4813

--- repos/pc/lib/mk/vfs_wifi.mk
+++ repos/pc/lib/mk/vfs_wifi.mk
@@ -4,8 +4,6 @@ DDE_LINUX_DIR := $(subst /src/include/lx_kit,,$(call select_from_repositories,sr
 
 INC_DIR += $(DDE_LINUX_DIR)/src/include
 
-LIBS := wifi
-
 vpath %.cc $(REP_DIR)/src/lib/vfs/wifi
 
 SHARED_LIB := yes
