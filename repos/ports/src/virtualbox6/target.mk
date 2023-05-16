cfca9899b26b2bd57956238377cc073c7d2d142a
Christian Helmuth <christian.helmuth@genode-labs.com>
2023-05-16 11:35:08 +0200

    virtualbox6: fix build errors with `-std=gnu++20`
    Fixes #4872

--- repos/ports/src/virtualbox6/target.mk
+++ repos/ports/src/virtualbox6/target.mk
@@ -2,8 +2,7 @@ REQUIRES = x86_64
 
 TARGET = virtualbox6
 
-# fake build pass for lib .inc
-called_from_lib_mk := yes
+included_from_target_mk := yes
 include $(REP_DIR)/lib/mk/virtualbox6-common.inc
 
 CC_WARN += -Wall
