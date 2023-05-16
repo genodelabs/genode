e1b6855352754ccd7c18b6b913fb2932658b3f88
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-05-16 08:17:53 +0200

    libport: remove icu library
    This was required by QtWebKit in the past, the QtWebEngine ships its own
    version. No other code depends on this. Revive if needed.
    
    fixes #4870

--- repos/libports/lib/import/import-icu.mk
+++
@@ -1,8 +0,0 @@
-ifeq ($(CONTRIB_DIR),)
-ICU_INC_DIR := $(call select_from_repositories,include/icu)
-else
-ICU_INC_DIR := $(call select_from_ports,icu)/include/icu
-endif
-
-INC_DIR += $(ICU_INC_DIR)/common
-INC_DIR += $(ICU_INC_DIR)/i18n
