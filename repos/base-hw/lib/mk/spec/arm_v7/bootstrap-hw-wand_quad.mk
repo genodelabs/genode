5e7e6514be925a8bd3694ef2cbb647f3a29dc583
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2023-06-28 16:18:35 +0200

    Remove i.MX6 platforms from base repositories
    Fix genodelabs/genode#4941

--- repos/base-hw/lib/mk/spec/arm_v7/bootstrap-hw-wand_quad.mk
+++
@@ -1,8 +0,0 @@
-REP_INC_DIR += src/bootstrap/board/wand_quad
-
-SRC_CC  += bootstrap/spec/arm/cpu.cc
-SRC_CC  += bootstrap/spec/arm/cortex_a9_mmu.cc
-SRC_CC  += bootstrap/spec/arm/gicv2.cc
-SRC_CC  += bootstrap/spec/arm/imx6_platform.cc
-
-include $(call select_from_repositories,lib/mk/spec/arm_v7/bootstrap-hw.inc)
--- repos/base/src/timer/epit/imx6/timer.cc
+++ repos/base/src/timer/epit/imx6/timer.cc
@@ -14,19 +14,22 @@
  * under the terms of the GNU Affero General Public License version 3.
  */
 
-/* base include */
-#include <drivers/defs/imx6.h>
-
 /* local include */
 #include <time_source.h>
+
+enum {
+	EPIT_2_IRQ       = 89,
+	EPIT_2_MMIO_BASE = 0x020d4000,
+	EPIT_2_MMIO_SIZE = 0x00004000
+};
 
 using namespace Genode;
 
 Timer::Time_source::Time_source(Env &env)
 :
-	Attached_mmio(env, Imx6::EPIT_2_MMIO_BASE, Imx6::EPIT_2_MMIO_SIZE),
+	Attached_mmio(env, EPIT_2_MMIO_BASE, EPIT_2_MMIO_SIZE),
 	Signalled_time_source(env),
-	_timer_irq(env, unsigned(Imx6::EPIT_2_IRQ))
+	_timer_irq(env, unsigned(EPIT_2_IRQ))
 {
 	_timer_irq.sigh(_signal_handler);
 	while (read<Cr::Swr>()) ;
