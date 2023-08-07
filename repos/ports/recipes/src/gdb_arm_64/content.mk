ae899b9bc7008c737a30c56b0c325b6ca647bbf8
Christian Prochaska <christian.prochaska@genode-labs.com>
2023-08-07 08:47:03 +0200

    ports: add a test for GDB
    Fixes #5014

---
+++ repos/ports/recipes/src/gdb_arm_64/content.mk
@@ -0,0 +1,12 @@
+content: src/noux-pkg/gdb LICENSE
+
+PORT_DIR := $(call port_dir,$(REP_DIR)/ports/gdb)
+
+src/noux-pkg/gdb:
+	mkdir -p $@
+	cp -a $(PORT_DIR)/src/noux-pkg/gdb/* $@
+	cp -a  $(REP_DIR)/src/noux-pkg/gdb/* $@
+	cp -a  $(REP_DIR)/src/noux-pkg/gdb_arm_64/* $@
+
+LICENSE:
+	cp $(PORT_DIR)/src/noux-pkg/gdb/COPYING $@
