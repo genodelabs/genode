2d48e209f4a15598c85fa119965a949af04a397c
Norman Feske <norman.feske@genode-labs.com>
2023-11-17 21:30:37 +0100

    fb_sdl: fix build on 64-bit ARM Linux

--- repos/os/src/drivers/framebuffer/sdl/target.mk
+++ repos/os/src/drivers/framebuffer/sdl/target.mk
@@ -4,3 +4,8 @@ REQUIRES = linux
 SRC_CC   = main.cc
 LX_LIBS  = sdl2
 INC_DIR += $(PRG_DIR) /usr/include/SDL2
+
+# accept conversion warnings caused by libgcc's include/arm_neon.h
+CC_OPT  += -Wno-error=narrowing \
+           -Wno-error=float-conversion \
+           -Wno-error=unused-parameter
