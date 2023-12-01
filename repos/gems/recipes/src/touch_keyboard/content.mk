610ba6e3b6e66fd8d28955a2c43b6ef2ba11166c
Norman Feske <norman.feske@genode-labs.com>
2023-12-01 15:57:25 +0100

    touch_keyboard: add dialog API to src recipe
    This is a follow-up commit to issue #5059. Without it, the binary
    archive for the touch_keyboard remains empty.

--- repos/gems/recipes/src/touch_keyboard/content.mk
+++ repos/gems/recipes/src/touch_keyboard/content.mk
@@ -1,3 +1,10 @@
 SRC_DIR := src/app/touch_keyboard
 
+MIRROR_FROM_REP_DIR := lib/mk/dialog.mk src/lib/dialog include/dialog
+
+content: $(MIRROR_FROM_REP_DIR)
+
+$(MIRROR_FROM_REP_DIR):
+	$(mirror_from_rep_dir)
+
 include $(GENODE_DIR)/repos/base/recipes/src/content.inc
