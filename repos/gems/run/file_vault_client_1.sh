02ef158748df49f0601547e0432e54bfd15e9c05
Martin Stein <martin.stein@genode-labs.com>
2023-12-01 04:21:46 +0100

    gems: add autopilot test 'file_vault_client'
    The script tests the use of an encrypted file system that is created and
    provided via the File Vault.
    
    Furthermore the script can be used for test-driving existing File-Vault
    containers (created with potentially older File-Vault versions) under the
    current File-Vault version. This is done via the "LX_FS_DIR_TEMPLATE"
    env variable.
    
    Ref #5062

---
+++ repos/gems/run/file_vault_client_1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Hallo Welt!" > file_vault/file_1
+cat file_vault/file_1
+echo "Ein zweiter Test." > file_vault/file_1
+cat file_vault/file_1
+ls -la file_vault/
+mkdir file_vault/dir_1
+echo "Eine weitere Datei." > file_vault/dir_1/file_2
+echo "Mit mehr Inhalt." >> file_vault/dir_1/file_2
+cat file_vault/dir_1/file_2
+echo "Und Sonderzeichen: /§($)=%!" >> file_vault/dir_1/file_2
+cat file_vault/dir_1/file_2
+ls -la file_vault/
+ls -la file_vault/dir_1
+exit 0
---
+++ repos/gems/run/file_vault_client_2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ls -la file_vault/
+ls -la file_vault/dir_1
+cat file_vault/file_1
+cat file_vault/dir_1/file_2
+exit 0
