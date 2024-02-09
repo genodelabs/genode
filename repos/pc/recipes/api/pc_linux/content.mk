c12224ffd3cbc117865030af9bf8f0e891363b6d
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2024-02-09 19:10:25 +0100

    api/pc_linux: adjust to legacy lx_kit removal
    With the removal of legacy lx_kit 'src/include/spec/x86' does not longer
    exist in dde_linux.
    
    issue #5104

--- repos/pc/recipes/api/pc_linux/content.mk
+++ repos/pc/recipes/api/pc_linux/content.mk
@@ -6,7 +6,6 @@ MIRRORED_FROM_DDE_LINUX := src/lib/lx_emul \
                            src/lib/lx_kit \
                            src/include/lx_emul \
                            src/include/lx_user \
-                           src/include/spec/x86 \
                            src/include/spec/x86_32 \
                            src/include/spec/x86_64 \
                            src/include/lx_kit \
