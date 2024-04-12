75e1f1797d0e1d1e0b37311bcd7c247041d4478b
Josef Söntgen <josef.soentgen@genode-labs.com>
2024-04-12 15:08:15 +0200

    sculpt_manager: raise runtime_view RAM quota
    Issue #5174.

--- repos/gems/src/app/sculpt_manager/dialog/distant_runtime.h
+++ repos/gems/src/app/sculpt_manager/dialog/distant_runtime.h
@@ -52,8 +52,8 @@ class Dialog::Distant_runtime : Noncopyable
 		Event::Seq_number _global_seq_number { 1 };
 
 		Start_name const _start_name   { "runtime_view" };
-		Ram_quota  const _initial_ram  { 4*1024*1024 };
-		Cap_quota  const _initial_caps { 200 };
+		Ram_quota  const _initial_ram  { 52*1024*1024 };
+		Cap_quota  const _initial_caps { 300 };
 
 		Ram_quota _ram  = _initial_ram;
 		Cap_quota _caps = _initial_caps;
