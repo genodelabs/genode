6162eae9e02846ed21ca16f57d9c28148f799ba1
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2024-06-19 13:43:05 +0200

    os: move tz_vmm example to imx repository
    Ref genodelabs/genode#5252

--- repos/os/src/server/tz_vmm/spec/imx53_qsb/target.mk
+++
@@ -1,5 +0,0 @@
-TARGET   = imx53_qsb_tz_vmm
-INC_DIR += $(REP_DIR)/src/server/tz_vmm/spec/imx53_qsb
-SRC_CC  += spec/imx53_qsb/vm.cc
-
-include $(REP_DIR)/src/server/tz_vmm/spec/imx53/target.inc
--- repos/os/src/server/tz_vmm/spec/usb_armory/target.mk
+++
@@ -1,5 +0,0 @@
-TARGET   = usb_armory_tz_vmm
-INC_DIR += $(REP_DIR)/src/server/tz_vmm/spec/usb_armory
-SRC_CC  += spec/usb_armory/vm.cc
-
-include $(REP_DIR)/src/server/tz_vmm/spec/imx53/target.inc
