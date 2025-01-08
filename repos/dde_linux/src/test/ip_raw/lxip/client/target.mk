836caa299d64f32e04c3fd106a463dac063d939e
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2025-01-08 12:21:23 +0100

    lxip_raw: split test to support more IP stacks
    issue #5471

--- repos/dde_linux/src/test/ip_raw/lxip/client/target.mk
+++ repos/dde_linux/src/test/ip_raw/lxip/client/target.mk
@@ -2,6 +2,10 @@ TARGET   = test-lxip_client
 LIBS     = lxip base net
 SRC_CC   = main.cc
 
-INC_DIR += $(PRG_DIR)/../include
+IP_DIR = $(REP_DIR)/src/test/ip_raw
+
+INC_DIR += $(IP_DIR)/include
+
+vpath %.cc $(IP_DIR)/client
 
 CC_CXX_WARN_STRICT =
--- repos/dde_linux/src/test/ip_raw/lxip/server/target.mk
+++ repos/dde_linux/src/test/ip_raw/lxip/server/target.mk
@@ -2,6 +2,10 @@ TARGET   = test-lxip_server
 LIBS     = lxip base net
 SRC_CC   = main.cc
 
-INC_DIR += $(PRG_DIR)/../include
+IP_DIR = $(REP_DIR)/src/test/ip_raw
+
+INC_DIR += $(IP_DIR)/include
+
+vpath %.cc $(IP_DIR)/server
 
 CC_CXX_WARN_STRICT =
