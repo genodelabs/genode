0e48ea08cf1cf38b60df36a666b4670d869ce150
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2025-01-14 11:09:57 +0100

    os: move genode_c_api/nic_client from lxip to os
    The Nic-client C-API is based on the Uplink C-API.
    
    issue #5471

--- repos/os/src/lib/genode_c_api/nic_client.cc
+++ repos/os/src/lib/genode_c_api/nic_client.cc
@@ -7,8 +7,8 @@
 /*
  * Copyright (C) 2021 Genode Labs GmbH
  *
- * This file is distributed under the terms of the GNU General Public License
- * version 2.
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
  */
 
 #include <base/registry.h>
