acf1488e65c5f7d0b2aaa9e2e47d895743229d48
Josef Söntgen <josef.soentgen@genode-labs.com>
2025-01-23 14:45:47 +0100

    libnl: use fixed_stint.h for typedefs
    Issue #5431.

--- repos/dde_linux/src/lib/libnl/include/linux/types.h
+++ repos/dde_linux/src/lib/libnl/include/linux/types.h
@@ -1,16 +1,16 @@
 #ifndef _LINUX_TYPES_H
 #define _LINUX_TYPES_H
 
-#include <platform/types.h>
+#include <base/fixed_stdint.h>
 
-typedef signed char    __s8;
-typedef unsigned char  __u8;
-typedef signed short   __s16;
-typedef unsigned short __u16;
-typedef signed int     __s32;
-typedef unsigned int   __u32;
-typedef int64_t        __s64;
-typedef uint64_t       __u64;
+typedef genode_int8_t   __s8;
+typedef genode_uint8_t  __u8;
+typedef genode_int16_t  __s16;
+typedef genode_uint16_t __u16;
+typedef genode_int32_t  __s32;
+typedef genode_uint32_t __u32;
+typedef genode_int64_t  __s64;
+typedef genode_uint64_t __u64;
 
 typedef __u16 __be16;
 typedef __u32 __be32;
