a7422e316d0ffd023aab4f59cf6c6966418d9c42
Christian Prochaska <christian.prochaska@genode-labs.com>
2025-02-21 15:06:49 +0100

    base: increase linker area size
    Fixes #5460

--- repos/base/include/pd_session/pd_session.h
+++ repos/base/include/pd_session/pd_session.h
@@ -189,7 +189,7 @@ struct Genode::Pd_session : Session, Pd_account, Ram_allocator
 	 ** Virtual address-space management **
 	 **************************************/
 
-	enum { LINKER_AREA_SIZE = 256*1024*1024UL };
+	enum { LINKER_AREA_SIZE = 320*1024*1024UL };
 
 	/**
 	 * Return region map of the PD's virtual address space
