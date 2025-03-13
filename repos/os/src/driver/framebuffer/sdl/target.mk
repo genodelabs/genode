8561f7724df8ba2686b2f2e7ca09f59d15703636
Benjamin Lamowski <benjamin.lamowski@genode-labs.com>
2025-03-13 10:49:53 +0100

    fb_sdl: don't treat host warning as error
    The sdl2-compat library from version 2.30.54 will issue the warning
    "#warning SDL_config.h might be incomplete, good luck"
    on unrecognized platforms, introduced with commit f4abaea[1].
    
    Add -Wno-error=cpp for fb_sdl to prevent this warning to be treated as
    an error.
    
    Fixes #5476
    
    [1] https://github.com/libsdl-org/sdl2-compat/commit/f4abaea268893ad8b67acdb732d8cd20b3072febu

--- repos/os/src/driver/framebuffer/sdl/target.mk
+++ repos/os/src/driver/framebuffer/sdl/target.mk
@@ -8,4 +8,5 @@ INC_DIR += $(PRG_DIR) /usr/include/SDL2
 # accept conversion warnings caused by libgcc's include/arm_neon.h
 CC_OPT  += -Wno-error=narrowing \
            -Wno-error=float-conversion \
-           -Wno-error=unused-parameter
+           -Wno-error=unused-parameter \
+           -Wno-error=cpp
