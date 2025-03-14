b4e6b96a19da4bd422d73edd7f7d79c3c63c8d58
Josef Söntgen <josef.soentgen@genode-labs.com>
2025-03-14 17:22:18 +0100

    libc: implement _SC_CLK_TCK
    Return the same value that is used in FreeBSD.
    
    Issue #5507.

--- repos/libports/src/lib/libc/sysctl.cc
+++ repos/libports/src/lib/libc/sysctl.cc
@@ -33,7 +33,7 @@
 
 using namespace Libc;
 
-enum { PAGESIZE = 4096 };
+enum { PAGESIZE = 4096, STATHZ = 128, };
 
 
 static Genode::Env *_global_env;
@@ -49,6 +49,7 @@ extern "C" long sysconf(int name)
 {
 	switch (name) {
 	case _SC_CHILD_MAX:        return CHILD_MAX;
+	case _SC_CLK_TCK:          return STATHZ;
 	case _SC_OPEN_MAX:         return getdtablesize();
 	case _SC_PAGESIZE:         return PAGESIZE;
 	case _SC_PHYS_PAGES:
