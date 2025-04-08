2f60a55327fdcb7624f99762339dc64311522f94
Josef Söntgen <josef.soentgen@genode-labs.com>
2025-04-08 17:15:48 +0200

    Remove lx_block component
    The 'lx_block' component uses the aged Block::Driver framework
    internally and most if not all use-cases where it could be employed
    can be addressed by using the 'vfs_block' component instead.
    
    This commit also removes some run-scripts that make use of 'lx_block'
    whose components are already excercised otherwise.
    
    Issue #5522.

--- repos/os/recipes/raw/test-lx_block/content.mk
+++
@@ -1,4 +0,0 @@
-content: lx_block.img
-
-lx_block.img:
-	dd if=/dev/zero of=$@ bs=1M count=64 2>/dev/null
--- repos/os/src/server/lx_block/target.mk
+++
@@ -1,7 +0,0 @@
-TARGET   = lx_block
-REQUIRES = linux
-
-SRC_CC   = main.cc
-LIBS     = lx_hybrid
-
-INC_DIR += $(PRG_DIR) /usr/include
