42bc8d8e8799e995fff96f043a9e22dbadf346d3
Christian Helmuth <christian.helmuth@genode-labs.com>
2025-05-15 16:41:45 +0200

    hw/riscv: add dummy symbols of signal receiver
    These symbols are never called in core but the linker fails to
    garbage-collect them as on other platforms.

---
+++ repos/base-hw/src/core/spec/riscv/dummies.c
@@ -0,0 +1,5 @@
+void _ZN6Genode15Signal_receiver14pending_signalEv()  {}
+void _ZN6Genode15Signal_receiver16block_for_signalEv() {}
+void _ZN6Genode15Signal_receiver20_platform_destructorEv() {}
+void _ZN6Genode15Signal_receiver24_platform_begin_dissolveERNS_14Signal_contextE() {}
+void _ZN6Genode15Signal_receiver25_platform_finish_dissolveERNS_14Signal_contextE() {}
