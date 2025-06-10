42dc30d43c219e8ab49a38b777590f816fe97fb1
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2025-06-10 19:36:09 +0200

    sel4: convert io_mem resources only once
    In case that IO-MEM resources are requested for serveral dataspaces in
    (disjunct) parts on the same physical 4k range, the sel4 kernel will deny
    to convert the same 4k physical page multiple times. Seen with USB EHCI
    device, e.g. on IMX6q-sabrelite.
    
    Instead, check whether a IO-MEM resource is requested the first time
    respectively the last reference is freed, so that the base-sel4 code can only
    convert the frame one time respectively revert the frame only once.
    
    Fixes #5597

--- repos/base-sel4/src/core/io_mem_session_support.cc
+++ repos/base-sel4/src/core/io_mem_session_support.cc
@@ -1,6 +1,7 @@
 /*
  * \brief  Implementation of the IO_MEM session interface
  * \author Norman Feske
+ * \author Alexander Boettcher
  * \date   2015-05-01
  */
 
@@ -26,18 +27,98 @@ Io_mem_session_component::Dataspace_attr Io_mem_session_component::_acquire(Phys
 
 	auto const size = request.size();
 	auto const base = request.base();
+	auto const req_base = request.req_base;
+	auto const req_size = request.req_size;
 
-	size_t const num_pages = size >> get_page_size_log2();
-
-	if (!Untyped_memory::convert_to_page_frames(base, num_pages))
+	if (size < get_page_size())
 		return Dataspace_attr();
 
-	return Dataspace_attr(size, 0 /* no core local mapping */,
-                          base, _cacheable, request.req_base);
+	/* check whether first/last pages are already in use by another ds */
+	bool first_page_used = _io_mem_alloc.alloc_addr(get_page_size(),
+	                                                base).failed();
+	bool  last_page_used = _io_mem_alloc.alloc_addr(get_page_size(),
+	                                                base + size -
+	                                                get_page_size()).failed();
+
+	/* try allocation of whole requested region */
+	bool const ok = _io_mem_alloc.alloc_addr(req_size, req_base).convert<bool>(
+		[&] (Range_allocator::Allocation &) { return true; },
+		[&] (Alloc_error) { return false; });
+
+	if (!ok) {
+		error("I/O memory ", Hex_range<addr_t>(req_base, req_size),
+		      " not available");
+		return Dataspace_attr();
+	}
+
+	auto convert_phys = base;
+	auto convert_size = size;
+
+	/* if first page was in use beforehand, exclude it from conversion */
+	if (first_page_used) {
+		convert_phys += get_page_size();
+		convert_size -= get_page_size();
+	}
+
+	if (convert_size >= get_page_size()) {
+		/* if last page was in use beforehand, exclude it from conversion */
+		if (last_page_used)
+			convert_size -= get_page_size();
+
+		size_t const num_pages = convert_size >> get_page_size_log2();
+
+		if (!Untyped_memory::convert_to_page_frames(convert_phys, num_pages))
+			return Dataspace_attr();
+	}
+
+	return Dataspace_attr(size, 0 /* no core local mapping */, base,
+	                      _cacheable, req_base);
 }
 
 
 void Io_mem_session_component::_release(Dataspace_attr const &attr)
 {
-	Untyped_memory::convert_to_untyped_frames(attr.phys_addr, attr.size);
+	/*
+	 * The generic base io_mem implementation already removed the region
+	 * from _io_mem_alloc. Now we have to check, which parts must be
+	 * converted back to untyped memory. The first and last page of the
+	 * region may still be in use by another io-mem allocation, so check
+	 * for first/last page and exclude it from conversion if required.
+	 */
+	auto convert_phys = attr.phys_addr;
+	auto convert_size = attr.size;
+	auto page_size    = get_page_size();
+
+	if (convert_size >= page_size)
+		_io_mem_alloc.alloc_addr(page_size, convert_phys).with_result(
+			/* if the region is not occupied anymore, it becomes untyped */
+			[&] (Range_allocator::Allocation &) { },
+			[&] (Alloc_error) {
+				/*
+				 * The region is still occupied by another allocation,
+				 * exclude the page from conversion back to untyped memory
+				 */
+				convert_phys += page_size;
+				convert_size -= page_size;
+			});
+
+	if (convert_size >= page_size)
+		_io_mem_alloc.alloc_addr(page_size, convert_phys + convert_size -
+			                                page_size).with_result(
+
+			/* if the region is not occupied anymore, it becomes untyped */
+			[&] (Range_allocator::Allocation &) { },
+			[&] (Alloc_error) {
+				/*
+				 * The region is still occupied by another allocation,
+				 * exclude the page from conversion back to untyped memory
+				 */
+				convert_size -= page_size;
+			});
+
+	auto const num_pages = convert_size >> get_page_size_log2();
+
+	if (num_pages)
+		Untyped_memory::convert_to_untyped_frames(convert_phys,
+		                                          page_size * num_pages);
 }
