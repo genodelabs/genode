c6e6457479590c7a66acaa55e7f7acdd0740977a
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2025-09-23 20:41:58 +0200

    base: adapt test for priority scaling
    Use separate (dummy) components instead of threads (support for multiple
    priority levels in the same PD was removed).
    
    Issue #5681

--- repos/base/src/test/scale_priority/main.cc
+++ repos/base/src/test/scale_priority/main.cc
@@ -15,37 +15,16 @@
 #include <base/attached_rom_dataspace.h>
 #include <base/component.h>
 #include <base/log.h>
-#include <base/thread.h>
-#include <cpu_session/connection.h>
 #include <trace_session/connection.h>
-
+#include <timer_session/connection.h>
 
 using namespace Genode;
-
-/******************************************
- ** Using cpu-session for thread creation *
- ******************************************/
-
-struct Cpu_helper : Thread
-{
-	Env &_env;
-
-	Cpu_helper(Env &env, Name const &name)
-	:
-		Thread(env, name, Stack_size { 4096 }, { }), _env(env)
-	{ }
-
-	void entry() override
-	{
-		log(Thread::name);
-	}
-};
-
 
 struct Main
 {
 	Env                    &env;
 	Attached_rom_dataspace  config { env, "config" };
+	Timer::Connection       timer { env };
 
 	unsigned const prio_levels_log2   { _prio_levels_from_node(config.node(), env) };
 	long const     prio_max           { (1 << prio_levels_log2) - 1 };
@@ -54,18 +33,7 @@ struct Main
 	long const     highest_prio_end   { platform_prio_size-1 };
 	long const     lowest_prio_start  { prio_max * platform_prio_size };
 
-
-	Cpu_connection cpu_high  { env, "highest",        highest_prio_end };
-	Cpu_connection cpu_high2 { env, "second highest", highest_prio_end+1 };
-	Cpu_connection cpu_low   { env, "lowest",         lowest_prio_start };
-	Cpu_connection cpu_low2  { env, "second lowest",  lowest_prio_start-1 };
-
-	Cpu_helper     thread_high  { env, "highest" };
-	Cpu_helper     thread_high2 { env, "second highest" };
-	Cpu_helper     thread_low   { env, "lowest" };
-	Cpu_helper     thread_low2  { env, "second lowest" };
-
-	size_t arg_buffer_ram  { 4096 };
+	size_t arg_buffer_ram  { 4*4096 };
 	size_t trace_ram_quota { arg_buffer_ram + 4 * 4096 };
 
 	Trace::Connection trace { env,
@@ -92,10 +60,8 @@ Main::Main(Env &env) : env(env)
 	bool const inverse       = config.node().attribute_value("inverse", true);
 	unsigned const offset    = start_at_zero ? 0 : 1;
 
-	thread_low.start();
-	thread_low2.start();
-	thread_high.start();
-	thread_high2.start();
+	/* wait for dummy components */
+	timer.msleep(1000);
 
 	/* query thread priorities from TRACE */
 	unsigned low_prio   = ~0U;
@@ -105,20 +71,20 @@ Main::Main(Env &env) : env(env)
 	trace.for_each_subject_info([&](Trace::Subject_id const &,
 	                                Trace::Subject_info const &info)
 	{
-		if (info.thread_name() == thread_low.name)
+		if (info.thread_name() == "lowest")
 			low_prio = info.execution_time().priority;
-		else if (info.thread_name() == thread_low2.name)
+		else if (info.thread_name() == "second lowest")
 			low2_prio = info.execution_time().priority;
-		else if (info.thread_name() == thread_high.name)
+		else if (info.thread_name() == "highest")
 			high_prio = info.execution_time().priority;
-		else if (info.thread_name() == thread_high2.name)
+		else if (info.thread_name() == "second highest")
 			high2_prio = info.execution_time().priority;
 	});
 
 	auto check_priority = [&](long const expected, long const current,
-	                          Thread const &thread) {
+	                          String<32> const &name) {
 		if (expected != current) {
-			error("Unexpected priority of Thread ", thread.name,
+			error("Unexpected priority of component ", name,
 			      " expected ", expected, " got ", current);
 			return false;
 		}
@@ -128,21 +94,16 @@ Main::Main(Env &env) : env(env)
 
 	bool success = true;
 	if (!inverse) {
-		success &= check_priority(prio_max + offset,     low_prio,   thread_low);
-		success &= check_priority(prio_max + offset - 1, low2_prio,  thread_low2);
-		success &= check_priority(offset,                high_prio,  thread_high);
-		success &= check_priority(offset + 1,            high2_prio, thread_high2);
+		success &= check_priority(prio_max + offset,     low_prio,   "lowest");
+		success &= check_priority(prio_max + offset - 1, low2_prio,  "second lowest");
+		success &= check_priority(offset,                high_prio,  "highest");
+		success &= check_priority(offset + 1,            high2_prio, "second highest");
 	} else {
-		success &= check_priority(offset,                low_prio,   thread_low);
-		success &= check_priority(offset + 1,            low2_prio,  thread_low2);
-		success &= check_priority(prio_max + offset,     high_prio,  thread_high);
-		success &= check_priority(prio_max + offset - 1, high2_prio, thread_high2);
+		success &= check_priority(offset,                low_prio,   "lowest");
+		success &= check_priority(offset + 1,            low2_prio,  "second lowest");
+		success &= check_priority(prio_max + offset,     high_prio,  "highest");
+		success &= check_priority(prio_max + offset - 1, high2_prio, "second highest");
 	}
-
-	thread_low.join();
-	thread_low2.join();
-	thread_high.join();
-	thread_high2.join();
 
 	if (success)
 		env.parent().exit(0);
