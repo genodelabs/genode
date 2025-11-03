9c113736f22ad79af7a6147e5fd4f551ac3cafac
Josef Söntgen <josef.soentgen@genode-labs.com>
2025-11-03 15:32:44 +0100

    libports: remove pcsc-lite and ccid
    These ports are now part of 'genode-world' and are provided in form of
    Goa projects.
    
    Fixes #5730.

--- repos/libports/lib/import/import-pcsc-lite.mk
+++
@@ -1,6 +0,0 @@
-PCSCLITE_DIR := $(call select_from_ports,pcsc-lite)
-ifeq ($(CONTRIB_DIR),)
-REP_INC_DIR += include/PCSC
-else
-INC_DIR += $(PCSCLITE_DIR)/include/PCSC
-endif
--- repos/libports/src/test/smartcard/target.mk
+++
@@ -1,7 +0,0 @@
-TARGET = test-smartcard
-LIBS   = base pcsc-lite posix libusb ccid
-SRC_CC = main.cc
-
-vpath main.cc $(PRG_DIR)/..
-
-CC_CXX_WARN_STRICT =
