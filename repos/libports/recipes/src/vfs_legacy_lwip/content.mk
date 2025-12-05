6ef98e38c740b30e08c08d042b291c47005c86a3
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2025-12-05 20:44:07 +0100

    libports: remove legacy_lwip and vfs_legacy_lwip
    With the unified vfs/ip-plugin in place and the old lwip/vfs_lwip
    versions being deprecated since Sculpt-25.04 its time to remove them.
    
    issue #5768

--- repos/libports/recipes/src/vfs_legacy_lwip/content.mk
+++
@@ -1,9 +0,0 @@
-MIRROR_FROM_REP_DIR := src/lib/vfs/legacy_lwip lib/mk/vfs_legacy_lwip.mk
-
-content: $(MIRROR_FROM_REP_DIR) LICENSE
-
-$(MIRROR_FROM_REP_DIR):
-	$(mirror_from_rep_dir)
-
-LICENSE:
-	cp $(GENODE_DIR)/LICENSE $@
