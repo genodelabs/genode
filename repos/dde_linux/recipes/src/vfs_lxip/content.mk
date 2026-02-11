132bdd3ad1730feec688cfd8ee42de20047c3a4d
Norman Feske <norman.feske@genode-labs.com>
2026-02-11 15:29:13 +0100

    fixup 'dde_linux: update linux to 6.18.5' (arm_v6 removal, src recipe)
    Issue #5798

--- repos/dde_linux/recipes/src/vfs_lxip/content.mk
+++ repos/dde_linux/recipes/src/vfs_lxip/content.mk
@@ -1,5 +1,5 @@
 LIB_MK := $(addprefix lib/mk/,lxip.inc vfs_lxip.mk) \
-          $(foreach SPEC,x86_32 x86_64 arm_v6 arm_v7 arm_v8,lib/mk/spec/$(SPEC)/lxip.mk)
+          $(foreach SPEC,x86_32 x86_64 arm_v7 arm_v8,lib/mk/spec/$(SPEC)/lxip.mk)
 
 MIRROR_FROM_REP_DIR := $(LIB_MK) \
                        lib/import/import-lxip.mk \
